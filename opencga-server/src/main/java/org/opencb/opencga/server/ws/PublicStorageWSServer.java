df0d430ddc48c85f377531e8f00527d3f490be19
Cristina Yenyxe Gonzalez Garcia <cgonzalez@cipf.es>
2013-12-13 13:03:42 +0100

    Public fetch: Checking permissions on bucket and file format

--- opencga-server/src/main/java/org/opencb/opencga/server/ws/PublicStorageWSServer.java
+++ opencga-server/src/main/java/org/opencb/opencga/server/ws/PublicStorageWSServer.java
@@ -50,13 +50,19 @@ public class PublicStorageWSServer extends GenericWSServer {
     public Response fetchData(@DefaultValue("") @PathParam("objectId") String objectIdFromURL,
                            @DefaultValue("") @QueryParam("region") String regionStr) {
         try {
-            // CSM:     
-            //   * validate user and object permissions
-            //   * retrieve paths (how? they are not in a bucket)
-            //cloudSessionManager.getObjectPath(accountId, accountId, objectId)
             Properties storageProperties = Config.getStorageProperties(Config.getGcsaHome());
+            if (storageProperties == null) {
+                return createErrorResponse("\"storage.properties\" file not found!");
+            }
+            
+            // 1: Check if the bucket is in the list of allowed folders
+            if (!isBucketAllowed(storageProperties)) {
+                throw new IllegalArgumentException("Reading the files inside the requested folder is forbidden");
+            }
+            
+            // 2: Check if the path corresponding to alias folder + filename exists
             if (!storageProperties.containsKey("OPENCGA.LOCAL.FOLDERS." + bucketId)) {
-                throw new IllegalStateException("There is no path defined for the requested bucket ID");
+                throw new IllegalStateException("There is no path defined for the requested folder ID");
             }
             
             String bucketPathName = storageProperties.getProperty("OPENCGA.LOCAL.FOLDERS." + bucketId, "");
@@ -65,7 +71,12 @@ public class PublicStorageWSServer extends GenericWSServer {
             System.out.println("* Object path name = " + bucketPathName);
             FileUtils.checkFile(objectPath); // Check file exists and is readable
             
-            // Storage recibe los paths (URI) 
+            // 3: Check if the file format is allowed for that alias
+            if (!isFileFormatAllowed(Files.getFileExtension(objectPath.toString()), storageProperties)) {
+                throw new IllegalArgumentException("Reading this file format inside the requested folder is forbidden");
+            }
+            
+            // 4: Launch queries
             List<String> regions = Splitter.on(',').splitToList(regionStr);
             List<String> results = new ArrayList<>();
             
@@ -75,9 +86,45 @@ public class PublicStorageWSServer extends GenericWSServer {
             return createOkResponse(results.toString());
         } catch (Exception e) {
             logger.error(e.toString());
-            e.printStackTrace();
             return createErrorResponse(e.getMessage());
         }
     }
 
+
+    private boolean isBucketAllowed(Properties storageProperties) throws IllegalStateException {
+        if (!storageProperties.containsKey("OPENCGA.LOCAL.FOLDERS.ALLOWED")) {
+            throw new IllegalStateException("List of folders with read-access not available. Please check your \"storage.properties\" file.");
+        }
+        String[] buckets = storageProperties.getProperty("OPENCGA.LOCAL.FOLDERS.ALLOWED", "").split(",");
+        boolean bucketAllowed = false;
+        for (String bucket : buckets) {
+            if (bucket.equalsIgnoreCase(bucketId)) {
+                bucketAllowed = true;
+                break;
+            }
+        }
+        
+        return bucketAllowed;
+    }
+
+    private boolean isFileFormatAllowed(String fileFormat, Properties storageProperties) {
+        String[] fileFormatsAllowed = new String[0]; // No formats allowed if none specified
+        if (storageProperties.containsKey("OPENCGA.LOCAL.EXTENSIONS." + bucketId)) {
+            fileFormatsAllowed = storageProperties.getProperty("OPENCGA.LOCAL.EXTENSIONS." + bucketId, "").split(",");
+        } else if (storageProperties.containsKey("OPENCGA.LOCAL.EXTENSIONS.ALLOWED")) {
+            fileFormatsAllowed = storageProperties.getProperty("OPENCGA.LOCAL.EXTENSIONS.ALLOWED", "").split(",");
+        }
+        
+        logger.debug("Bucket extensions: " + storageProperties.getProperty("OPENCGA.LOCAL.EXTENSIONS." + bucketId));
+        logger.debug("Global extensions: " + storageProperties.getProperty("OPENCGA.LOCAL.EXTENSIONS.ALLOWED"));
+        
+        boolean formatAllowed = false;
+        for (String format : fileFormatsAllowed) {
+            if (format.equalsIgnoreCase(fileFormat)) {
+                formatAllowed = true;
+                break;
+            }
+        }
+        return formatAllowed;
+    }
 }
