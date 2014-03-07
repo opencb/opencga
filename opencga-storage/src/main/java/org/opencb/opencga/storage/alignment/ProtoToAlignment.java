11f4bd73e6d7516c2610aff52746d7bae794cc45
jose miguel mut <jm_mut@hotmail.com>
2014-03-07 11:47:11 +0100

    Added empty ProtoToAlignment.

---
+++ opencga-storage/src/main/java/org/opencb/opencga/storage/alignment/ProtoToAlignment.java
@@ -0,0 +1,21 @@
+package org.opencb.opencga.storage.alignment;
+
+import org.opencb.commons.bioformats.alignment.Alignment;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: josemi
+ * Date: 3/7/14
+ * Time: 11:29 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ProtoToAlignment {
+
+    public static Alignment toAlignment(AlignmentProto.AlignmentRecord alignmentRecord){
+        return null;
+    }
+
+    public static AlignmentProto.AlignmentRecord toProto(Alignment alignment){
+        return null;
+    }
+}
