c875320b656ebdcc2631f6ea8b1c60d556f24db2
frasator <frasator@gmail.com>
2014-06-20 16:45:46 +0200

    opencga-lib: added test resources root directory. Fixes #8

--- opencga-lib/src/test/java/org/opencb/opencga/lib/common/IOUtilsTest.java
+++ opencga-lib/src/test/java/org/opencb/opencga/lib/common/IOUtilsTest.java
@@ -5,19 +5,19 @@ import org.junit.Test;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.file.Paths;
-
-import static org.junit.Assert.*;
+import java.nio.file.Paths
 
 public class IOUtilsTest {
+
+    private static String inputFile = IOUtilsTest.class.getResource("/file.txt").getFile();
 
     @Test
     public void testHeadOffset() throws Exception {
 
-        InputStream is = IOUtils.headOffset(Paths.get("/home/fsalavert/file.txt"), 0, 1);
+        InputStream is = IOUtils.headOffset(Paths.get(inputFile), 0, 1);
         BufferedReader in = new BufferedReader(new InputStreamReader(is));
         String inputLine;
-        while ((inputLine = in.readLine()) != null){
+        while ((inputLine = in.readLine()) != null) {
             System.out.println(inputLine);
         }
         in.close();
@@ -27,11 +27,11 @@ public class IOUtilsTest {
     @Test
     public void testGrepFile() throws Exception {
 
-//        InputStream is = IOUtils.grepFile(Paths.get("/home/fsalavert/file.txt"), "^#a.*", false, false);
-        InputStream is = IOUtils.grepFile(Paths.get("/home/fsalavert/file.txt"), ".*", false, true);
+//        InputStream is = IOUtils.grepFile(Paths.get(inputFile), "^#a.*", false, false);
+        InputStream is = IOUtils.grepFile(Paths.get(inputFile), ".*", false, true);
         BufferedReader in = new BufferedReader(new InputStreamReader(is));
         String inputLine;
-        while ((inputLine = in.readLine()) != null){
+        while ((inputLine = in.readLine()) != null) {
             System.out.println(inputLine);
         }
         in.close();
