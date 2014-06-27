81a78ff683b8b91e9ea112aa7513b53d66aabff9
Cristina Yenyxe Gonzalez Garcia <cyenyxe@ebi.ac.uk>
2014-06-27 16:04:40 +0100

    Fixed compilation error in IOUtilsTest #8

--- opencga-lib/src/test/java/org/opencb/opencga/lib/common/IOUtilsTest.java
+++ opencga-lib/src/test/java/org/opencb/opencga/lib/common/IOUtilsTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.file.Paths
+import java.nio.file.Paths;
 
 public class IOUtilsTest {
 
