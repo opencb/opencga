d0d240457c24cc9ac767e46b580dfb03c2ad5f27
Jacobo Coll Moragón <jacobo167@gmail.com>
2015-01-15 11:21:16 +0000

    lib: generified Config class.

--- opencga-lib/src/main/java/org/opencb/opencga/lib/common/Config.java
+++ opencga-lib/src/main/java/org/opencb/opencga/lib/common/Config.java
@@ -7,34 +7,47 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 public class Config {
 
+    public static final String ACCOUNT_PROPERTIES = "account.properties";
+    public static final String CATALOG_PROPERTIES = "catalog.properties";
+    public static final String ANALYSIS_PROPERTIES = "analysis.properties";
+    public static final String STORAGE_PROPERTIES = "storage.properties";
     protected static Logger logger = LoggerFactory.getLogger(Config.class);
 
     private static String opencgaHome = System.getenv("OPENCGA_HOME");
 //    private static String opencgaLightHome;
     private static boolean log4jReady = false;
 
-    private static Properties accountProperties = null;
-    private static Properties analysisProperties = null;
+    private static Map<String, Properties> propertiesMap = new HashMap<>();
+
     private static Properties storageProperties = null;
-    private static Properties catalogProperties = null;
 
     private static long lastPropertyLoad = System.currentTimeMillis();
 
+    public static String getOpenCGAHome() {
+        return opencgaHome;
+    }
+
+    public static void setOpenCGAHome(String opencgaHome) {
+        Config.opencgaHome = opencgaHome;
+        propertiesMap.clear();
+    }
+
+    @Deprecated
     public static String getGcsaHome() {
         return opencgaHome;
     }
 
+    @Deprecated
     public static void setGcsaHome(String gcsaHome) {
         Config.opencgaHome = gcsaHome;
 
-        accountProperties = null;
-        analysisProperties = null;
         storageProperties = null;
-        catalogProperties = null;
 
         log4jReady = false;
 //        LogManager.resetConfiguration();
@@ -54,54 +67,43 @@ public class Config {
 //        }
 //    }
 
-    public static Properties getAccountProperties() {
-        // checkPopertiesStatus();
-        if (accountProperties == null) {
-            Path path = Paths.get(opencgaHome, "conf", "account.properties");
-            accountProperties = new Properties();
+
+    public static Properties getProperties(String fileName) {
+        return getProperties(fileName, null);
+    }
+
+    public static Properties getProperties(String fileName, Properties defaultProperties) {
+        if(!propertiesMap.containsKey(fileName)) {
+            Path path = Paths.get(opencgaHome, "conf", fileName);
+            Properties properties = new Properties(defaultProperties);
             try {
-                accountProperties.load(Files.newInputStream(path));
+                defaultProperties.load(Files.newInputStream(path));
             } catch (IOException e) {
-                logger.error("Failed to load account.properties: " + e.getMessage());
+                logger.error("Failed to load " + fileName + ": " + e.getMessage());
                 return null;
             }
+            propertiesMap.put(fileName, properties);
         }
-        return accountProperties;
+        return propertiesMap.get(fileName);
+    }
+
+    @Deprecated
+    public static Properties getAccountProperties() {
+        return getProperties(ACCOUNT_PROPERTIES);
     }
 
     public static Properties getCatalogProperties() {
-        // checkPopertiesStatus();
-        if (catalogProperties == null) {
-            Path path = Paths.get(opencgaHome, "conf", "catalog.properties");
-            catalogProperties = new Properties();
-            try {
-                catalogProperties.load(Files.newInputStream(path));
-            } catch (IOException e) {
-                logger.error("Failed to load catalog.properties: " + e.getMessage());
-                return null;
-            }
-        }
-        return catalogProperties;
+        return getProperties(CATALOG_PROPERTIES);
     }
 
     public static Properties getAnalysisProperties() {
-        // checkPopertiesStatus();
-        if (analysisProperties == null) {
-            Path path = Paths.get(opencgaHome, "conf", "analysis.properties");
-            analysisProperties = new Properties();
-            try {
-                analysisProperties.load(Files.newInputStream(path));
-            } catch (IOException e) {
-                logger.error("Failed to load analysis.properties: " + e.getMessage());
-                return null;
-            }
-        }
-        return analysisProperties;
+        return getProperties(ANALYSIS_PROPERTIES);
     }
 
     public static Properties getStorageProperties() {
-        return getStorageProperties(opencgaHome);
+        return getProperties(STORAGE_PROPERTIES);
     }
+    @Deprecated
     public static Properties getStorageProperties(String basePath) {
 //        opencgaLightHome = basePath;
 
@@ -116,7 +118,7 @@ public class Config {
 //		return storageProperties;
 
         if (storageProperties == null) {
-            Path path = Paths.get(basePath, "conf", "storage.properties");
+            Path path = Paths.get(basePath, "conf", STORAGE_PROPERTIES);
             storageProperties = new Properties();
             try {
                 storageProperties.load(Files.newInputStream(path));
@@ -144,8 +146,8 @@ public class Config {
 
     private static void checkPopertiesStatus() {
         if (System.currentTimeMillis() - lastPropertyLoad > 60000) {
-            loadProperties(accountProperties, Paths.get(opencgaHome, "conf", "account.properties"));
-            loadProperties(analysisProperties, Paths.get(opencgaHome, "conf", "analysis.properties"));
+//            loadProperties(accountProperties, Paths.get(opencgaHome, "conf", "account.properties"));
+//            loadProperties(analysisProperties, Paths.get(opencgaHome, "conf", "analysis.properties"));
             loadProperties(storageProperties, Paths.get(opencgaHome, "conf", "storage.properties"));
             lastPropertyLoad = System.currentTimeMillis();
         }
