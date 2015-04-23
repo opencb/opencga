67bda9ad513d2ff7f8ea0cc616e7333d320b5c07
Jacobo Coll Moragón <jacobo167@gmail.com>
2015-04-23 19:17:52 +0100

    app: Improved example test script.

--- opencga-app/build/examples/opencga-index.sh
+++ opencga-app/build/examples/opencga-index.sh
@@ -20,15 +20,16 @@ export uri_arg=""
 export study_uri=""
 
 export transform_file=false
-export input_file=false
+export pedigree_file=false
 export log_level=info
+export input_files=()
+export input_files_len=0
 
 function getFileId() {
     $OPENCGA_BIN files search --study-id $user@${project_alias}:${study_alias} -u $user -p $password --name $1 --output-format IDS --log-level ${log_level}
-#    $OPENCGA_BIN users list  -u $user -p $password -R | grep $1 | cut -d"(" -f2 | cut -d ")" -f1
 }
 
-while getopts "htu:i:l:U:" opt; do
+while getopts "htu:i:p:l:U:" opt; do
 	#echo $opt "=" $OPTARG
 	case "$opt" in
 	h)
@@ -52,8 +53,13 @@ while getopts "htu:i:l:U:" opt; do
 	    echo "Using password "$password
 	    ;;
 	i)
-	    input_file=$OPTARG
-	    echo "Using input file "$input_file
+	    echo "Using input file "$OPTARG
+	    input_files[$input_files_len]=$OPTARG
+	    input_files_len=$(( $input_files_len + 1 ))
+	    ;;
+	p)
+	    pedigree_file=$OPTARG
+	    echo "Using pedigree file " $OPTARG
 	    ;;
 	U)
 
@@ -75,14 +81,12 @@ while getopts "htu:i:l:U:" opt; do
 done
 
 
-if [ $input_file == false ]
-then
-	echo "No input file!"
+if [ $input_files_len == 0 ]; then
+	echo "No input files!"
 	exit 1
 fi
 
-if [ $log_level == "debug" ]
-then
+if [ $log_level == "debug" ]; then
 	set -x
 fi
 
@@ -94,29 +98,41 @@ $OPENCGA_BIN studies create -a ${study_alias}  -n "Phase 1" -u $user -p $passwor
 
 $OPENCGA_BIN users list -u $user -p $password -R
 
-$OPENCGA_BIN files create -P -s $user@${project_alias}:${study_alias} -u $user -p $password --input $input_file --bioformat VARIANT  --path data/vcfs/ --checksum --output-format IDS  --log-level ${log_level}
 
-VCF_FILE_ID=$(getFileId $(echo $input_file | rev | cut -d / -f1 | rev ) )
-
-echo "Added VCF file "$input_file" = "$VCF_FILE_ID
-
-$OPENCGA_BIN users list -u $user -p $password -R
-
-if [ "$transform_file" == "true" ]
-then
-	#Transform file
-	$OPENCGA_BIN files index -u $user -p $password --file-id $VCF_FILE_ID --output-format IDS --log-level ${log_level} --transform
-	$OPENCGA_BIN users list -u $user -p $password -R
-	$OPENCGA_BIN files info -u $user -p $password -id $VCF_FILE_ID --exclude projects.studies.files.attributes,projects.studies.files.sampleIds
-
-	#Load file
-	TRANSFORMED_VARIANTS_FILE_ID=$(getFileId "variants.json")
-	$OPENCGA_BIN files index -u $user -p $password --file-id $TRANSFORMED_VARIANTS_FILE_ID --log-level ${log_level} --load -Dannotate=false
-	$OPENCGA_BIN files info -u $user -p $password -id $VCF_FILE_ID --exclude projects.studies.files.attributes,projects.studies.files.sampleIds
+if [ $pedigree_file == false ]; then
+	echo "Do not load ped file"
 else
-	$OPENCGA_BIN files index -u $user -p $password --file-id $VCF_FILE_ID  --log-level ${log_level}
-	$OPENCGA_BIN files info -u $user -p $password -id $VCF_FILE_ID --exclude projects.studies.files.attributes,projects.studies.files.sampleIds
+	PEDIGREE_FILE_NAME=$(echo $pedigree_file | rev | cut -d / -f1 | rev )
+	$OPENCGA_BIN files create -P -s $user@${project_alias}:${study_alias} -u $user -p $password --input $pedigree_file --bioformat PEDIGREE  --path data/peds/ --checksum --output-format IDS  --log-level ${log_level}
+	$OPENCGA_BIN samples load --study-id $user@${project_alias}:${study_alias} -u $user -p $password --pedigree-id $(getFileId $PEDIGREE_FILE_NAME ) --log-level ${log_level}
 fi
+
+for input_file in ${input_files[@]}; do
+	echo "Indexing file $input_file"
+	$OPENCGA_BIN files create -P -s $user@${project_alias}:${study_alias} -u $user -p $password --input $input_file --bioformat VARIANT  --path data/vcfs/ --checksum --output-format IDS  --log-level ${log_level}
+
+	FILE_NAME=$(echo $input_file | rev | cut -d / -f1 | rev )
+	VCF_FILE_ID=$(getFileId $FILE_NAME )
+
+	echo "Added VCF file "$input_file" = "$VCF_FILE_ID
+
+	$OPENCGA_BIN users list -u $user -p $password -R
+
+	if [ "$transform_file" == "true" ]; then
+		#Transform file
+		$OPENCGA_BIN files index -u $user -p $password --file-id $VCF_FILE_ID --output-format IDS --log-level ${log_level} --transform
+		$OPENCGA_BIN users list -u $user -p $password -R
+		$OPENCGA_BIN files info -u $user -p $password -id $VCF_FILE_ID --exclude projects.studies.files.attributes,projects.studies.files.sampleIds
+
+		#Load file
+		TRANSFORMED_VARIANTS_FILE_ID=$(getFileId $FILE_NAME".variants.json")
+		$OPENCGA_BIN files index -u $user -p $password --file-id $TRANSFORMED_VARIANTS_FILE_ID --log-level ${log_level} --load -Dannotate=false
+		$OPENCGA_BIN files info -u $user -p $password -id $VCF_FILE_ID --exclude projects.studies.files.attributes,projects.studies.files.sampleIds
+	else
+		$OPENCGA_BIN files index -u $user -p $password --file-id $VCF_FILE_ID  --log-level ${log_level}
+		$OPENCGA_BIN files info -u $user -p $password -id $VCF_FILE_ID --exclude projects.studies.files.attributes,projects.studies.files.sampleIds
+	fi
+done
 
 $OPENCGA_BIN users list -u $user -p $password -R
 
