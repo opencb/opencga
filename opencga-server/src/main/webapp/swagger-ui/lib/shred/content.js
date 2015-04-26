39bc9bc97d06e6846eaf13b57a509996b6a918f8
imedina <igmecas@gmail.com>
2015-04-26 20:58:29 +0100

    License changed, Apache License v2 is now used

--- opencga-server/src/main/webapp/swagger-ui/lib/shred/content.js
+++ opencga-server/src/main/webapp/swagger-ui/lib/shred/content.js
@@ -1,4 +1,20 @@
 
+/*
+ * Copyright 2015 OpenCB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 // The purpose of the `Content` object is to abstract away the data conversions
 // to and from raw content entities as strings. For example, you want to be able
 // to pass in a Javascript object and have it be automatically converted into a
