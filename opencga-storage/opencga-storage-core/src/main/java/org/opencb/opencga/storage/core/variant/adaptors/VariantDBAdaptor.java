fcf5fb538dca3a9c509a42b55fe2525252b7b239
Cristina Yenyxe Gonzalez Garcia <cyenyxe@ebi.ac.uk>
2015-06-02 16:18:09 +0100

    New option 'sort' allows to optionally sort the results returned from a query by region (default: no sorting)

--- opencga-storage/opencga-storage-core/src/main/java/org/opencb/opencga/storage/core/variant/adaptors/VariantDBAdaptor.java
+++ opencga-storage/opencga-storage-core/src/main/java/org/opencb/opencga/storage/core/variant/adaptors/VariantDBAdaptor.java
@@ -5,9 +5,7 @@ import java.util.*;
 import org.opencb.biodata.models.feature.Region;
 import org.opencb.biodata.models.variant.Variant;
 import org.opencb.biodata.models.variant.annotation.VariantAnnotation;
-import org.opencb.biodata.models.variant.stats.VariantStats;
 import org.opencb.commons.io.DataWriter;
-import org.opencb.datastore.core.ObjectMap;
 import org.opencb.datastore.core.QueryOptions;
 import org.opencb.datastore.core.QueryResult;
 import org.opencb.opencga.storage.core.variant.stats.VariantStatsWrapper;
@@ -43,6 +41,7 @@ public interface VariantDBAdaptor extends Iterable<Variant> {
     public static final String PROTEIN_SUBSTITUTION = "protein_substitution";
     public static final String CONSERVED_REGION = "conserved_region";
     public static final String MERGE = "merge";
+    public static final String SORT = "sort";
 
     static public class QueryParams {
         public static final Set<String> acceptedValues;
