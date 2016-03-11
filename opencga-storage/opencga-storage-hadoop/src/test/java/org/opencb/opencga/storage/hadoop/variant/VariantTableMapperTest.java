8632aae23bb3b290749d73f917d42bf31c574fdd
Matthias Haimel <mh719@cam.ac.uk>
2016-03-11 16:19:24 +0000

    hadoop test: extract helper classes for testing

--- opencga-storage/opencga-storage-hadoop/src/test/java/org/opencb/opencga/storage/hadoop/variant/VariantTableMapperTest.java
+++ opencga-storage/opencga-storage-hadoop/src/test/java/org/opencb/opencga/storage/hadoop/variant/VariantTableMapperTest.java
@@ -1,16 +1,13 @@
 package org.opencb.opencga.storage.hadoop.variant;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
-import java.net.URI;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.client.Result;
-import org.apache.hadoop.hbase.client.ResultScanner;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -18,56 +15,21 @@ import org.opencb.biodata.models.variant.StudyEntry;
 import org.opencb.biodata.models.variant.Variant;
 import org.opencb.biodata.models.variant.VariantSource;
 import org.opencb.biodata.models.variant.avro.FileEntry;
-import org.opencb.biodata.models.variant.avro.VariantType;
 import org.opencb.datastore.core.ObjectMap;
 import org.opencb.opencga.storage.core.StudyConfiguration;
 import org.opencb.opencga.storage.core.config.StorageEngineConfiguration;
-import org.opencb.opencga.storage.core.config.StorageEtlConfiguration;
-import org.opencb.opencga.storage.core.variant.VariantStorageManager;
 import org.opencb.opencga.storage.core.variant.VariantStorageManagerTestUtils;
-import org.opencb.opencga.storage.core.variant.VariantStorageManagerTestUtils.ETLResult;
-import org.opencb.opencga.storage.core.variant.annotation.VariantAnnotationManager;
 import org.opencb.opencga.storage.hadoop.utils.HBaseManager;
-import org.opencb.opencga.storage.hadoop.variant.HadoopVariantStorageManager;
 import org.opencb.opencga.storage.hadoop.variant.index.HBaseToVariantConverter;
 
 public class VariantTableMapperTest extends HadoopVariantStorageManagerTestUtils {
 
-    public VariantSource loadFile(String resourceName, int fileId, StudyConfiguration studyConfiguration, Map<? extends String, ?> otherParams) throws Exception {
-        HadoopVariantStorageManager variantStorageManager = getVariantStorageManager();
-        URI fileInputUri = VariantStorageManagerTestUtils.getResourceUri(resourceName);
-
-        ObjectMap params = new ObjectMap(VariantStorageManager.Options.TRANSFORM_FORMAT.key(), "avro")
-                .append(VariantStorageManager.Options.STUDY_CONFIGURATION.key(), studyConfiguration)
-                .append(VariantStorageManager.Options.STUDY_ID.key(), studyConfiguration.getStudyId())
-                .append(VariantStorageManager.Options.DB_NAME.key(), DB_NAME)
-                .append(VariantStorageManager.Options.ANNOTATE.key(), false)
-                .append(VariantAnnotationManager.SPECIES, "hsapiens")
-                .append(VariantAnnotationManager.ASSEMBLY, "GRc37")
-                .append(VariantStorageManager.Options.CALCULATE_STATS.key(), false)
-                .append(HadoopVariantStorageManager.HADOOP_LOAD_ARCHIVE, true)
-                .append(HadoopVariantStorageManager.HADOOP_LOAD_VARIANT, true);
-        if (otherParams != null) {
-            params.putAll(otherParams);
-        }
-
-        if (fileId > 0) {
-            params.append(VariantStorageManager.Options.FILE_ID.key(), fileId);
-        }
-
-        ETLResult etlResult = runETL(variantStorageManager, fileInputUri, outputUri, params, params, params,
-                params, params, params, params, true, true, true);
-
-        return variantStorageManager.readVariantSource(etlResult.transformResult, new ObjectMap());
-    }
-
-    public VariantSource loadFile(String resourceName, int fileId, StudyConfiguration studyConfiguration) throws Exception {
-        return loadFile(resourceName, fileId, studyConfiguration, null);
-    }
-    public VariantSource loadFile(String resourceName, StudyConfiguration studyConfiguration, Map<? extends String, ?> otherParams) throws Exception {
-        return loadFile(resourceName, -1, studyConfiguration, otherParams);
-    }
     private VariantHadoopDBAdaptor dbAdaptor;
+
+    private VariantSource loadFile(String resource, StudyConfiguration studyConfiguration, Map<? extends String, ?> map) throws Exception {
+        return VariantHbaseTestUtils.loadFile(getVariantStorageManager(), DB_NAME, outputUri, resource, studyConfiguration, map);
+    }
+
     @Before
     public void setUp() throws Exception {
         clearDB(DB_NAME);
