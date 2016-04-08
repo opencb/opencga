352532d9ed862d54fa2fa9456b6f01243ba77ea1
pfurio <pedrofurio@gmail.com>
2016-04-08 18:12:48 +0100

    web: basic structure of the web application

--- opencga-web/src/config.js
+++ opencga-web/src/config.js
@@ -14,5 +14,7 @@
  * limitations under the License.
  */
 
-var OPENCGA_HOSTS = "bioinfodev.hpc.cam.ac.uk/cellbase-dev-v4.0";
+var OPENCGA_HOST = "http://localhost:8080/opencga";
 var OPENCGA_VERSION = "v1";
+var OPENCGA_COOKIE_SESSION_ID = "opencga_sId";
+var OPENCGA_COOKIE_USER_NAME = "opencga_userId"
