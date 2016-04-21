20fcfc3048498e8b87bb5939f5a41b3ab8aacb05
Jacobo Coll Moragón <jacobo167@gmail.com>
2016-04-21 15:01:45 +0100

    daemon: Add RollingFileAppender to the Daemon

--- opencga-app/app/bin/opencga-start.sh
+++ opencga-app/app/bin/opencga-start.sh
@@ -4,7 +4,7 @@ PRGDIR=`dirname "$0"`
 BASEDIR=`cd "$PRGDIR/.." >/dev/null; pwd`
 OPENCGA_DAEMON_BIN=$BASEDIR'/bin/opencga-daemon.sh'
 
-mkdir -p $BASEDIR/log
+mkdir -p $BASEDIR/logs
 export OPENCGA_HOME=$BASEDIR
 
-exec $OPENCGA_DAEMON_BIN $@ &>> $BASEDIR/logs/daemon.log &
+exec $OPENCGA_DAEMON_BIN $@ &>> /dev/null &
--- opencga-app/app/bin/opencga-stop.sh
+++ opencga-app/app/bin/opencga-stop.sh
@@ -5,3 +5,5 @@ BASEDIR=`cd "$PRGDIR/.." >/dev/null; pwd`
 daemon_port=`grep "OPENCGA.APP.DAEMON.PORT" ${BASEDIR}/conf/daemon.properties | cut -d "=" -f2`
 
 curl "http://localhost:${daemon_port}/opencga/rest/admin/stop"
+
+echo ""
