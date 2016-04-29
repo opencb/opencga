6ad22e8f015bd2b2c5d84e5b4eb8dd7d76ba8f45
imedina <igmecas@gmail.com>
2016-04-29 13:13:34 +0100

    storage: remove old datastore dependencies

--- opencga-server/src/main/java/org/opencb/opencga/server/grpc/GenericGrpcService.java
+++ opencga-server/src/main/java/org/opencb/opencga/server/grpc/GenericGrpcService.java
@@ -16,8 +16,8 @@
 
 package org.opencb.opencga.server.grpc;
 
-import org.opencb.datastore.core.Query;
-import org.opencb.datastore.core.QueryOptions;
+import org.opencb.commons.datastore.core.Query;
+import org.opencb.commons.datastore.core.QueryOptions;
 import org.opencb.opencga.server.grpc.GenericServiceModel.Request;
 import org.opencb.opencga.storage.core.StorageManagerFactory;
 import org.opencb.opencga.storage.core.config.StorageConfiguration;
