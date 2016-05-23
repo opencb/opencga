cedadfcb351c2f88ec41ab3629958b5d7a9b3ca0
imedina <igmecas@gmail.com>
2016-05-23 16:05:06 +0100

    app: make analysis.sh to use the .opencga stored sessions

--- opencga-app/src/main/java/org/opencb/opencga/app/cli/analysis/AnalysisStorageCommandExecutor.java
+++ opencga-app/src/main/java/org/opencb/opencga/app/cli/analysis/AnalysisStorageCommandExecutor.java
@@ -1,5 +1,6 @@
 package org.opencb.opencga.app.cli.analysis;
 
+import org.apache.commons.lang3.StringUtils;
 import org.opencb.commons.datastore.core.Query;
 import org.opencb.commons.datastore.core.QueryOptions;
 import org.opencb.opencga.app.cli.GeneralCliOptions;
