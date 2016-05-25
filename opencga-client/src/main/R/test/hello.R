6b406fa0a8b420c0eec021de156831fae7c10cdc
melsiddieg <melsiddieg@gmail.com>
2016-05-25 18:38:48 +0100

    some tests

--- opencga-client/src/main/R/test/hello.R
+++ opencga-client/src/main/R/test/hello.R
@@ -1,8 +1,8 @@
 base <- "http://localhost:8080/opencga/webservices/rest/v1"
-l1 <- OpencgaLogin(baseurl ="http://localhost:8080/opencga/webservices/rest/v1",
+cga <- OpencgaLogin(baseurl ="http://localhost:8080/opencga/webservices/rest/v1",
                    userid = "melsiddieg", passwd = '1234')
 cga <- Opencga(baseurl = "http://localhost:8080/opencga/webservices/rest/v1",
-               userID = l1$userId, sessionID = l1$sessionId)
+               userID = cga$userId, sessionID = cga$sessionId)
 cga
 res1 <- OpencgaStudy(object = cga, id = 3, action = "files")
 library(rlist)
@@ -10,34 +10,32 @@ library(pipeR)
 # vfl%>>%
 #   list.filter(annotation$consequenceTypes$geneName=="NOC2L")%>>%length
 cga <- OpencgaLogin(baseurl = base,interactive = T)
-vardata <-OpencgaStudy(object = l1, id = 3, action = "variants")
+vardata <-OpencgaStudy(object = cga, id = 3, action = "variants")
 varainst <- vardata$data
 test <- fromJSON(vardata$url)
 str(vardata,1)
-sampledata <- OpencgaStudy(object = l1, id = 3, action = "samples")
-studyFiles <- OpencgaStudy(object = l1, id = 3, action = "files")
+sampledata <- OpencgaStudy(object = cga, id = 3, action = "samples")
+studyFiles <- OpencgaStudy(object = cga, id = 3, action = "files")
 sams <- sampledata$num_results
 str(sams,1)
-studyInfo <- OpencgaStudy(object = l1, id = 3, action = "info")
-userinfo <- OpencgaUser(object = l1, id = "melsiddieg", action = "info")
+studyInfo <- OpencgaStudy(object = cga, id = 3, action = "info")
+userinfo <- OpencgaUser(object = cga, id = "melsiddieg", action = "info")
 
 ### fetch variants from a file
 
-test <- OpencgaFiles(object = l1, id = 32, action = "variants")
+test <- OpencgaFiles(object = cga, id = 32, action = "variants")
 system.time({
-  test2 <- OpencgaFiles(object = l1, id = 32, action = "variants")
+  test2 <- OpencgaFiles(object = cga, id = 32, action = "variants")
 })
 
-files <- OpencgaStudy(object = l1, id = 3, action = "files")
-info <- OpencgaStudy(object = l1, id = 3, action = "info")
-samples <- OpencgaStudy(object = l1, id = 3, action = "samples")
+files <- OpencgaStudy(object = cga, id = 3, action = "files")
+info <- OpencgaStudy(object = cga, id = 3, action = "info")
+samples <- OpencgaStudy(object = cga, id = 3, action = "samples")
 
-filvar <- OpencgaFiles(object = l1, id = 32, action = "variants", params = filt)
+filvar <- OpencgaFiles(object = cga, id = 32, action = "variants", params = filt)
 
-
-
-
-
+summ <- OpencgaStudy(cga, id = 3, action = "summary")
+jobs <-
 
 
 
@@ -47,6 +45,6 @@ filvar <- OpencgaFiles(object = l1, id = 32, action = "variants", params = filt)
 
 
 p <- profvis({
-  test3 <- OpencgaFiles(object = l1, id = 32, action = "variants")
+  test3 <- OpencgaFiles(object = cga, id = 32, action = "variants")
 })
 p
