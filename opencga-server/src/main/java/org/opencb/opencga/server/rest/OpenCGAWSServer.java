fdc1f08dc7f8c3ef17d8925d2909f11a85a0c938
imedina <igmecas@gmail.com>
2016-07-08 22:34:27 +0100

    Print server-sed request headers

--- opencga-server/src/main/java/org/opencb/opencga/server/rest/OpenCGAWSServer.java
+++ opencga-server/src/main/java/org/opencb/opencga/server/rest/OpenCGAWSServer.java
@@ -443,12 +443,15 @@ public class OpenCGAWSServer {
     }
 
     protected Response buildResponse(Response.ResponseBuilder responseBuilder) {
-        return responseBuilder
+        Response response = responseBuilder
                 .header("Access-Control-Allow-Origin", "*")
                 .header("Access-Control-Allow-Headers", "x-requested-with, content-type")
                 .header("Access-Control-Allow-Credentials", "true")
                 .header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
                 .build();
+
+        System.out.println("response = " + response.getHeaders());
+        return response;
     }
 
 }
