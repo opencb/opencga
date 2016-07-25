272721b81a7ca27a0f9e645850c7e46937d80415
imedina <igmecas@gmail.com>
2016-07-25 10:43:00 +0100

    server: CORS cleanups

--- opencga-server/src/main/java/org/opencb/opencga/server/rest/OpenCGAWSServer.java
+++ opencga-server/src/main/java/org/opencb/opencga/server/rest/OpenCGAWSServer.java
@@ -443,15 +443,12 @@ public class OpenCGAWSServer {
     }
 
     protected Response buildResponse(Response.ResponseBuilder responseBuilder) {
-        Response response = responseBuilder
-//                .header("Access-Control-Allow-Origin", "*")
-//                .header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type")
-//                .header("Access-Control-Allow-Credentials", "true")
-//                .header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+        return responseBuilder
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Headers", "x-requested-with, content-type")
+                .header("Access-Control-Allow-Credentials", "true")
+                .header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
                 .build();
-
-        System.out.println("response = " + response.getHeaders());
-        return response;
     }
 
 }
