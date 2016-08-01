8f933c4f98b4ab2504febbe42d10c258e9929d1a
Jacobo Coll Moragón <jacobo167@gmail.com>
2016-08-01 17:39:48 +0100

    storage: Add avro.jar to the HADOOP_CLASSPATH.
    Hadoop clusters use to use avro 1.7.4 instead of 1.7.7
    With the old version, generated code in biodata-models cant be used due to:
        java.lang.NoSuchMethodError: org.apache.avro.Schema$Parser.parse(Ljava/lang/String;[Ljava/lang/String;)Lorg/apache/avro/Schema;

--- opencga-app/app/bin/opencga-env.sh
+++ opencga-app/app/bin/opencga-env.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Variable BASEDIR is defined by the main script
+
 #Set log4j properties file
 export JAVA_OPTS="${JAVA_OPTS} -Dlog4j.configuration=file:${BASEDIR}/conf/log4j.properties"
 
@@ -9,6 +11,6 @@ export JAVA_OPTS="${JAVA_OPTS} -Dlog4j.configuration=file:${BASEDIR}/conf/log4j.
 # Check if the command hbase exists
 if `command -v hbase >/dev/null 2>&1` ; then
     export CLASSPATH_PREFIX=${CLASSPATH_PREFIX}:`hbase classpath | tr ":" "\n" | grep "conf" | tr "\n" ":"`
-    export HADOOP_CLASSPATH=${BASEDIR}/libs/protobuf-java-*:${CLASSPATH_PREFIX}
+    export HADOOP_CLASSPATH=${BASEDIR}/libs/protobuf-java-*:${BASEDIR}/libs/avro-*:${CLASSPATH_PREFIX}
     export HADOOP_USER_CLASSPATH_FIRST=true
 fi
