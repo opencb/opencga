357f2c8b49f54198f49681fc3a97186f26a07652
imedina <igmecas@gmail.com>
2016-08-12 17:28:21 +0100

    client: fix a NPE when logging out

--- opencga-client/src/main/java/org/opencb/opencga/client/rest/AbstractParentClient.java
+++ opencga-client/src/main/java/org/opencb/opencga/client/rest/AbstractParentClient.java
@@ -146,6 +146,10 @@ public abstract class AbstractParentClient<T, A> {
     protected <T> QueryResponse<T> execute(String category1, String id1, String category2, String id2, String action,
                                            Map<String, Object> params, String method, Class<T> clazz) throws IOException {
 
+        if (params == null) {
+            params = new HashMap<>();
+        }
+
         // Remove null or empty params
         for (Map.Entry<String, Object> param : params.entrySet()) {
             Object value = param.getValue();
