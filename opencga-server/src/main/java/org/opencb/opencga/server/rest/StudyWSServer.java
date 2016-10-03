f12378eb24c11e5f260fcde295229fdfc70262f7
pfurio <pedrofurio@gmail.com>
2016-10-03 13:31:54 +0100

    webservice: Fix issue with parameter name and description in study search ws. Closes #391.

--- opencga-server/src/main/java/org/opencb/opencga/server/rest/StudyWSServer.java
+++ opencga-server/src/main/java/org/opencb/opencga/server/rest/StudyWSServer.java
@@ -93,7 +93,7 @@ public class StudyWSServer extends OpenCGAWSServer {
                                   @ApiParam(value = "projectId") @QueryParam("projectId") String projectId,
                                   @ApiParam(value = "name") @QueryParam("name") String name,
                                   @ApiParam(value = "alias") @QueryParam("alias") String alias,
-                                  @ApiParam(value = "type") @QueryParam("Comma separated list of type") String type,
+                                  @ApiParam(value = "Comma separated list of types") @QueryParam("type") String type,
                                   @ApiParam(value = "creationDate") @QueryParam("creationDate") String creationDate,
                                   @ApiParam(value = "status") @QueryParam("status") String status,
                                   @ApiParam(value = "attributes") @QueryParam("attributes") String attributes,
