b9f7823c4509ac2529e06f13cb1c46fb98539298
Matthias Haimel <mh719@cam.ac.uk>
2016-12-21 13:41:41 +0000

    hadoop export: also write studyconfiguration file

--- opencga-storage/opencga-storage-hadoop/opencga-storage-hadoop-core/src/main/java/org/opencb/opencga/storage/hadoop/variant/exporters/VariantTableExportDriver.java
+++ opencga-storage/opencga-storage-hadoop/opencga-storage-hadoop-core/src/main/java/org/opencb/opencga/storage/hadoop/variant/exporters/VariantTableExportDriver.java
@@ -1,18 +1,24 @@
 package org.opencb.opencga.storage.hadoop.variant.exporters;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.avro.mapreduce.AvroJob;
 import org.apache.avro.mapreduce.AvroKeyOutputFormat;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.mapreduce.TableMapper;
 import org.apache.hadoop.io.compress.GzipCodec;
 import org.apache.hadoop.mapreduce.Job;
 import org.opencb.biodata.models.variant.avro.VariantAvro;
+import org.opencb.opencga.storage.core.exceptions.StorageManagerException;
+import org.opencb.opencga.storage.core.metadata.StudyConfiguration;
 import org.opencb.opencga.storage.hadoop.variant.AbstractAnalysisTableDriver;
 
 import java.io.IOException;
+import java.nio.file.Paths;
 
 /**
  * Created by mh719 on 21/11/2016.
@@ -49,6 +55,22 @@ public class VariantTableExportDriver extends AbstractAnalysisTableDriver {
         AvroJob.setOutputKeySchema(job, VariantAvro.getClassSchema()); // Set schema
         AvroKeyOutputFormat.setOutputCompressorClass(job, GzipCodec.class); // compression
         job.setNumReduceTasks(0);
+    }
+
+    @Override
+    protected void postExecution(boolean succeed) throws IOException, StorageManagerException {
+        super.postExecution(succeed);
+        StudyConfiguration studyConfiguration = loadStudyConfiguration();
+        writeMetadata(studyConfiguration, this.outFile + ".studyConfiguration");
+    }
+
+    protected void writeMetadata(StudyConfiguration studyConfiguration, String output) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Path path = new Path(output);
+        FileSystem fs = FileSystem.get(getConf());
+        try(FSDataOutputStream fos = fs.create(path)){
+            objectMapper.writeValue(fos, studyConfiguration);
+        }
     }
 
     public static void main(String[] args) throws Exception {
