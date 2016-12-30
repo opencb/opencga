14e5aad7bc71877e7626b097aee72f962f64de99
pfurio <pedrofurio@gmail.com>
2016-12-30 11:53:10 +0000

    app: Changed app names shown when executing the binaries

--- opencga-app/src/main/java/org/opencb/opencga/app/cli/analysis/AnalysisCliOptionsParser.java
+++ opencga-app/src/main/java/org/opencb/opencga/app/cli/analysis/AnalysisCliOptionsParser.java
@@ -979,7 +979,7 @@ public class AnalysisCliOptionsParser {
         String parsedCommand = getCommand();
         if (parsedCommand.isEmpty()) {
             System.err.println("");
-            System.err.println("Program:     OpenCGA (OpenCB)");
+            System.err.println("Program:     OpenCGA Analysis (OpenCB)");
             System.err.println("Version:     " + GitRepositoryState.get().getBuildVersion());
             System.err.println("Git commit:  " + GitRepositoryState.get().getCommitId());
             System.err.println("Description: Big Data platform for processing and analysing NGS data");
