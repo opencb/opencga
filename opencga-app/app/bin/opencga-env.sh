319599302c49301f1170407719a90a828646dc4a
Matthias Haimel <mh719@cam.ac.uk>
2017-01-12 21:47:09 +0000

    hbase: add phoenix library to HADOOP_CLASSPATH

--- opencga-app/app/bin/opencga-env.sh
+++ opencga-app/app/bin/opencga-env.sh
@@ -7,12 +7,17 @@ export JAVA_OPTS="${JAVA_OPTS} -Dlog4j.configuration=file:${BASEDIR}/conf/log4j.
 
 # export OPENCGA_HOME=${BASEDIR}
 
+phoenix=""
+if `command -v phoenix_utils.py > /dev/null 2>&1`; then
+    phoenix=$(phoenix_utils.py | grep phoenix_client_jar | cut -f 2 -d " ")
+fi
+
 # see http://stackoverflow.com/questions/592620/check-if-a-program-exists-from-a-bash-script
 # Check if the command hbase exists
 if `command -v hbase >/dev/null 2>&1` ; then
     export CLASSPATH_PREFIX=${CLASSPATH_PREFIX}:`hbase classpath | tr ":" "\n" | grep "conf" | tr "\n" ":"`
     proto=$(find ${BASEDIR}/libs/ -name "protobuf-java-*.jar" | tr "\n" ":")
     avro=$(find ${BASEDIR}/libs/ -name "avro-*.jar" | tr "\n" ":")
-    export HADOOP_CLASSPATH="${proto}:${avro}:${CLASSPATH_PREFIX}"
+    export HADOOP_CLASSPATH="${phoenix}:${proto}:${avro}:${CLASSPATH_PREFIX}"
     export HADOOP_USER_CLASSPATH_FIRST=true
 fi
