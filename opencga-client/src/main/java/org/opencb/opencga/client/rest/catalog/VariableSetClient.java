c885c6f6fb927922bf5903e4677c185b4108cdc4
pfurio <pedrofurio@gmail.com>
2017-03-24 17:43:45 +0000

    client: Fixed variable set base url to point properly to the updated variable set entrypoint

--- opencga-client/src/main/java/org/opencb/opencga/client/rest/catalog/VariableSetClient.java
+++ opencga-client/src/main/java/org/opencb/opencga/client/rest/catalog/VariableSetClient.java
@@ -29,7 +29,7 @@ import java.io.IOException;
  */
 public class VariableSetClient extends CatalogClient<VariableSet, VariableSet> {
 
-    private static final String VARIABLES_URL = "variableSet";
+    private static final String VARIABLES_URL = "variableset";
 
     public VariableSetClient(String sessionId, ClientConfiguration configuration, String userId) {
         super(userId, sessionId, configuration);
