opencb/opencga

commit 0cb72f3925c8409a6abc7f7b3eb3ab04970f67a5

server: Fixed behaviour in ws returning also empty or null parameters. Closes #569

opencga-catalog/src/main/java/org/opencb/opencga/catalog/managers/FileManager.java
@@ -550,9 +550,7 @@ public class FileManager extends AbstractManager implements IFileManager {
             if (!FileIndex.IndexStatus.isValid(newStatus)) {
                 throw new CatalogException("The status " + newStatus + " is not a valid status.");
             } else {
-                index.getStatus().setName(newStatus);
-                index.getStatus().setCurrentDate();
-                index.getStatus().setMessage(message);
+                index.setStatus(new FileIndex.IndexStatus(newStatus, message));
             }
         } else {
             index = new FileIndex(userId, TimeUtils.getTime(), new FileIndex.IndexStatus(newStatus), -1, new ObjectMap());

opencga-catalog/src/main/java/org/opencb/opencga/catalog/models/Cohort.java
@@ -45,7 +45,6 @@ public class Cohort extends Annotable<CohortAclEntry> {
 
 
     public Cohort() {
-        this(null, null, null, null, new LinkedList<>(), new HashMap<>());
     }
 
     public Cohort(String name, Study.Type type, String creationDate, String description, List<Long> samples,

opencga-catalog/src/main/java/org/opencb/opencga/catalog/models/File.java
@@ -77,7 +77,6 @@ public class File extends AbstractAcl<FileAclEntry> {
 
 
     public File() {
-        this(null, null, null, null, null, null, new FileStatus(), -1);
     }
 
     public File(String name, Type type, Format format, Bioformat bioformat, String path, String description, FileStatus status,

opencga-catalog/src/main/java/org/opencb/opencga/catalog/models/FileIndex.java
@@ -16,7 +16,6 @@
 
 package org.opencb.opencga.catalog.models;
 
-import java.util.Collections;
 import java.util.Map;
 
 /**
@@ -39,7 +38,6 @@ public class FileIndex {
 
 
     public FileIndex() {
-        this(null, null, new IndexStatus(), -1, Collections.emptyMap());
     }
 
     public FileIndex(String userId, String creationDate, IndexStatus status, long jobId, Map<String, Object> attributes) {

opencga-catalog/src/main/java/org/opencb/opencga/catalog/models/Group.java
@@ -16,7 +16,6 @@
 
 package org.opencb.opencga.catalog.models;
 
-import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -37,7 +36,6 @@ public class Group {
     private List<String> userIds;
 
     public Group() {
-        this(null, new LinkedList<>());
     }
 
     public Group(String name, List<String> userIds) {

opencga-catalog/src/main/java/org/opencb/opencga/catalog/models/Individual.java
@@ -67,7 +67,6 @@ public class Individual extends Annotable<IndividualAclEntry> {
     }
 
     public Individual() {
-        this(-1, null, -1, -1, null, null, null, new Species(), new Population(), new LinkedList<>(), new HashMap<>());
     }
 
     public Individual(long id, String name, long fatherId, long motherId, String family, Sex sex, String ethnicity, Species species,

opencga-catalog/src/main/java/org/opencb/opencga/catalog/models/Job.java
@@ -96,7 +96,6 @@ public class Job extends AbstractAcl<JobAclEntry> {
 
 
     public Job() {
-        this(null, null, null, null, null, -1, new LinkedList<>());
     }
 
     public Job(String name, String userId, String executable, Type type, List<Long> input, List<Long> output, long outDirId,

opencga-catalog/src/main/java/org/opencb/opencga/catalog/models/Sample.java
@@ -41,7 +41,6 @@ public class Sample extends Annotable<SampleAclEntry> {
 
 
     public Sample() {
-        this(-1, null, null, new Individual(), null);
     }
 
     public Sample(long id, String name, String source, Individual individual, String description) {

commit 2b40a22fc016a27098e24b0714d85f273b4243c1

catalog-app-storage: Fix some errors caused by changes made in commit 0cb72f3

opencga-app/src/main/java/org/opencb/opencga/app/cli/main/io/TextOutputWriter.java
@@ -232,9 +232,13 @@ public class TextOutputWriter extends AbstractOutputWriter {
     private void printFiles(List<File> files, StringBuilder sb, String format) {
         // # name	type	format	bioformat	description	path	id	status	size	index status	related files   samples
         for (File file : files) {
+            String indexStatus = "NA";
+            if (file.getIndex() != null && file.getIndex().getStatus() != null && file.getIndex().getStatus().getName() != null) {
+                indexStatus = file.getIndex().getStatus().getName();
+            }
             sb.append(String.format("%s%s\t%s\t%s\t%s\t%s\t%s\t%s\t%d\t%s\t%d\t%s\t%s\t%s\n", format, file.getName(), file.getType(),
                     file.getFormat(), file.getBioformat(), file.getDescription(), file.getPath(), file.getUri(), file.getId(),
-                    file.getStatus().getName(), file.getSize(), file.getIndex() != null ? file.getIndex().getStatus().getName() : "NA",
+                    file.getStatus().getName(), file.getSize(), indexStatus,
                     StringUtils.join(file.getRelatedFiles().stream().map(File.RelatedFile::getFileId).collect(Collectors.toList()), ", "),
                     StringUtils.join(file.getSampleIds().stream().collect(Collectors.toList()), ", ")));
         }

opencga-catalog/src/main/java/org/opencb/opencga/catalog/managers/FileManager.java
@@ -383,11 +383,11 @@ public class FileManager extends AbstractManager implements IFileManager {
             Query query = new Query()
                     .append(FileDBAdaptor.QueryParams.STUDY_ID.key(), studyId)
                     .append(FileDBAdaptor.QueryParams.PATH.key(), variantPathName)
-                    .append(FileDBAdaptor.QueryParams.BIOFORMAT.key(), File.Bioformat.VARIANT)
-                    .append(FileDBAdaptor.QueryParams.INDEX_STATUS_NAME.key(), Arrays.asList(
-                            FileIndex.IndexStatus.NONE, FileIndex.IndexStatus.TRANSFORMING, FileIndex.IndexStatus.INDEXING,
-                            FileIndex.IndexStatus.READY
-                    ));
+                    .append(FileDBAdaptor.QueryParams.BIOFORMAT.key(), File.Bioformat.VARIANT);
+//                    .append(FileDBAdaptor.QueryParams.INDEX_STATUS_NAME.key(), Arrays.asList(
+//                            FileIndex.IndexStatus.NONE, FileIndex.IndexStatus.TRANSFORMING, FileIndex.IndexStatus.INDEXING,
+//                            FileIndex.IndexStatus.READY
+//                    ));
 
             QueryResult<File> fileQueryResult = fileDBAdaptor.get(query, new QueryOptions());
 
@@ -398,13 +398,28 @@ public class FileManager extends AbstractManager implements IFileManager {
                 query = new Query()
                         .append(FileDBAdaptor.QueryParams.STUDY_ID.key(), studyId)
                         .append(FileDBAdaptor.QueryParams.NAME.key(), variantFileName)
-                        .append(FileDBAdaptor.QueryParams.BIOFORMAT.key(), File.Bioformat.VARIANT)
-                        .append(FileDBAdaptor.QueryParams.INDEX_STATUS_NAME.key(), Arrays.asList(
-                                FileIndex.IndexStatus.NONE, FileIndex.IndexStatus.TRANSFORMING, FileIndex.IndexStatus.INDEXING,
-                                FileIndex.IndexStatus.READY
-                        ));
+                        .append(FileDBAdaptor.QueryParams.BIOFORMAT.key(), File.Bioformat.VARIANT);
+//                        .append(FileDBAdaptor.QueryParams.INDEX_STATUS_NAME.key(), Arrays.asList(
+//                                FileIndex.IndexStatus.NONE, FileIndex.IndexStatus.TRANSFORMING, FileIndex.IndexStatus.INDEXING,
+//                                FileIndex.IndexStatus.READY
+//                        ));
 //                        .append(CatalogFileDBAdaptor.QueryParams.INDEX_TRANSFORMED_FILE.key(), null);
                 fileQueryResult = fileDBAdaptor.get(query, new QueryOptions());
+            }
+
+            if (fileQueryResult.getNumResults() > 0) {
+                List<File> fileList = new ArrayList<>(fileQueryResult.getNumResults());
+                List<String> acceptedStatus = Arrays.asList(FileIndex.IndexStatus.NONE, FileIndex.IndexStatus.TRANSFORMING,
+                        FileIndex.IndexStatus.INDEXING, FileIndex.IndexStatus.READY);
+                // Check index status
+                for (File file : fileQueryResult.getResult()) {
+                    if (file.getIndex() == null || file.getIndex().getStatus() == null || file.getIndex().getStatus().getName() == null
+                            || acceptedStatus.contains(file.getIndex().getStatus().getName())) {
+                        fileList.add(file);
+                    }
+                }
+                fileQueryResult.setResult(fileList);
+                fileQueryResult.setNumResults(fileList.size());
             }
 
             if (fileQueryResult.getNumResults() == 0 || fileQueryResult.getNumResults() > 1) {
@@ -458,7 +473,10 @@ public class FileManager extends AbstractManager implements IFileManager {
             if (index.getTransformedFile() == null) {
                 index.setTransformedFile(new FileIndex.TransformedFile(transformedFile.getId(), json.getId()));
             }
-            String status = vcf.getIndex().getStatus().getName();
+            String status = FileIndex.IndexStatus.NONE;
+            if (vcf.getIndex() != null && vcf.getIndex().getStatus() != null && vcf.getIndex().getStatus().getName() != null) {
+                status = vcf.getIndex().getStatus().getName();
+            }
             if (FileIndex.IndexStatus.NONE.equals(status)) {
                 // If TRANSFORMED, TRANSFORMING, etc, do not modify the index status
                 index.setStatus(new FileIndex.IndexStatus(FileIndex.IndexStatus.TRANSFORMED));
@@ -1685,16 +1703,16 @@ public class FileManager extends AbstractManager implements IFileManager {
 
             // Limit the number of results and only some fields
             QueryOptions queryOptions = new QueryOptions()
-                    .append(QueryOptions.LIMIT, 100)
-                    .append(QueryOptions.INCLUDE, Arrays.asList(
-                            FileDBAdaptor.QueryParams.ID.key(),
-                            FileDBAdaptor.QueryParams.NAME.key(),
-                            FileDBAdaptor.QueryParams.TYPE.key(),
-                            FileDBAdaptor.QueryParams.PATH.key(),
-                            FileDBAdaptor.QueryParams.URI.key(),
-                            FileDBAdaptor.QueryParams.FORMAT.key(),
-                            FileDBAdaptor.QueryParams.BIOFORMAT.key()
-                    ));
+                    .append(QueryOptions.LIMIT, 100);
+//                    .append(QueryOptions.INCLUDE, Arrays.asList(
+//                            FileDBAdaptor.QueryParams.ID.key(),
+//                            FileDBAdaptor.QueryParams.NAME.key(),
+//                            FileDBAdaptor.QueryParams.TYPE.key(),
+//                            FileDBAdaptor.QueryParams.PATH.key(),
+//                            FileDBAdaptor.QueryParams.URI.key(),
+//                            FileDBAdaptor.QueryParams.FORMAT.key(),
+//                            FileDBAdaptor.QueryParams.BIOFORMAT.key()
+//                    ));
 
             return fileDBAdaptor.get(query, queryOptions);
         }
@@ -1924,16 +1942,16 @@ public class FileManager extends AbstractManager implements IFileManager {
 
             // Limit the number of results and only some fields
             QueryOptions queryOptions = new QueryOptions()
-                    .append(QueryOptions.LIMIT, 100)
-                    .append(QueryOptions.INCLUDE, Arrays.asList(
-                            FileDBAdaptor.QueryParams.ID.key(),
-                            FileDBAdaptor.QueryParams.NAME.key(),
-                            FileDBAdaptor.QueryParams.TYPE.key(),
-                            FileDBAdaptor.QueryParams.PATH.key(),
-                            FileDBAdaptor.QueryParams.URI.key(),
-                            FileDBAdaptor.QueryParams.FORMAT.key(),
-                            FileDBAdaptor.QueryParams.BIOFORMAT.key()
-                    ));
+                    .append(QueryOptions.LIMIT, 100);
+//                    .append(QueryOptions.INCLUDE, Arrays.asList(
+//                            FileDBAdaptor.QueryParams.ID.key(),
+//                            FileDBAdaptor.QueryParams.NAME.key(),
+//                            FileDBAdaptor.QueryParams.TYPE.key(),
+//                            FileDBAdaptor.QueryParams.PATH.key(),
+//                            FileDBAdaptor.QueryParams.URI.key(),
+//                            FileDBAdaptor.QueryParams.FORMAT.key(),
+//                            FileDBAdaptor.QueryParams.BIOFORMAT.key()
+//                    ));
 
             return fileDBAdaptor.get(query, queryOptions);
         }

opencga-storage/opencga-storage-core/src/main/java/org/opencb/opencga/storage/core/manager/variant/operations/VariantFileIndexerStorageOperation.java
@@ -631,7 +631,7 @@ public class VariantFileIndexerStorageOperation extends StorageOperation {
         for (File file : fileList) {
             if (file.getStatus().getName().equals(File.FileStatus.READY)
                     && file.getFormat().equals(File.Format.VCF)) {
-                if (file.getIndex() != null) {
+                if (file.getIndex() != null && file.getIndex().getStatus() != null && file.getIndex().getStatus().getName() != null) {
                     switch (file.getIndex().getStatus().getName()) {
                         case FileIndex.IndexStatus.NONE:
                             filteredFiles.add(file);

opencga-storage/opencga-storage-core/src/test/java/org/opencb/opencga/storage/core/manager/variant/CatalogStudyConfigurationFactoryTest.java
@@ -160,7 +160,8 @@ public class CatalogStudyConfigurationFactoryTest {
             assertEquals(file.getName(), entry.getKey());
             int id = (int) file.getId();
             assertEquals(file.getSampleIds().stream().map(Long::intValue).collect(Collectors.toSet()), studyConfiguration.getSamplesInFiles().get((id)));
-            if (file.getIndex() != null && file.getIndex().getStatus().getName().equals(FileIndex.IndexStatus.READY)) {
+            if (file.getIndex() != null && file.getIndex().getStatus() != null && file.getIndex().getStatus().getName() != null
+                    && file.getIndex().getStatus().getName().equals(FileIndex.IndexStatus.READY)) {
 //                assertTrue(studyConfiguration.getIndexedFiles().contains(id));
 //                assertTrue("Missing header for file " + file.getId(), studyConfiguration.getHeaders().containsKey(id));
 //                assertTrue("Missing header for file " + file.getId(), !studyConfiguration.getHeaders().get(id).isEmpty());