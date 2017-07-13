82d300e27e8bd3cb5b20fbad06601d531d1190b7
Jacobo Coll Moragón <jacobo167@gmail.com>
2017-07-13 16:18:47 +0100

    storage: Improve engineQuery for approxCount. #638

--- opencga-storage/opencga-storage-core/src/main/java/org/opencb/opencga/storage/core/search/solr/VariantSearchUtils.java
+++ opencga-storage/opencga-storage-core/src/main/java/org/opencb/opencga/storage/core/search/solr/VariantSearchUtils.java
@@ -2,11 +2,16 @@ package org.opencb.opencga.storage.core.search.solr;
 
 import org.opencb.commons.datastore.core.Query;
 import org.opencb.commons.datastore.core.QueryOptions;
+import org.opencb.opencga.storage.core.exceptions.StorageEngineException;
+import org.opencb.opencga.storage.core.metadata.StudyConfigurationManager;
 import org.opencb.opencga.storage.core.variant.adaptors.VariantField;
 import org.opencb.opencga.storage.core.variant.adaptors.VariantQueryParam;
+import org.opencb.opencga.storage.core.variant.adaptors.VariantQueryUtils;
 
 import java.util.*;
 
+import static org.opencb.opencga.storage.core.variant.adaptors.VariantQueryParam.RETURNED_STUDIES;
+import static org.opencb.opencga.storage.core.variant.adaptors.VariantQueryParam.STUDIES;
 import static org.opencb.opencga.storage.core.variant.adaptors.VariantQueryUtils.isValidParam;
 import static org.opencb.opencga.storage.core.variant.adaptors.VariantQueryUtils.validParams;
 
@@ -47,6 +52,11 @@ public class VariantSearchUtils {
         return true;
     }
 
+    public static Collection<VariantQueryParam> coveredParams(Query query) {
+        Set<VariantQueryParam> params = validParams(query);
+        return coveredParams(params);
+    }
+
     public static List<VariantQueryParam> coveredParams(Collection<VariantQueryParam> params) {
         List<VariantQueryParam> coveredParams = new ArrayList<>();
 
@@ -58,8 +68,9 @@ public class VariantSearchUtils {
         return coveredParams;
     }
 
-    public static List<VariantQueryParam> uncoveredParams(Query query) {
-        return uncoveredParams(validParams(query));
+    public static Collection<VariantQueryParam> uncoveredParams(Query query) {
+        Set<VariantQueryParam> params = validParams(query);
+        return uncoveredParams(params);
     }
 
     public static List<VariantQueryParam> uncoveredParams(Collection<VariantQueryParam> params) {
@@ -83,4 +94,25 @@ public class VariantSearchUtils {
         return true;
     }
 
+    public static Query getEngineQuery(Query query, QueryOptions options, StudyConfigurationManager scm)
+            throws StorageEngineException {
+        Collection<VariantQueryParam> uncoveredParams = uncoveredParams(query);
+        Query engineQuery = new Query();
+        for (VariantQueryParam uncoveredParam : uncoveredParams) {
+            engineQuery.put(uncoveredParam.key(), query.get(uncoveredParam.key()));
+        }
+        // Despite STUDIES is a covered filter by Solr, it has to be in the underlying
+        // query to be used as defaultStudy
+        if (isValidParam(query, STUDIES)) {
+            if (!uncoveredParams.isEmpty()) {
+                // This will set the default study, if needed
+                engineQuery.put(STUDIES.key(), query.get(STUDIES.key()));
+            } else if (!isValidParam(query, RETURNED_STUDIES)) {
+                // If returned studies is not defined, we need to define it with the values from STUDIES
+                List<Integer> studies = VariantQueryUtils.getReturnedStudies(query, options, scm);
+                engineQuery.put(RETURNED_STUDIES.key(), studies);
+            }
+        }
+        return engineQuery;
+    }
 }
