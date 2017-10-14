9ed987d1effd8cb1915f236805030856e72bdbc8
imedina <igmecas@gmail.com>
2017-10-14 23:25:35 +0100

    app : remove obsolote app/bin/obsolete folder

--- opencga-app/app/bin/obsolete/opencga-stop.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-PRGDIR=`dirname "$0"`
-BASEDIR=`cd "$PRGDIR/.." >/dev/null; pwd`
-daemon_port=`grep "OPENCGA.APP.DAEMON.PORT" ${BASEDIR}/conf/daemon.properties | cut -d "=" -f2`
-
-curl "http://localhost:${daemon_port}/opencga/rest/admin/stop"
-
-echo ""
