0ce3c38c01a8ac4f0cd01bad25195595d4ce30e6
pfurio <pedrofurio@gmail.com>
2018-05-04 17:01:13 +0100

    app: add migration for #819

--- opencga-app/app/migration/v1.4.0/opencga_catalog_v1.3.x_to_1.4.0.js
+++ opencga-app/app/migration/v1.4.0/opencga_catalog_v1.3.x_to_1.4.0.js
@@ -5,6 +5,8 @@ print(" --- Step 1/X ---");
 load("catalog/01_permission_rules.js");
 print(" --- Step 2/X ---");
 load("catalog/02_creation_dates.js");
+print(" --- Step 3/X ---");
+load("catalog/03_uid_id.js");
 
 
 print("Catalog database migrated correctly!");
