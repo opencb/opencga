b099e247818d09029b1204c4ec221d97a4b2f0b2
Marcus Robinson <marrobi@microsoft.com>
2018-12-06 15:43:35 +0000

    remove quotes

--- opencga-app/app/scripts/azure/arm/deploy.sh
+++ opencga-app/app/scripts/azure/arm/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd $(dirname "$0")
 
-az group deployment create -n storage -g $RG --template-file azuredeploy.json --parameters @azuredeploy.parameters.json    --parameters sshPassword=$PASSWORD --parameters clusterLoginPassword=$PASSWORD --parameters clusterName=$CLUSTERNAME --parameters _artifactsLocation=$ARTIFACTS_LOCATION
+az group deployment create -n storage -g $RG --template-file azuredeploy.json --parameters @azuredeploy.parameters.json    --parameters sshPassword=$PASSWORD --parameters clusterLoginPassword=$PASSWORD --parameters clusterName=$CLUSTERNAME --parameters _artifactsLocation=$ARTIFACTS_LOCATION --parameters  _artifactsLocationSasToken=?$RANDOM
