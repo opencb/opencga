24f20a5561a764083b214e593c7b45bb86d4c9a7
Marcus Robinson <marrobi@microsoft.com>
2018-12-06 15:07:19 +0000

    intial nested

---
+++ opencga-app/app/scripts/azure/arm/deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd $(dirname "$0")
+
+az group deployment create -n storage -g $RG --template-file azuredeploy.json --parameters @azuredeploy.parameters.json    --parameters sshPassword=$PASSWORD --parameters clusterLoginPassword=$PASSWORD --parameters clusterName=$CLUSTERNAME 
