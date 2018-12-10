3d4e0e64c481359ad22138e52d265ac3a7b49c1e
Lawrence Gripper <info@grippers.co.uk>
2018-12-10 18:09:31 +0000

    #931 Add dependson VM->StorageAccount. Fix environment var typo

--- opencga-app/app/scripts/azure/arm/testavere.sh
+++ opencga-app/app/scripts/azure/arm/testavere.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
+
+cd $(dirname "$0")
+
 export RG=temp-opencga-arm
+export SP_ID="d25b66ce-cb60-4d27-aeaa-e73df7611b33"
 az group delete -n $RG
 az group create -n $RG -l westeurope
-az role assignment create --role "Owner" --assignee "d25b66ce-cb60-4d27-aeaa-e73df7611b33" -g $RG
+az role assignment create --role "Owner" --assignee $SP_ID -g $RG
 bash -f ./vnet/deploy.sh
 bash -f ./avere/deploy.sh
 notify-send -u critical "Finished with exit code: $?"
+
