06291baeabc9aa9ce3b70ca35440068ff248cd2d
Marcus Robinson <marrobi@microsoft.com>
2018-12-11 21:58:15 +0000

    remove uneeded files

--- opencga-app/app/scripts/azure/arm/daemonvm/tmp.sh
+++
@@ -1,7 +0,0 @@
-az vm create \
-  --resource-group Demo-OpenCGADev \
-  --name marcuscloudinittest2 \
-  --image Canonical:UbuntuServer:18.04-LTS:latest \
-  --custom-data /mnt/c/repos/opencga-marrobi/opencga-app/app/scripts/azure/arm/loginvm/cloudinit.yaml \
-    --admin-password $PASSWORD      \
-    --admin-username opencgaadmin
