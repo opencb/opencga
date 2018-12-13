3155778d11cc57d9ad28a8bfd2e91b51071f2f81
Lawrence Gripper <info@grippers.co.uk>
2018-12-13 16:26:03 +0000

    #931 Fix review feedback

--- opencga-app/app/scripts/azure/arm/hdinsights-storage/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd $(dirname "$0")
-
-az group deployment create -n storage -g $RG --template-file azuredeploy.json --parameters @azuredeploy.parameters.json   --parameters networkAclsVirtualNetworkRule=$SUBNET_ID --parameters storageAccountName=$STORAGE_ACCOUNT_NAME
--- opencga-app/app/scripts/azure/arm/hdinsights/deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd $(dirname "$0")
-az group deployment create -n hdinsight -g $RG --template-file azuredeploy.json --parameters @azuredeploy.parameters.json   --parameters sshPassword=$PASSWORD --parameters clusterLoginPassword=$PASSWORD --parameters clusterName=$CLUSTERNAME --parameters storageAccountName=$STORAGE_ACCOUNT_NAME
