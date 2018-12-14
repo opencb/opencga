f57867d0e031d79a2374bc737acb2dc986f32ab1
Lawrence Gripper <info@grippers.co.uk>
2018-12-14 15:06:15 +0000

    #948 Review feedback conversion to python3

--- opencga-app/app/scripts/azure/arm/avere/mount.py
+++ opencga-app/app/scripts/azure/arm/avere/mount.py
@@ -7,8 +7,9 @@ import os
 import shutil
 import subprocess
 import time
+import ipaddress
 
-# Run `python -m unittest discover` in this dir to execute tests
+# Run `python3 -m unittest discover` in this dir to execute tests
 
 default_mount_options_nfs = "nfs hard,nointr,proto=tcp,mountproto=tcp,retry=30 0 0"
 default_mount_options_cifs = "dir_mode=0777,file_mode=0777,serverino,nofail,vers=3.0"
@@ -47,9 +48,7 @@ def get_ip_address():
     return IP
 
 def ip_as_int(ip):
-    o = map(int, ip.split('.'))
-    res = (16777216 * o[0]) + (65536 * o[1]) + (256 * o[2]) + o[3]
-    return res
+    return int(ipaddress.ip_address(ip))
 
 def remove_lines_containing(file, contains):
     with open(file,"r+") as file:
@@ -61,21 +60,21 @@ def remove_lines_containing(file, contains):
         file.truncate()
 
 def print_help():
-    print "For example 'sudo python mount.py avere 10.0.1.10-10.0.1.14'"
-    print "or 'sudo python mount.py azurefiles <storage-account-name>,<share-name>,<storage-account-key>'"
+    print("For example 'sudo python mount.py avere 10.0.1.10-10.0.1.14'")
+    print("or 'sudo python mount.py azurefiles <storage-account-name>,<share-name>,<storage-account-key>'")
 
 def install_apt_package(package):
     try:
-        print "Attempt to install " + package
+        print("Attempt to install " + package)
         subprocess.check_call(["apt", "install", package, "-y"])
-        print "Install completed successfully"
+        print("Install completed successfully")
     except subprocess.CalledProcessError as e:
-        print "Failed install " + package + " error:" + e.message
+        print("Failed install " + package + " error:" + e)
         exit(4)
 
 def main():  
     if len(sys.argv) < 3:
-        print "Expected arg1: 'mount_type' and arg2 'mount_data'"
+        print("Expected arg1: 'mount_type' and arg2 'mount_data'")
         print_help()
         exit(1)
 
@@ -83,18 +82,18 @@ def main():
     mount_data = str(sys.argv[2])
 
     if mount_type.lower() != "avere" and mount_type.lower() != "azurefiles": 
-        print "Expected first arg to be either 'avere' or 'azurefiles'"
+        print("Expected first arg to be either 'avere' or 'azurefiles'")
         print_help()
         exit(1)
 
     if mount_data == "":
-        print "Expected second arg to be the mounting data. For avere this is the vserver iprange. Fo azure files this should be the azure files connection details."
+        print("Expected second arg to be the mounting data. For avere this is the vserver iprange. Fo azure files this should be the azure files connection details.")
         print_help()
         exit(2)
 
 
-    print 'Mounting type:' + sys.argv[1]
-    print 'Mounting data:' + sys.argv[2]
+    print('Mounting type:' + sys.argv[1])
+    print('Mounting data:' + sys.argv[2])
     
     mount_point_permissions = 0o0777 #Todo: What permissions does this really need?
     primary_mount_folder = "/media/primarynfs"
@@ -118,7 +117,7 @@ def main():
 
             params = mount_data.split(",")
             if len(params) != 3:
-                print "Wrong params for azure files mount, expected 3 as CSV"
+                print("Wrong params for azure files mount, expected 3 as CSV")
                 print_help()
                 exit(1)
             
@@ -127,7 +126,7 @@ def main():
             account_key = params[2]
 
             with open('/etc/fstab', 'a') as file:
-                print "Mounting primary"
+                print("Mounting primary")
                 file.write("\n//{0}.file.core.windows.net/{1} {2} cifs username={0},password={3},{4} \n"
                     .format(account_name, share_name, primary_mount_folder, account_key, default_mount_options_cifs))
                 
@@ -136,14 +135,14 @@ def main():
             install_apt_package("nfs-common")            
 
             ips = get_avere_ips(mount_data)
-            print "Found ips:" + ",".join(ips)
+            print("Found ips:" + ",".join(ips))
 
             # Deterministically select a primary node from the available
             # servers for this vm to use. By using the ip as a seed this ensures
             # re-running will get the same node as primary
             current_ip = get_ip_address()
             current_ip_int = ip_as_int(current_ip)
-            print "Using ip as int: {0} for random seed".format(current_ip_int)
+            print("Using ip as int: {0} for random seed".format((current_ip_int)))
             random.seed(current_ip_int)
             random_node = random.randint(0, len(ips))
 
@@ -151,15 +150,15 @@ def main():
             ips.remove(primary)
             secondarys = ips
 
-            print "Primary node selected:" + primary
-            print "Secondary nodes selected:" + ",".join(secondarys)
+            print("Primary node selected:" + primary)
+            print("Secondary nodes selected:" + ",".join(secondarys))
 
             with open('/etc/fstab', 'a') as file:
 
-                print "Mounting primary"
+                print("Mounting primary")
                 file.write("\n"+ primary +":/msazure "+ primary_mount_folder + " "+ default_mount_options_nfs + "\n")
                 
-                print "Mounting secondarys"
+                print("Mounting secondarys")
                 number = 0
                 for ip in secondarys:
                     number = number+1
@@ -169,29 +168,30 @@ def main():
                         os.chmod(folder, mount_point_permissions)
                     
                     file.write("\n"+ ip +":/msazure "+ folder + " "+ default_mount_options_nfs + "\n")
-    except IOError as (errno, strerror):
-        print "I/O error({0}): {1}".format(errno, strerror)
+    except IOError as e:
+        print("I/O error({0})".format(e))
         exit(1)
     except:
-        print "Unexpected error:", sys.exc_info()[0]
+        print("Unexpected error:{0}".format, sys.exc_info())
+        raise
         exit(1)
 
-    print "Done editing fstab ... attempting mount"
+    print("Done editing fstab ... attempting mount")
 
     # Retry mounting for a while to handle race where VM exists before storage 
     # or temporary issue with storage
     retryExponentialFactor = 3
     for i in range(1,100):
         if i == 100:
-            print "Failed to mount after max 100 retries"
+            print("Failed to mount after max 100 retries")
             exit(3)
         try:
-            print "Attempt #" + str(i)
+            print("Attempt #" + str(i))
             subprocess.check_call(["mount", "-a"])
         except subprocess.CalledProcessError as e:
-            print "Failed to mount:" + e.message
+            print("Failed to mount:{0}".format(e))
             retry_in = i*retryExponentialFactor
-            print "retrying in {0}secs".format(retry_in)
+            print("retrying in {0}secs".format(retry_in))
             time.sleep(retry_in)
             continue
         else:
