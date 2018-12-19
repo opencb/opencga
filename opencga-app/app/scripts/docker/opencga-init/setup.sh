b718146984a5af60dd92044c5352ab33f8d0ff9d
Joni Collinge <jonathancollinge@live.com>
2018-12-19 07:12:35 +0000

    copy conf to volume only after HDInsight conf downloaded

--- opencga-app/app/scripts/docker/opencga-init/setup.sh
+++ opencga-app/app/scripts/docker/opencga-init/setup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Copies the config files from our local directory into a
-# persistent volume to be shared by the other containers.
-cp -r /opt/opencga/conf/* /opt/volume
-
 # TODO
 # Change hbase.client.keyvalue.maxsize to 0 in /etc/hbase/conf/hbase-site.xml on hadoop cluster to avoid 'KeyValue size too large' error
 # by sshing to your (hdinsight cluster e.g. ssh sshuser@bartcga-ssh.azurehdinsight.net) and changing the xml (e.g. vi /etc/hbase/conf/hbase-site.xml)
@@ -13,5 +9,9 @@ cp -r /opt/opencga/conf/* /opt/volume
 sshpass -p $HD_INSIGHTS_SSH_PASS scp -o StrictHostKeyChecking=no -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r $HD_INSIGHTS_SSH_USER@$HD_INSIGHTS_SSH_DNS:/etc/hadoop/conf/* /opt/opencga/conf/hadoop
 # same with /etc/hbase/conf, e.g.
 sshpass -p $HD_INSIGHTS_SSH_PASS scp -o StrictHostKeyChecking=no  -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r $HD_INSIGHTS_SSH_USER@$HD_INSIGHTS_SSH_DNS:/etc/hbase/conf/* /opt/opencga/conf/hadoop
+
+# Copies the config files from our local directory into a
+# persistent volume to be shared by the other containers.
+mkdir -p /opt/volume/conf && cp -r /opt/opencga/conf/* /opt/volume/
 
 /opt/opencga/bin/opencga-admin.sh catalog install --secret-key ${1}
