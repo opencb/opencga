19eecc7da5030b3118e3362a23b83d9ac59b30a0
Bart Jansen <bajansen@microsoft.com>
2019-01-02 10:47:08 +0100

    parameterized clusterSize var & WIP on postdeploy mongdb replica set config

--- opencga-app/app/scripts/azure/arm/mongodb/postdeploy.sh
+++ opencga-app/app/scripts/azure/arm/mongodb/postdeploy.sh
@@ -9,6 +9,8 @@ set -e
 # echo $MONGODB_USERNAME
 # echo $MONGODB_PASSWORD
 # echo $CERT_EMAIL
+# echo $VM_INDEX
+# echo $VM_SIZE
 apt-get update
 apt-get upgrade -y
 apt-get install software-properties-common -y
@@ -31,10 +33,37 @@ systemctl restart mongod
 sleep 10
 mongo admin --eval 'db.createUser({user: "'${MONGODB_USERNAME}'",pwd: "'${MONGODB_PASSWORD}'",roles: ["root"]})'
 sed -i '/#security/csecurity:\n  authorization: "enabled"\n' /etc/mongod.conf
+sed -i '/#replication/creplication:\n  replSetName: rs0\n' /etc/mongod.conf
 sed -i -e '/bindIp/ s/: .*/: ::,0.0.0.0\n  ssl:\n    mode: allowSSL\n    PEMKeyFile: \/etc\/ssl\/mongo.pem\n    CAFile: \/etc\/ssl\/ca.pem\n    allowConnectionsWithoutCertificates: true /' /etc/mongod.conf
 
 #add cronjob to renew certificate
 crontab -l | { cat; echo "0 0 1 * * /opt/renew_mongo_cert.sh ${APP_DNS_NAME}"; } | crontab -
 
 systemctl restart mongod
+
+# only for primary mongodb instance
+if [$VM_INDEX == 0]
+then
+    sleep 60 # waiting for other mongodb instances to be successfully configured
+    openssl rand -base64 741 > /opt/mongodb.key
+    chmod 600 /opt/mongodb.key
+    chown mongodb /opt/mongodb.key
+    systemctl restart mongod
+
+    #copy keyfile to other mongodb instances, chmod, chown & restart mongodb there as well
+
+    #sleep some time
+
+    mongo admin -u ${MONGODB_USERNAME} -p ${MONGODB_PASSWORD} --eval 'rs.initiate()'
+
+    #for vmindex +1
+    for (( c=1; c<$VM_SIZE; c++ ))
+    do
+        #replace 0. with 1., 2. etc
+        VM_DNS=$(sed -e "s/0./$c./g" <<< $APP_DNS_NAME)
+        echo $VM_DNS
+        mongo admin -u ${MONGODB_USERNAME} -p ${MONGODB_PASSWORD} --eval 'rs.add("'${VM_DNS}':27017")'
+        sleep 5
+    done
+fi
 
