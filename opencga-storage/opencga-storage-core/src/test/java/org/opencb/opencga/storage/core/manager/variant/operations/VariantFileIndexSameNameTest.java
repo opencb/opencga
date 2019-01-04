86df4c043ed243c04e8fd4bdb6a32d791984f101
Jacobo Coll Moragón <jacobo167@gmail.com>
2019-01-04 14:26:49 +0000

    storage: Fix error indexing file existing file with the same name. #988

--- opencga-storage/opencga-storage-core/src/test/java/org/opencb/opencga/storage/core/manager/variant/operations/VariantFileIndexSameNameTest.java
+++ opencga-storage/opencga-storage-core/src/test/java/org/opencb/opencga/storage/core/manager/variant/operations/VariantFileIndexSameNameTest.java
@@ -1,15 +1,20 @@
 package org.opencb.opencga.storage.core.manager.variant.operations;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.opencb.biodata.models.variant.metadata.Aggregation;
 import org.opencb.commons.datastore.core.QueryOptions;
 import org.opencb.opencga.catalog.exceptions.CatalogException;
 import org.opencb.opencga.core.models.File;
+import org.opencb.opencga.core.models.FileIndex;
 import org.opencb.opencga.storage.core.manager.variant.AbstractVariantStorageOperationTest;
 
 import java.util.Arrays;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.internal.matchers.ThrowableCauseMatcher.hasCause;
+import static org.junit.internal.matchers.ThrowableMessageMatcher.hasMessage;
 import static org.opencb.opencga.storage.core.variant.VariantStorageBaseTest.getResourceUri;
 
 /**
@@ -39,11 +44,15 @@ public class VariantFileIndexSameNameTest extends AbstractVariantStorageOperatio
     @Test
     public void testIndex1() throws Exception {
         indexFile(inputFile1, new QueryOptions(), outputId);
+        Assert.assertEquals(FileIndex.IndexStatus.READY, catalogManager.getFileManager().get(studyId, inputFile1.getPath(), null, sessionId).first().getIndex().getStatus().getName());
+        Assert.assertNull(catalogManager.getFileManager().get(studyId, inputFile2.getPath(), null, sessionId).first().getIndex().getStatus());
     }
 
     @Test
     public void testIndex2() throws Exception {
         indexFile(inputFile2, new QueryOptions(), outputId);
+        Assert.assertNull(catalogManager.getFileManager().get(studyId, inputFile1.getPath(), null, sessionId).first().getIndex().getStatus());
+        Assert.assertEquals(FileIndex.IndexStatus.READY, catalogManager.getFileManager().get(studyId, inputFile2.getPath(), null, sessionId).first().getIndex().getStatus().getName());
     }
 
     @Test
@@ -69,8 +78,8 @@ public class VariantFileIndexSameNameTest extends AbstractVariantStorageOperatio
     @Test
     public void testIndexBoth() throws Exception {
         indexFile(inputFile1, new QueryOptions(), outputId);
-        thrown.expect(CatalogException.class);
-        thrown.expectMessage("Unable to index multiple files with the same file name");
+        thrown.expect(hasMessage(containsString("Exception executing transform")));
+        thrown.expect(hasCause(hasMessage(containsString("Already registered with a different path"))));
         indexFile(inputFile2, new QueryOptions(), outputId);
     }
 
@@ -84,12 +93,20 @@ public class VariantFileIndexSameNameTest extends AbstractVariantStorageOperatio
     @Test
     public void testBySteps1() throws Exception {
         File transformFile = transformFile(inputFile1, new QueryOptions());
+        Assert.assertEquals(FileIndex.IndexStatus.TRANSFORMED, catalogManager.getFileManager().get(studyId, inputFile1.getPath(), null, sessionId).first().getIndex().getStatus().getName());
+        Assert.assertNull(catalogManager.getFileManager().get(studyId, inputFile2.getPath(), null, sessionId).first().getIndex().getStatus());
         loadFile(transformFile, new QueryOptions(), outputId);
+        Assert.assertEquals(FileIndex.IndexStatus.READY, catalogManager.getFileManager().get(studyId, inputFile1.getPath(), null, sessionId).first().getIndex().getStatus().getName());
+        Assert.assertNull(catalogManager.getFileManager().get(studyId, inputFile2.getPath(), null, sessionId).first().getIndex().getStatus());
     }
 
     @Test
     public void testBySteps2() throws Exception {
         File transformFile = transformFile(inputFile2, new QueryOptions());
+        Assert.assertNull(catalogManager.getFileManager().get(studyId, inputFile1.getPath(), null, sessionId).first().getIndex().getStatus());
+        Assert.assertEquals(FileIndex.IndexStatus.TRANSFORMED, catalogManager.getFileManager().get(studyId, inputFile2.getPath(), null, sessionId).first().getIndex().getStatus().getName());
         loadFile(transformFile, new QueryOptions(), outputId);
+        Assert.assertNull(catalogManager.getFileManager().get(studyId, inputFile1.getPath(), null, sessionId).first().getIndex().getStatus());
+        Assert.assertEquals(FileIndex.IndexStatus.READY, catalogManager.getFileManager().get(studyId, inputFile2.getPath(), null, sessionId).first().getIndex().getStatus().getName());
     }
 }
