9efd5693a9e90ecff2fae766d4aa1f62901ac51f
pfurio <pedrofurio@gmail.com>
2019-01-29 16:53:41 +0000

    catalog: add new field 'assigned' to Clinical Analysis

--- opencga-catalog/src/main/java/org/opencb/opencga/catalog/db/api/ClinicalAnalysisDBAdaptor.java
+++ opencga-catalog/src/main/java/org/opencb/opencga/catalog/db/api/ClinicalAnalysisDBAdaptor.java
@@ -52,6 +52,7 @@ public interface ClinicalAnalysisDBAdaptor extends DBAdaptor<ClinicalAnalysis> {
         STATUS_MSG("status.msg", TEXT, ""),
         STATUS_DATE("status.date", TEXT, ""),
         PRIORITY("priority", TEXT, ""),
+        ASSIGNED("assigned", TEXT_ARRAY, ""),
         FLAGS("flags", TEXT_ARRAY, ""),
         RELEASE("release", INTEGER, ""),
 
--- opencga-catalog/src/main/java/org/opencb/opencga/catalog/managers/ClinicalAnalysisManager.java
+++ opencga-catalog/src/main/java/org/opencb/opencga/catalog/managers/ClinicalAnalysisManager.java
@@ -143,6 +143,16 @@ public class ClinicalAnalysisManager extends ResourceManager<ClinicalAnalysis> {
         ParamUtils.checkAlias(clinicalAnalysis.getId(), "id");
         ParamUtils.checkObj(clinicalAnalysis.getType(), "type");
         ParamUtils.checkObj(clinicalAnalysis.getDueDate(), "dueDate");
+        if (clinicalAnalysis.getAssigned() != null) {
+            // We obtain the users with access to the study
+            Set<String> users = new HashSet<>(catalogManager.getStudyManager().getGroup(studyStr, "members", sessionId).first()
+                    .getUserIds());
+            if (!users.contains(clinicalAnalysis.getAssigned().getAssignee())) {
+                throw new CatalogException("Cannot assign clinical analysis to " + clinicalAnalysis.getAssigned().getAssignee()
+                        + ". User not found or with no access to the study.");
+            }
+            clinicalAnalysis.getAssigned().setAssignedBy(userId);
+        }
 
         if (TimeUtils.toDate(clinicalAnalysis.getDueDate()) == null) {
             throw new CatalogException("Unrecognised due date. Accepted format is: yyyyMMddHHmmss");
@@ -440,6 +450,17 @@ public class ClinicalAnalysisManager extends ResourceManager<ClinicalAnalysis> {
                     if (TimeUtils.toDate(parameters.getString(DUE_DATE.key())) == null) {
                         throw new CatalogException("Unrecognised due date. Accepted format is: yyyyMMddHHmmss");
                     }
+                    break;
+                case ASSIGNED:
+                    LinkedHashMap<String, Object> assigned = (LinkedHashMap<String, Object>) param.getValue();
+                    // We obtain the users with access to the study
+                    Set<String> users = new HashSet<>(catalogManager.getStudyManager().getGroup(studyStr, "members", sessionId).first()
+                            .getUserIds());
+                    if (!users.contains(String.valueOf(assigned.get("assignee")))) {
+                        throw new CatalogException("Cannot assign clinical analysis to " + assigned.get("assignee")
+                                + ". User not found or with no access to the study.");
+                    }
+                    assigned.put("assignedBy", resource.getUser());
                     break;
                 case FILES:
                 case STATUS:
