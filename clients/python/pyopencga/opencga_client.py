383fc6d6ed0a8a287b7c0c5f9c9ec88b0c2dc3e0
Pablo Marin <pablo.marin@mgviz.org>
2019-02-20 01:18:19 +0000

    [pyopencga] initial comit with files from pycga. Changed dir to ./clients

---
+++ clients/python/pyopencga/opencga_client.py
@@ -0,0 +1,109 @@
+from opencga_config import ConfigClient
+from rest_clients._all_rest_clients import *
+
+# from rest_clients.user_client import Users
+# from rest_clients.project_client import Projects
+# from rest_clients.study_client import Studies
+# from rest_clients.file_client import Files
+# from rest_clients.sample_client import Samples
+# from rest_clients.cohort_client import Cohorts
+# from rest_clients.family_client import Families
+# from rest_clients.job_client import Jobs
+# from rest_clients.individual_client import Individuals
+# from rest_clients.clinical_client import Clinical
+# from rest_clients.variableset_client import VariableSets
+# from rest_clients.alignment_client import AnalysisAlignment
+# from rest_clients.variant_client import AnalysisVariant
+# from rest_clients.ga4gh_client import GA4GH
+# from rest_clients.meta_client import Meta
+
+
+class OpenCGAClient(object):
+    def __init__(self, configuration, user=None, pwd=None, session_id=None,
+                 anonymous=False, on_retry=None, auto_refresh=True):
+        """
+        :param on_retry: callback to be called with client retries an operation.
+            It must accept parameters: client, exc_type, exc_val, exc_tb, call
+        """
+        self.auto_refresh = auto_refresh
+        self.configuration = ConfigClient(configuration, on_retry)
+        self.on_retry = on_retry
+        self.clients = []
+        self.user_id = user  # if user and session_id are supplied, we can log out
+        if anonymous:
+            self._login_handler = None
+            self.session_id = None
+        else:
+            if user and pwd:
+                self._login_handler = self._make_login_handler(user, pwd)
+                self._login()
+            else:
+                if session_id is None:
+                    msg = "User/password or session_id must be supplied"
+                    raise Exception(msg)
+                self._login_handler = None
+                self.session_id = session_id
+        self._create_clients()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.logout()
+
+    def _create_clients(self):
+        self.users = Users(self.configuration, self.session_id, self._login_handler, auto_refresh=self.auto_refresh)
+        self.projects = Projects(self.configuration, self.session_id, self._login_handler, auto_refresh=self.auto_refresh)
+        self.studies = Studies(self.configuration, self.session_id, self._login_handler, auto_refresh=self.auto_refresh)
+        self.files = Files(self.configuration, self.session_id, self._login_handler, auto_refresh=self.auto_refresh)
+        self.samples = Samples(self.configuration, self.session_id, self._login_handler, auto_refresh=self.auto_refresh)
+        self.cohorts = Cohorts(self.configuration, self.session_id, self._login_handler, auto_refresh=self.auto_refresh)
+        self.families = Families(self.configuration, self.session_id, self._login_handler, auto_refresh=self.auto_refresh)
+        self.jobs = Jobs(self.configuration, self.session_id, self._login_handler, auto_refresh=self.auto_refresh)
+        self.individuals = Individuals(self.configuration, self.session_id, self._login_handler, auto_refresh=self.auto_refresh)
+        self.clinical = Clinical(self.configuration, self.session_id, self._login_handler, auto_refresh=self.auto_refresh)
+        self.variable_sets = VariableSets(self.configuration, self.session_id, self._login_handler, auto_refresh=self.auto_refresh)
+        self.analysis_alignment = AnalysisAlignment(self.configuration, self.session_id, self._login_handler, auto_refresh=self.auto_refresh)
+        self.analysis_variant = AnalysisVariant(self.configuration, self.session_id, self._login_handler, auto_refresh=self.auto_refresh)
+        self.ga4gh = GA4GH(self.configuration, self.session_id, self._login_handler, auto_refresh=self.auto_refresh)
+        self.meta = Meta(self.configuration, self.session_id, self._login_handler, auto_refresh=self.auto_refresh)
+
+        self.clients = [self.users, self.projects, self.studies, self.files,
+                        self.samples, self.cohorts, self.families, self.jobs,
+                        self.individuals, self.variable_sets, self.clinical,
+                        self.analysis_alignment, self.analysis_variant,
+                        self.ga4gh, self.meta]
+
+        for client in self.clients:
+            client.on_retry = self.on_retry
+
+    def _make_login_handler(self, user, pwd):
+        """
+        Returns a closure that performs the log-in. This will be called on retries
+        if the current session ever expires.
+        The reason for using a closure and not a normal function is that a normal
+        function would require storing the password in a field. It is more secure
+        not to do so. This way, the password stored in the closure is inaccessible
+        to other code
+        """
+
+        def login_handler(refresh=False):
+            self.user_id = user
+            if refresh:
+                self.session_id = Users(self.configuration, session_id=self.session_id).refresh_token(user=user).get().sessionId
+            else:
+                self.session_id = Users(self.configuration).login(user=user, pwd=pwd).get().sessionId
+
+            for client in self.clients:
+                client.session_id = self.session_id  # renew the client's session id
+            return self.session_id
+
+        return login_handler
+
+    def _login(self):
+        assert self._login_handler, "Can't login without username and password provided"
+        self._login_handler()
+
+    def logout(self):
+        self.session_id = None
+
---
+++ clients/python/pyopencga/rest_clients/_parent_rest_clients.py
@@ -0,0 +1,189 @@
+import os
+import sys
+
+## Hack for an import from a parent dir
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
+from retry import retry
+from commons import execute, 
+
+class _ParentRestClient(object):
+    """Queries the REST service given the different query params"""
+
+    def __init__(self, configuration, category, session_id=None, login_handler=None, auto_refresh=True):
+        """
+        :param login_handler: a parameterless method that can log in this connector
+        and return a session id
+        """
+        self.auto_refresh = auto_refresh
+        self._cfg = configuration
+        self._category = category
+        self.session_id = session_id
+        self.login_handler = login_handler
+        self.on_retry = None
+
+    def _client_login_handler(self):
+        if self.login_handler:
+            self.session_id = self.login_handler()
+
+    def _refresh_token_client(self):
+        if self.login_handler:
+            self.session_id = self.login_handler(refresh=True)
+
+    @staticmethod
+    def _get_query_id_str(query_ids):
+        if query_ids is None:
+            return None
+        elif isinstance(query_ids, list):
+            return ','.join(map(str, query_ids))
+        else:
+            return str(query_ids)
+
+    def _rest_retry(self, method, resource, query_id=None, subcategory=None,
+                    second_query_id=None, data=None, dont_retry=None, **options):
+        """Invokes the specified HTTP method, with retries if they are specified in the configuration
+        :return: an instance of OpenCGAResponseList"""
+
+        query_ids_str = self._get_query_id_str(query_id)
+
+        def exec_retry():
+            return execute(host=self._cfg.host,
+                           version=self._cfg.version,
+                           sid=self.session_id,
+                           category=self._category,
+                           subcategory=subcategory,
+                           method=method,
+                           query_id=query_ids_str,
+                           second_query_id=second_query_id,
+                           resource=resource,
+                           data=data,
+                           options=options)
+
+        def notify_retry(exc_type, exc_val, exc_tb):
+            if self.on_retry:
+                self.on_retry(self, exc_type, exc_val, exc_tb, dict(
+                    method=method, resource=resource, query_id=query_id,
+                    category=self._category, subcategory=subcategory,
+                    second_query_id=second_query_id, data=data,
+                    options=options
+                ))
+
+        response = retry(
+            exec_retry, self._cfg.max_attempts, self._cfg.min_retry_secs, self._cfg.max_retry_secs,
+            login_handler=self._client_login_handler if self.login_handler else None,
+            on_retry=notify_retry, dont_retry=dont_retry)
+
+        if self.auto_refresh:
+            self._refresh_token_client()
+        return OpenCGAResponseList(response, query_ids_str)
+
+    def _get(self, resource, query_id=None, subcategory=None, second_query_id=None, **options):
+        """Queries the REST service and returns the result"""
+        return self._rest_retry('get', resource, query_id, subcategory, second_query_id, **options)
+
+    def _post(self, resource, data, query_id=None, subcategory=None, second_query_id=None, **options):
+        """Queries the REST service and returns the result"""
+        return self._rest_retry('post', resource, query_id, subcategory, second_query_id, data=data, **options)
+
+
+class _ParentBasicCRUDClient(_ParentRestClient):
+    def create(self, data, **options):
+        return self._post('create', data=data, **options)
+
+    def info(self, query_id, **options):
+        return self._get('info', query_id=query_id, **options)
+
+    def update(self, query_id, data, **options):
+        return self._post('update', query_id=query_id, data=data, **options)
+
+    def delete(self, query_id, **options):
+        return self._get('delete', query_id=query_id, **options)
+
+
+class _ParentAclRestClient(_ParentRestClient):
+    def acl(self, query_id, **options):
+        """
+        acl info
+
+        :param query_id:
+        :param options:
+        """
+
+        return self._get('acl', query_id=query_id, **options)
+
+    def acl_update(self, memberId, data, **options):
+        """
+        update acl
+
+        :param query_id:
+        :param options:
+        """
+
+        return self._post('acl', subcategory='update', second_query_id=memberId, data=data, **options)
+
+
+class _ParentAnnotationSetRestClient(_ParentRestClient):
+    def annotationsets_search(self, query_id, study, **options):
+        """
+        annotationsets search
+
+        :param query_id:
+        :param options:
+        """
+
+        return self._get('annotationsets', study=study, query_id=query_id, subcategory='search', **options)
+
+    def annotationsets(self, query_id, study, **options):
+        """
+        annotationsets
+
+        :param query_id:
+        :param options:
+        """
+
+        return self._get('annotationsets', query_id=query_id, study=study, subcategory='info', **options)
+
+    def annotationsets_delete(self, query_id, study, annotationset_name, **options):
+        """
+        delete annotationsets
+
+        :param query_id:
+        :param options:
+        """
+
+        return self._get('annotationsets', query_id=query_id, study=study, subcategory='delete',
+                         second_query_id=annotationset_name, **options)
+
+    def annotationsets_info(self, query_id, study, annotationset_name, **options):
+        """
+        info annotationsets
+
+        :param query_id:
+        :param options:
+        """
+
+        return self._get('annotationsets', query_id=query_id, study=study, subcategory='info',
+                         second_query_id=annotationset_name, **options)
+
+    def annotationsets_create(self, query_id, study, variable_set_id, data, **options):
+        """
+        create annotationsets
+
+        :param query_id:
+        :param options:
+        """
+
+        return self._post('annotationsets', study=study, query_id=query_id, subcategory='create',
+                          variableSetId=variable_set_id, data=data, **options)
+
+    def annotationsets_update(self, query_id, study, annotationset_name, data, **options):
+        """
+        update annotationsets
+
+        :param query_id:
+        :param options:
+        """
+
+        return self._post('annotationsets', study=study, query_id=query_id, subcategory='update',
+                          second_query_id=annotationset_name, data=data, **options)
+
