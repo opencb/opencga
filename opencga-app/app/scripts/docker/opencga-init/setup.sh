043b5c30d2a84aebff9571c3da8c8b7557cbf3bb
Lawrence Gripper <info@grippers.co.uk>
2019-03-21 17:16:47 +0000

    Fix issue with newly prefixed environment vars

--- opencga-app/app/scripts/docker/opencga-init/setup.sh
+++ opencga-app/app/scripts/docker/opencga-init/setup.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-sshpass -p "$HBASE_SSH_PASS" ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "$HBASE_SSH_USER@$HBASE_SSH_DNS" "sudo sed -i '/<name>hbase.client.keyvalue.maxsize<\/name>/!b;n;c<value>0</value>' /etc/hbase/conf/hbase-site.xml"
+sshpass -p "$INIT_HBASE_SSH_PASS" ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "\$INIT_HBASE_SSH_USER@$INIT_HBASE_SSH_DNS" "sudo sed -i '/<name>hbase.client.keyvalue.maxsize<\/name>/!b;n;c<value>0</value>' /etc/hbase/conf/hbase-site.xml"
 
 # copy conf files from hdinsight cluster (from /etc/hadoop/conf & /etc/hbase/conf) to opencga VM
 # place these files in /opt/opencga/conf/hadoop, by e.g.: (todo: change connection strings)
 echo "Fetching HDInsight configuration"
-sshpass -p "$HBASE_SSH_PASS" scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r "$HBASE_SSH_USER@$HBASE_SSH_DNS":/etc/hadoop/conf/* /opt/opencga/conf/hadoop
+sshpass -p "$INIT_HBASE_SSH_PASS" scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r "\$INIT_HBASE_SSH_USER@$INIT_HBASE_SSH_DNS":/etc/hadoop/conf/* /opt/opencga/conf/hadoop
 # same with /etc/hbase/conf, e.g.
-sshpass -p "$HBASE_SSH_PASS" scp -o StrictHostKeyChecking=no  -o UserKnownHostsFile=/dev/null -r "$HBASE_SSH_USER@$HBASE_SSH_DNS":/etc/hbase/conf/* /opt/opencga/conf/hadoop
+sshpass -p "$INIT_HBASE_SSH_PASS" scp -o StrictHostKeyChecking=no  -o UserKnownHostsFile=/dev/null -r "\$INIT_HBASE_SSH_USER@$INIT_HBASE_SSH_DNS":/etc/hbase/conf/* /opt/opencga/conf/hadoop
 
 # Copy the OpenCGA installation directory to the hdinsights cluster
 # TODO - Optimize this down to only required jars
-REMOTE_OPENCGA_HOME="/home/$HBASE_SSH_USER/opencga/"
-sshpass -p "$HBASE_SSH_PASS" scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r /opt/opencga/ "$HBASE_SSH_USER@$HBASE_SSH_DNS":"$REMOTE_OPENCGA_HOME"
+INIT_REMOTE_OPENCGA_HOME="/home/\$INIT_HBASE_SSH_USER/opencga/"
+sshpass -p "$INIT_HBASE_SSH_PASS" scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r /opt/opencga/ "\$INIT_HBASE_SSH_USER@$INIT_HBASE_SSH_DNS":"$INIT_REMOTE_OPENCGA_HOME"
 
 echo "Initialising configs"
 # Override Yaml configs
@@ -28,7 +28,7 @@ cp -r /opt/opencga/conf/* /opt/volume/conf
 cp -r /opt/opencga/ivaconf/* /opt/volume/ivaconf
 
 echo "Installing catalog"
-echo "${OPENCGA_PASS}" | /opt/opencga/bin/opencga-admin.sh catalog install --secret-key ${1}
+echo "${INIT_OPENCGA_PASS}" | /opt/opencga/bin/opencga-admin.sh catalog install --secret-key ${1}
 
 # Catalog install will create a sub-folders in sessions
 # that need copying to the volume.
