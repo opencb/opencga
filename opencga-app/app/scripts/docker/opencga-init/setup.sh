d4b9858704416b6fdcc1d510f12f401b925e5fe6
Lawrence Gripper <info@grippers.co.uk>
2019-03-22 09:01:55 +0000

    Fix typo in setup.sh when renaming envar for sshuser

--- opencga-app/app/scripts/docker/opencga-init/setup.sh
+++ opencga-app/app/scripts/docker/opencga-init/setup.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-sshpass -p "$INIT_HBASE_SSH_PASS" ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "\$INIT_HBASE_SSH_USER@$INIT_HBASE_SSH_DNS" "sudo sed -i '/<name>hbase.client.keyvalue.maxsize<\/name>/!b;n;c<value>0</value>' /etc/hbase/conf/hbase-site.xml"
+sshpass -p "$INIT_HBASE_SSH_PASS" ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "$INIT_HBASE_SSH_USER@$INIT_HBASE_SSH_DNS" "sudo sed -i '/<name>hbase.client.keyvalue.maxsize<\/name>/!b;n;c<value>0</value>' /etc/hbase/conf/hbase-site.xml"
 
 # copy conf files from hdinsight cluster (from /etc/hadoop/conf & /etc/hbase/conf) to opencga VM
 # place these files in /opt/opencga/conf/hadoop, by e.g.: (todo: change connection strings)
 echo "Fetching HDInsight configuration"
-sshpass -p "$INIT_HBASE_SSH_PASS" scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r "\$INIT_HBASE_SSH_USER@$INIT_HBASE_SSH_DNS":/etc/hadoop/conf/* /opt/opencga/conf/hadoop
+sshpass -p "$INIT_HBASE_SSH_PASS" scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r "$INIT_HBASE_SSH_USER@$INIT_HBASE_SSH_DNS":/etc/hadoop/conf/* /opt/opencga/conf/hadoop
 # same with /etc/hbase/conf, e.g.
-sshpass -p "$INIT_HBASE_SSH_PASS" scp -o StrictHostKeyChecking=no  -o UserKnownHostsFile=/dev/null -r "\$INIT_HBASE_SSH_USER@$INIT_HBASE_SSH_DNS":/etc/hbase/conf/* /opt/opencga/conf/hadoop
+sshpass -p "$INIT_HBASE_SSH_PASS" scp -o StrictHostKeyChecking=no  -o UserKnownHostsFile=/dev/null -r "$INIT_HBASE_SSH_USER@$INIT_HBASE_SSH_DNS":/etc/hbase/conf/* /opt/opencga/conf/hadoop
 
 # Copy the OpenCGA installation directory to the hdinsights cluster
 # TODO - Optimize this down to only required jars
-INIT_REMOTE_OPENCGA_HOME="/home/\$INIT_HBASE_SSH_USER/opencga/"
-sshpass -p "$INIT_HBASE_SSH_PASS" scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r /opt/opencga/ "\$INIT_HBASE_SSH_USER@$INIT_HBASE_SSH_DNS":"$INIT_REMOTE_OPENCGA_HOME"
+INIT_REMOTE_OPENCGA_HOME="/home/$INIT_HBASE_SSH_USER/opencga/"
+sshpass -p "$INIT_HBASE_SSH_PASS" scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r /opt/opencga/ "$INIT_HBASE_SSH_USER@$INIT_HBASE_SSH_DNS":"$INIT_REMOTE_OPENCGA_HOME"
 
 echo "Initialising configs"
 # Override Yaml configs
