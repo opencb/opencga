fbb45309c21f0e6c357263780f8cdc7fb90a36cf
Jacobo Coll Moragón <jacobo167@gmail.com>
2019-03-27 13:56:42 +0100

    storage: Get samplesData grouped by genotypes. #1230

---
+++ opencga-storage/opencga-storage-core/src/main/java/org/opencb/opencga/storage/core/variant/adaptors/sample/VariantSampleDataManager.java
@@ -0,0 +1,136 @@
+package org.opencb.opencga.storage.core.variant.adaptors.sample;
+
+import org.opencb.biodata.models.feature.Genotype;
+import org.opencb.biodata.models.variant.StudyEntry;
+import org.opencb.biodata.models.variant.Variant;
+import org.opencb.biodata.models.variant.avro.FileEntry;
+import org.opencb.commons.datastore.core.Query;
+import org.opencb.commons.datastore.core.QueryOptions;
+import org.opencb.commons.datastore.core.QueryResult;
+import org.opencb.opencga.core.results.VariantQueryResult;
+import org.opencb.opencga.storage.core.variant.adaptors.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
+
+/**
+ * Created by jacobo on 27/03/19.
+ */
+public class VariantSampleDataManager {
+
+    private final VariantDBAdaptor dbAdaptor;
+    private static Logger logger = LoggerFactory.getLogger(VariantSampleDataManager.class);
+    private Map<String, String> normalizeGt = new HashMap<>();
+
+    public VariantSampleDataManager(VariantDBAdaptor dbAdaptor) {
+        this.dbAdaptor = dbAdaptor;
+    }
+
+    public QueryResult<VariantSampleData> sampleData(String variant, String study, QueryOptions options) {
+        options = options == null ? new QueryOptions() : options;
+        int sampleLimit = options.getInt("sampleLimit", 100);
+        return sampleData(variant, study, options, sampleLimit);
+    }
+
+    public QueryResult<VariantSampleData> sampleData(String variant, String study, QueryOptions options, int sampleLimit) {
+        options = options == null ? new QueryOptions() : options;
+
+        Set<String> genotypes = new HashSet<>(options.getAsStringList(VariantQueryParam.GENOTYPE.key()));
+        if (genotypes.isEmpty()) {
+            genotypes.add("0/1");
+            genotypes.add("1/1");
+        }
+        boolean merge = options.getBoolean("merge", false);
+
+        return sampleData(variant, study, options, genotypes, merge, sampleLimit);
+    }
+
+    public QueryResult<VariantSampleData> sampleData(String variant, String study, QueryOptions options,
+                                                     Set<String> genotypes, boolean merge, int sampleLimit) {
+        options = options == null ? new QueryOptions() : options;
+        int skip = Math.max(0, options.getInt(QueryOptions.SKIP, 0));
+        int limit = Math.max(0, options.getInt(QueryOptions.LIMIT, 10));
+
+        int dbTime = 0;
+        Map<String, Integer> gtCountMap = new HashMap<>();
+        Map<String, List<SampleData>> gtMap = new HashMap<>();
+        Map<String, FileEntry> files = new HashMap<>();
+
+        for (String gt : merge ? Collections.singleton(VariantQueryUtils.ALL) : genotypes) {
+            gtCountMap.put(gt, 0);
+            gtMap.put(gt, new ArrayList<>(limit));
+        }
+
+        int sampleSkip = 0;
+        while (true) {
+            Query query = new Query(VariantQueryParam.ID.key(), variant)
+                    .append(VariantQueryParam.STUDY.key(), study)
+                    .append(VariantQueryParam.SAMPLE_LIMIT.key(), sampleLimit)
+                    .append(VariantQueryParam.SAMPLE_SKIP.key(), sampleSkip);
+            sampleSkip += sampleLimit;
+            QueryOptions variantQueryOptions = new QueryOptions(QueryOptions.EXCLUDE,
+                    Arrays.asList(VariantField.ANNOTATION, VariantField.STUDIES_STATS));
+
+            VariantQueryResult<Variant> result = dbAdaptor.get(query, variantQueryOptions);
+            if (result.getNumResults() == 0) {
+                throw VariantQueryException.variantNotFound(variant);
+            }
+            dbTime += result.getDbTime();
+            Variant v = result.first();
+
+            StudyEntry studyEntry = v.getStudies().get(0);
+
+            List<String> samples = studyEntry.getOrderedSamplesName();
+            for (String sample : samples) {
+                Map<String, String> sampleDataAsMap = studyEntry.getSampleDataAsMap(sample);
+
+                String gt = normalizeGt(sampleDataAsMap.getOrDefault("GT", GenotypeClass.NA_GT_VALUE));
+
+                if (genotypes.contains(gt)) {
+                    // Skip other genotypes
+
+                    if (merge) {
+                        // Merge after filtering by genotype
+                        gt = VariantQueryUtils.ALL;
+                    }
+                    List<SampleData> gtList = gtMap.get(gt);
+                    if (gtCountMap.merge(gt, 1, Integer::sum) > skip) {
+                        if (gtList.size() < limit) {
+                            FileEntry fileEntry = studyEntry.getFiles().get(0);
+                            SampleData sampleData = new SampleData(sample, sampleDataAsMap, fileEntry.getFileId());
+                            files.put(fileEntry.getFileId(), fileEntry);
+                            gtList.add(sampleData);
+                        }
+                    }
+                }
+            }
+
+            if (samples.size() < sampleLimit) {
+//                logger.debug("Exit end samples");
+                break;
+            }
+
+
+            if (gtMap.values().stream().allMatch(c -> c.size() >= limit)) {
+//                logger.debug("Exit limit");
+                break;
+            }
+        }
+
+        return new QueryResult<>(variant, dbTime, 1, 1, null, null,
+                Collections.singletonList(new VariantSampleData(variant, study, gtMap, files)));
+    }
+
+    protected String normalizeGt(String gt) {
+        if (gt.contains("|")) {
+            return normalizeGt.computeIfAbsent(gt, k -> {
+                Genotype genotype = new Genotype(k.replace('|', '/'));
+                genotype.normalizeAllelesIdx();
+                return genotype.toString();
+            });
+        }
+        return gt;
+    }
+
+}
