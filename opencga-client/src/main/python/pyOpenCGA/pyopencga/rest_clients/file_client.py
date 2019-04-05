faab2dd367c8256016792f23b141e0a8f5f10fef
David Gómez Peregrina <dgomezpere@gmail.com>
2019-04-05 14:58:58 +0000

    [pyopencga] files_client **kwargs to **options changed

--- opencga-client/src/main/python/pyOpenCGA/pyopencga/rest_clients/file_client.py
+++ opencga-client/src/main/python/pyOpenCGA/pyopencga/rest_clients/file_client.py
@@ -9,7 +9,7 @@ class Files(_ParentBasicCRUDClient, _ParentAclRestClient):
         _category = 'files'
         super(Files, self).__init__(configuration, _category, session_id, login_handler, *args, **kwargs)
 
-    def stats(self, **kwargs):
+    def stats(self, **options):
         """
         Fetch catalog file stats
         URL: /{apiVersion}/files/stats
@@ -23,8 +23,8 @@ class Files(_ParentBasicCRUDClient, _ParentAclRestClient):
         :param creationMonth: creation month (JANUARY, FEBRUARY...)
         :param creationDay: creation day
         :param creationDayOfWeek: creation day of week (MONDAY, TUESDAY...)
-        :param status: status 
-        :param release: release 
+        :param status: status
+        :param release: release
         :param external: external (boolean)
         :param size: size
         :param software: software
@@ -33,29 +33,29 @@ class Files(_ParentBasicCRUDClient, _ParentAclRestClient):
         :param numRelatedFiles: number of related files
         :param annotation: annotation, e.g: key1=value(;key2=value)
         :param default: calculate default stats (bool -> false (default))
-        :param field: list of fields separated by semicolons, e.g.: studies;type. 
+        :param field: list of fields separated by semicolons, e.g.: studies;type.
             For nested fields use >>, e.g.: studies>>biotype;type;numSamples[0..10]:1
         """
 
-        return self._get('stats', **kwargs)
+        return self._get('stats', **options)
 
-    def bioformats(self, **kwargs):
+    def bioformats(self, **options):
         """
         List of accepted file bioformats
         URL: /{apiVersion}/files/bioformats
         """
-        
-        return self._get('bioformats', **kwargs)
 
-    def formats(self, **kwargs):
+        return self._get('bioformats', **options)
+
+    def formats(self, **options):
         """
         List of accepted file formats
         URL: /{apiVersion}/files/formats
         """
 
-        return self._get('formats', **kwargs)
+        return self._get('formats', **options)
 
-    def search(self, **kwargs):
+    def search(self, **options):
         """
         File search method.
         URL: /{apiVersion}/files/search
@@ -69,10 +69,10 @@ class Files(_ParentBasicCRUDClient, _ParentAclRestClient):
         :param status: status
         :param directory: directory under which we want to look for files or folders
         :param creationDate: creation date (Format: yyyyMMddHHmmss. Examples: >2018, 2017-2018, <201805...)
-        :param modificationDate: modification date (Format: yyyyMMddHHmmss. 
+        :param modificationDate: modification date (Format: yyyyMMddHHmmss.
             Examples: >2018, 2017-2018, <201805...)
         :param description: description
-        :param tags: tags    
+        :param tags: tags
         :param size: size
         :param samples: comma separated list of sample ids
         :param job.id: job id that created the file(s) or folder(s)
@@ -90,21 +90,21 @@ class Files(_ParentBasicCRUDClient, _ParentAclRestClient):
         :param flattenAnnotations: flatten the annotations? (default = false)
         """
 
-        return self._get('search', **kwargs)
+        return self._get('search', **options)
 
-    def scan_folder(self, folder, **kwargs):
+    def scan_folder(self, folder, **options):
         """
         Scans a folder
         URL: /{apiVersion}/files/{folder}/scan
-        
+
         :param folder: Folder id, name or path. Paths must be separated by : instead of /
         :param study: Study [[user@]project:]study where study and project can be either the id or alias
         :param calculateChecksum: calculateChecksum (bool -> false (default))
         """
-        
-        return self._get('scan', query_id=folder, **kwargs)
 
-    def list_folder(self, folder, **kwargs):
+        return self._get('scan', query_id=folder, **options)
+
+    def list_folder(self, folder, **options):
         """
         List all the files inside the folder
         URL: /{apiVersion}/files/{folder}/list
@@ -118,22 +118,22 @@ class Files(_ParentBasicCRUDClient, _ParentAclRestClient):
         :param count: total number of results
         """
 
-        return self._get('list', query_id=folder, **kwargs)
+        return self._get('list', query_id=folder, **options)
 
-    def content(self, file, **kwargs):
+    def content(self, file, **options):
         """
         Show the content of a file (up to a limit)
         URL: /{apiVersion}/files/{file}/content
-        
+
         :param file: File id, name or path. Paths must be separated by : instead of /
         :param study: Study [[user@]project:]study where study and project can be either the id or alias
         :param start: start (default: -1 -> All file)
         :param limit: limit (default: -1 -> All file)
         """
 
-        return self._get('content', query_id=file, **kwargs)
+        return self._get('content', query_id=file, **options)
 
-    def grep(self, file, **kwargs):
+    def grep(self, file, **options):
         """
         Filter lines of the file containing a match of the pattern [NOT TESTED]
         URL: /{apiVersion}/files/{file}/grep
@@ -145,9 +145,9 @@ class Files(_ParentBasicCRUDClient, _ParentAclRestClient):
         :param multi: Return multiple matches (default = True)
         """
 
-        return self._get('grep', query_id=file, **kwargs)
+        return self._get('grep', query_id=file, **options)
 
-    def refresh(self, file, **kwargs):
+    def refresh(self, file, **options):
         """
         Refresh metadata from the selected file or folder. Return updated files.
         URL: /{apiVersion}/files/{file}/refresh
@@ -156,14 +156,14 @@ class Files(_ParentBasicCRUDClient, _ParentAclRestClient):
         :param study: Study [[user@]project:]study where study and project can be either the id or alias
         """
 
-        return self._get('refresh', query_id=file, **kwargs)
+        return self._get('refresh', query_id=file, **options)
 
-    def tree_folder(self, folder, **kwargs):
+    def tree_folder(self, folder, **options):
         """
         Obtain a tree view of the files and folders within a folder
         URL: /{apiVersion}/files/{folder}/tree
 
-        :param folder: Folder id, name or path. Paths must be separated by : instead of / 
+        :param folder: Folder id, name or path. Paths must be separated by : instead of /
             (default root -> ":")
         :param study: study [[user@]project:]study where study and project can be either the id or alias
         :param maxDepth: maximum depth to get files from (default maxDepth = 5)
@@ -172,9 +172,9 @@ class Files(_ParentBasicCRUDClient, _ParentAclRestClient):
         :param limit: [TO BE IMPLEMENTED] Number of results to be returned in the queries
         """
 
-        return self._get('tree', query_id=folder, **kwargs)
+        return self._get('tree', query_id=folder, **options)
 
-    def upload(self, data, **kwargs):
+    def upload(self, data, **options):
         """
         Resource to upload a file by chunks
         URL: /{apiVersion}/files/upload
@@ -183,17 +183,17 @@ class Files(_ParentBasicCRUDClient, _ParentAclRestClient):
         :param fileFormat: For existing Bioformats see files/formats
         :param bioformat: For existing Bioformats see files/bioformats
         :param study: study [[user@]project:]study where study and project can be either the id or alias
-        :param relativeFilePath: path within catalog where the file will be located 
+        :param relativeFilePath: path within catalog where the file will be located
             (default: root folder = ".")
         :param description: description
         :param parents: create the parent directories if they do not exist
         """
 
-        return self._post('upload', data=data, **kwargs)
+        return self._post('upload', data=data, **options)
 
-    def download(self, file, data, **kwargs):
+    def download(self, file, data, **options):
         """
-        Download file. The usage of /{file}/download webservice through Swagger is discouraged. 
+        Download file. The usage of /{file}/download webservice through Swagger is discouraged.
         An special DOWNLOAD permission is needed to download files from OpenCGA.
         URL: /{apiVersion}/files/{file}/download
 
@@ -201,5 +201,5 @@ class Files(_ParentBasicCRUDClient, _ParentAclRestClient):
         :param study: Study [[user@]project:]study where study and project can be either the id or alias
         """
 
-        return self._post('download', query_id=file, data=data, **kwargs)
+        return self._post('download', query_id=file, data=data, **options)
 
