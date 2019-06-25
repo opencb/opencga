a798475a6bf8db1ad567f7c372ba22f9bcbf09aa
pfurio <pedrofurio@gmail.com>
2019-06-25 10:16:08 +0100

    app: add missing migration method for 1.4.2

--- opencga-app/app/migration/v1.4.2/catalog/migration.js
+++ opencga-app/app/migration/v1.4.2/catalog/migration.js
@@ -51,3 +51,18 @@ migrateCollection("study", {}, {groups: 1}, function(bulk, doc) {
 
     bulk.find({"_id": doc._id}).updateOne({"$set": params});
 });
+
+migrateCollection("file", {"relatedFiles": {"$nin": ["", null, []]}}, {relatedFiles: 1}, function(bulk, doc) {
+    var relatedFiles = [];
+
+    for (var i = 0; i < doc.relatedFiles.length; i++) {
+        relatedFiles.push({
+            "relation": doc.relatedFiles[i].relation,
+            "file": {
+                "id": doc.relatedFiles[i].fileId,
+            }
+        });
+    }
+
+    bulk.find({"_id": doc._id}).updateOne({"$set": {"relatedFiles": relatedFiles}});
+});
