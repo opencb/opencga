7a6838415aeec38ac8ceb8978b366e6508ad2aa6
pfurio <pedrofurio@gmail.com>
2019-12-10 11:19:31 +0000

    app: add migration to create new admin user, closes #1425

--- opencga-app/app/scripts/migration/v2.0.0/catalog/migration.js
+++ opencga-app/app/scripts/migration/v2.0.0/catalog/migration.js
@@ -15,5 +15,48 @@ for (var i = 0; i < individuals.length(); i++) {
     }
 }
 
+// Add empty array of tags when tags = null
+db.getCollection("file").update({"tags": null}, {"$set": {"tags": []}}, {"multi": true});
+
 // Drop dataset collection
 db.getCollection("dataset").drop()
+
+// Add new opencga administrator user to user collection #1425
+var metadata = db.getCollection("metadata").findOne({});
+db.getCollection("user").insert({
+    "id" : "opencga",
+    "name" : "opencga",
+    "email" : metadata["admin"]["email"],
+    "password" : metadata["admin"]["password"],
+    "organization" : "",
+    "account" : {
+        "type" : "ADMINISTRATOR",
+        "creationDate" : metadata["creationDate"],
+        "expirationDate" : "",
+        "authOrigin" : null,
+        "authentication" : {
+            "id" : "internal",
+            "application" : false
+        }
+    },
+    "status" : {
+        "name" : "READY",
+        "date" : metadata["creationDate"],
+        "message" : ""
+    },
+    "lastModified" : metadata["creationDate"],
+    "size" : -1,
+    "quota" : -1,
+    "projects" : [ ],
+    "tools" : [ ],
+    "configs" : {
+        "filters" : [ ]
+    },
+    "attributes" : {
+    }
+});
+// Remove from metadata collection the admin credentials
+db.getCollection("metadata").update({}, {"$unset": {"admin.email": "", "admin.password": ""}})
+
+
+// TODO: Add indexes for new "deleted" collections
