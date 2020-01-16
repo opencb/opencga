ef9f09abce009529146dbfaad938d50407410c52
dapregi <dperezgil89@gmail.com>
2020-01-16 16:27:30 +0000

    client: created rest clients with new python rest client creator

--- opencga-client/src/main/python/pyOpenCGA/pyopencga/rest_clients/jobs_client.py
+++ opencga-client/src/main/python/pyOpenCGA/pyopencga/rest_clients/jobs_client.py
@@ -14,23 +14,23 @@ class Jobs(_ParentRestClient):
 
     def update(self, jobs, data=None, **options):
         """
-        Update some job attributes
+        Update some job attributes.
         PATH: /{apiVersion}/jobs/{jobs}/update
 
-        :param str jobs: Comma separated list of job IDs or UUIDs up to a maximum of 100
-        :param str study: Study [[user@]project:]study where study and project can be either the ID or UUID
-        :param dict data: params
+        :param str jobs: Comma separated list of job IDs or UUIDs up to a maximum of 100.
+        :param str study: Study [[user@]project:]study where study and project can be either the ID or UUID.
+        :param dict data: params.
         """
 
         return self._post('update', query_id=jobs, data=data, **options)
 
-    def create(self, data, **options):
+    def create(self, data=None, **options):
         """
-        Register an executed job with POST method
+        Register an executed job with POST method.
         PATH: /{apiVersion}/jobs/create
 
-        :param str study: Study [[user@]project:]study where study and project can be either the ID or UUID
-        :param dict data: job
+        :param str study: Study [[user@]project:]study where study and project can be either the ID or UUID.
+        :param dict data: job.
         """
 
         return self._post('create', data=data, **options)
@@ -40,73 +40,74 @@ class Jobs(_ParentRestClient):
         Return the acl of the job. If member is provided, it will only return the acl for the member.
         PATH: /{apiVersion}/jobs/{jobs}/acl
 
-        :param str jobs: Comma separated list of job IDs or UUIDs up to a maximum of 100
-        :param str member: User or group id
-        :param bool silent: Boolean to retrieve all possible entries that are queried for, false to raise an exception whenever one of the entries looked for cannot be shown for whichever reason
+        :param str jobs: Comma separated list of job IDs or UUIDs up to a maximum of 100.
+        :param str member: User or group id.
+        :param bool silent: Boolean to retrieve all possible entries that are queried for, false to raise an exception whenever one of the entries looked for cannot be shown for whichever reason.
         """
 
         return self._get('acl', query_id=jobs, **options)
 
-    def update_acl(self, members, data, **options):
+    def update_acl(self, members, data=None, **options):
         """
-        Update the set of permissions granted for the member
+        Update the set of permissions granted for the member.
         PATH: /{apiVersion}/jobs/acl/{members}/update
 
-        :param str members: Comma separated list of user or group ids
-        :param dict data: JSON containing the parameters to add ACLs
+        :param str members: Comma separated list of user or group ids.
+        :param dict data: JSON containing the parameters to add ACLs.
         """
 
         return self._post('update', query_id=members, data=data, **options)
 
     def info(self, jobs, **options):
         """
-        Get job information
+        Get job information.
         PATH: /{apiVersion}/jobs/{jobs}/info
 
-        :param str include: Fields included in the response, whole JSON path must be provided
-        :param str exclude: Fields excluded in the response, whole JSON path must be provided
-        :param str jobs: Comma separated list of job IDs or UUIDs up to a maximum of 100
-        :param str study: Study [[user@]project:]study where study and project can be either the ID or UUID
-        :param bool deleted: Boolean to retrieve deleted jobs
+        :param str include: Fields included in the response, whole JSON path must be provided.
+        :param str exclude: Fields excluded in the response, whole JSON path must be provided.
+        :param str jobs: Comma separated list of job IDs or UUIDs up to a maximum of 100.
+        :param str study: Study [[user@]project:]study where study and project can be either the ID or UUID.
+        :param bool deleted: Boolean to retrieve deleted jobs.
         """
 
         return self._get('info', query_id=jobs, **options)
 
     def delete(self, jobs, **options):
         """
-        Delete existing jobs
+        Delete existing jobs.
         PATH: /{apiVersion}/jobs/{jobs}/delete
 
-        :param str study: Study [[user@]project:]study where study and project can be either the ID or UUID
-        :param str jobs: Comma separated list of job ids
+        :param str study: Study [[user@]project:]study where study and project can be either the ID or UUID.
+        :param str jobs: Comma separated list of job ids.
         """
 
         return self._delete('delete', query_id=jobs, **options)
 
     def search(self, **options):
         """
-        Job search method
+        Job search method.
         PATH: /{apiVersion}/jobs/search
 
-        :param str include: Fields included in the response, whole JSON path must be provided
-        :param str exclude: Fields excluded in the response, whole JSON path must be provided
-        :param int limit: Number of results to be returned
-        :param int skip: Number of results to skip
+        :param str include: Fields included in the response, whole JSON path must be provided.
+        :param str exclude: Fields excluded in the response, whole JSON path must be provided.
+        :param int limit: Number of results to be returned.
+        :param int skip: Number of results to skip.
         :param bool count: Get the total number of results matching the query. Deactivated by default.
-        :param str study: Study [[user@]project:]study where study and project can be either the ID or UUID
-        :param str name: Job name
-        :param str tool: Tool executed by the job
-        :param str user: User that created the job
-        :param str priority: Priority of the job
-        :param str status: Job status
-        :param str creation_date: Creation date. Format: yyyyMMddHHmmss. Examples: >2018, 2017-2018, <201805
-        :param str modification_date: Modification date. Format: yyyyMMddHHmmss. Examples: >2018, 2017-2018, <201805
-        :param bool visited: Visited status of job
-        :param str tags: Job tags
+        :param str study: Study [[user@]project:]study where study and project can be either the ID or UUID.
+        :param str name: Job name.
+        :param str tool: Tool executed by the job.
+        :param str user: User that created the job.
+        :param str priority: Priority of the job.
+        :param str status: Job status.
+        :param str creation_date: Creation date. Format: yyyyMMddHHmmss. Examples: >2018, 2017-2018, <201805.
+        :param str modification_date: Modification date. Format: yyyyMMddHHmmss. Examples: >2018, 2017-2018, <201805.
+        :param bool visited: Visited status of job.
+        :param str tags: Job tags.
         :param str input: Comma separated list of file ids used as input.
         :param str output: Comma separated list of file ids used as output.
-        :param str release: Release when it was created
-        :param bool deleted: Boolean to retrieve deleted jobs
+        :param str release: Release when it was created.
+        :param bool deleted: Boolean to retrieve deleted jobs.
         """
 
         return self._get('search', **options)
+
