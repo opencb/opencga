26cefd49e59ba4b6449f8dd073fb4e967a497242
Joaquín Tárraga Giménez <joaquintarraga@gmail.com>
2020-02-24 10:54:03 +0000

    analysis: improve IBD computation for relatedness analysis, #1521

--- opencga-analysis/src/main/java/org/opencb/opencga/analysis/variant/relatedness/IBDComputation.java
+++ opencga-analysis/src/main/java/org/opencb/opencga/analysis/variant/relatedness/IBDComputation.java
@@ -1,5 +1,6 @@
 package org.opencb.opencga.analysis.variant.relatedness;
 
+import org.apache.commons.io.FileUtils;
 import org.opencb.commons.datastore.core.Query;
 import org.opencb.commons.datastore.core.QueryOptions;
 import org.opencb.commons.utils.DockerUtils;
@@ -24,7 +25,6 @@ public class IBDComputation {
 
     public static File compute(String study, List<String> samples, Path outDir, VariantStorageManager storageManager, String token)
             throws ToolException {
-        String filename = "variants";
         AbstractMap.SimpleEntry<String, String> outputBinding = new AbstractMap.SimpleEntry<>(outDir.toAbsolutePath().toString(),
                 "/data/output");
 
@@ -36,27 +36,33 @@ public class IBDComputation {
         //.append(VariantQueryParam.FILTER.key(), "PASS")
 
         // First, autosomal chromosomes
-        File tpedAutosomeFile = outDir.resolve(filename + ".autosome.tped").toFile();
-        File tfamAutosomeFile = outDir.resolve(filename + ".autosome.tfam").toFile();
-        query.put(VariantQueryParam.REGION.key(), Arrays.asList(new String[]{"22"}));
-        //query.put(VariantQueryParam.STATS_MAF.key(), "ALL>0.3");
+        File tpedAutosomeFile = outDir.resolve("autosome.tped").toFile();
+        File tfamAutosomeFile = outDir.resolve("autosome.tfam").toFile();
+        query.put(VariantQueryParam.REGION.key(), Arrays.asList("1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22".split(",")));
+        query.put(VariantQueryParam.STATS_MAF.key(), "ALL>0.3");
         exportData(tpedAutosomeFile, tfamAutosomeFile, query, storageManager, token);
-        pruneVariants(filename + ".autosome", outputBinding);
+        if (tpedAutosomeFile.exists() && tpedAutosomeFile.length() > 0) {
+            pruneVariants("autosome", outputBinding);
+        }
 
         // First, X chromosome
-        File tpedXFile = outDir.resolve(filename + ".x.tped").toFile();
-        File tfamXFile = outDir.resolve(filename + ".x.tfam").toFile();
+        File tpedXFile = outDir.resolve("x.tped").toFile();
+        File tfamXFile = outDir.resolve("x.tfam").toFile();
         query.put(VariantQueryParam.REGION.key(), "X");
-        //query.put(VariantQueryParam.STATS_MAF.key(), "ALL>0.05");
+        query.put(VariantQueryParam.STATS_MAF.key(), "ALL>0.05");
         exportData(tpedXFile, tfamXFile, query, storageManager, token);
-        pruneVariants(filename + ".x", outputBinding);
+        if (tpedXFile.exists() && tpedXFile.length() > 0) {
+            pruneVariants("x", outputBinding);
+        }
 
-        // For performance purposes, append the X tped file to autosome tped file
-        // (since tfam files contain sample information, X tfam file is equal to autosome tfam file)
-        appendFile(tpedAutosomeFile, tpedXFile);
+        // Append files:
+        //   - the x.tped file to autosome.tped file (since tfam files contain the same sample information)
+        //   - the x.prune.out file to autosome.prune.out file
+        appendFile(tpedXFile.getAbsolutePath(), tpedAutosomeFile.getAbsolutePath());
+        appendFile(outDir.resolve("x.prune.out").toString(), outDir.resolve("autosome.prune.out").toString());
 
-        // runIBD and return the result file
-        return runIBD(filename, outputBinding);
+        // runIBD and return the result file (now autosome-file comprises X chromosome too)
+        return runIBD("autosome", outputBinding);
     }
 
 
@@ -75,25 +81,26 @@ public class IBDComputation {
 
     private static void pruneVariants(String basename, AbstractMap.SimpleEntry<String, String> outputBinding) throws ToolException {
         // Variant pruning using PLINK in docker
-        String plinkParams = "--noweb -tfile /data/output/" + basename + " --indep 50 5 2 --not-chr 24";
+        String plinkParams = "plink --tfile /data/output/" + basename + " --indep 50 5 2 --out /data/output/" + basename;
         try {
-            String cmdline = DockerUtils.run(PlinkWrapperAnalysis.PLINK_DOCKER_IMAGE, null, outputBinding, plinkParams, null);
-            System.out.println("Docker command line: " + cmdline);
+            DockerUtils.run(PlinkWrapperAnalysis.PLINK_DOCKER_IMAGE, null, outputBinding, plinkParams, null);
         } catch (IOException e) {
             throw new ToolException(e);
         }
     }
 
-    private static void appendFile(File file, File filetoAppend) throws ToolException {
-        try (FileWriter f = new FileWriter(file.getAbsolutePath(), true);
-             PrintWriter p = new PrintWriter(new BufferedWriter(f))) {
-            FileInputStream fis = new FileInputStream(filetoAppend.getAbsolutePath());
-            Scanner sc = new Scanner(fis);
-            while(sc.hasNextLine()) {
-                p.println(sc.nextLine());
+    private static void appendFile(String srcFilename, String destFilename) throws ToolException {
+        if (new File(srcFilename).exists()) {
+            try (FileWriter f = new FileWriter(destFilename, true);
+                 PrintWriter p = new PrintWriter(new BufferedWriter(f))) {
+                FileInputStream fis = new FileInputStream(srcFilename);
+                Scanner sc = new Scanner(fis);
+                while (sc.hasNextLine()) {
+                    p.println(sc.nextLine());
+                }
+            } catch (IOException e) {
+                throw new ToolException(e);
             }
-        } catch (IOException e) {
-            throw new ToolException(e);
         }
     }
 
@@ -107,18 +114,19 @@ public class IBDComputation {
             Scanner sc = new Scanner(fis);
             while(sc.hasNextLine()) {
                 String[] splits = sc.nextLine().split("\t");
+                String[] varSplits = splits[1].split(":");
+
                 int a1 = 0;
                 int a2 = 0;
                 for (int i = 4; i < splits.length; i++) {
-                    if (splits[i].equals("0")) {
+                    if (splits[i].equals(varSplits[2])) {
                         a1++;
-                    } else {
+                    } else if (splits[i].equals(varSplits[3])) {
                         a2++;
                     }
                 }
-                String[] varSplits = splits[1].split(":");
                 p.println(splits[0] + "\t" + splits[1] + "\t" + varSplits[2] + "\t" + varSplits[3] + "\t" + (1.0d * a2 / (a1 + a2)) + "\t"
-                + (a1 + a2));
+                        + (a1 + a2));
             }
         } catch (IOException e) {
             throw new ToolException(e);
@@ -130,21 +138,29 @@ public class IBDComputation {
 
         // Calculate allele frequencies to pass to PLINK
         // --read-freq <.freq filename>
+        String readFreq = "";
         String mafFilename = "maf.freq";
         calculateMAF(outputBinding.getKey() + "/" + basename + ".tped", outputBinding.getKey() + "/" + mafFilename);
+        if (new File(outputBinding.getKey() + "/" + mafFilename).exists()) {
+            readFreq = " --read-freq /data/output/" + mafFilename;
+        }
 
         // Run IBD using PLINK in docker
-        String plinkParams = "--noweb -tfile /data/output/" + basename + " --genome rel-check --read-freq /data/output/" + mafFilename
-                + " --out /data/output/" + outFilename;
+        String exclude = "";
+        File pruneOutFile = new File(outputBinding.getKey() + "/" + basename + ".prune.out");
+        if (pruneOutFile.exists() && pruneOutFile.length() > 0) {
+            exclude = " --exclude /data/output/" + basename + ".prune.out";
+        }
+        String plinkParams = "plink --tfile /data/output/" + basename + " --genome rel-check " + readFreq + exclude + " --out /data/output/"
+                + outFilename;
         try {
             String cmdline = DockerUtils.run(PlinkWrapperAnalysis.PLINK_DOCKER_IMAGE, null, outputBinding, plinkParams, null);
-            System.out.println("Docker command line: " + cmdline);
         } catch (IOException e) {
             throw new ToolException(e);
         }
 
         // Check output file
-        File outFile = new File(outputBinding.getKey() + "/" + basename + ".genome");
+        File outFile = new File(outputBinding.getKey() + "/" + outFilename + ".genome");
         if (!outFile.exists()) {
             throw new ToolException("Something wrong executing relatedness analysis (i.e., IBD/IBS computation) in PLINK docker.");
         }
