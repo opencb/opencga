6eb5e2abea669e8c28feaa469d57cd2e14aa4994
Joaquín Tárraga Giménez <joaquintarraga@gmail.com>
2020-05-12 20:04:59 +0200

    analysis: add watches to monitor mutational-signature times

--- opencga-analysis/src/main/java/org/opencb/opencga/analysis/variant/mutationalSignature/MutationalSignatureLocalAnalysisExecutor.java
+++ opencga-analysis/src/main/java/org/opencb/opencga/analysis/variant/mutationalSignature/MutationalSignatureLocalAnalysisExecutor.java
@@ -20,6 +20,7 @@ import htsjdk.samtools.reference.BlockCompressedIndexedFastaSequenceFile;
 import htsjdk.samtools.reference.FastaSequenceIndex;
 import htsjdk.samtools.reference.ReferenceSequence;
 import htsjdk.samtools.util.GZIIndex;
+import org.apache.commons.lang3.time.StopWatch;
 import org.opencb.biodata.models.variant.Variant;
 import org.opencb.commons.datastore.core.Query;
 import org.opencb.commons.datastore.core.QueryOptions;
@@ -52,29 +53,59 @@ public class MutationalSignatureLocalAnalysisExecutor extends MutationalSignatur
     public void run() throws ToolException {
         Map<String, Map<String, Double>> countMap = initFreqMap();
         try {
+            long prepIteratorTime = 0;
+            long prepFaiTime = 0;
+            long iteratorTime = 0;
+            long faiTime = 0;
+            long writeCountTime = 0;
+            long rTime = 0;
+            StopWatch watch = new StopWatch();
+            StopWatch loopWatch = new StopWatch();
+            StopWatch totalWatch = new StopWatch();
+            totalWatch.start();
+
             VariantStorageManager storageManager = getVariantStorageManager();
 
             // Compute signature profile: contextual frequencies of each type of base substitution
 
+            watch.start();
             Query query = new Query()
                     .append(VariantQueryParam.STUDY.key(), getStudy())
                     .append(VariantQueryParam.SAMPLE.key(), getSampleName())
                     //.append(VariantQueryParam.FILTER.key(), "PASS")
                     .append(VariantQueryParam.TYPE.key(), "SNV");
 
-            QueryOptions queryOptions = new QueryOptions(QueryOptions.INCLUDE, "chromosome,start,end,reference,alternate");
+            QueryOptions queryOptions = new QueryOptions(QueryOptions.INCLUDE, "id");
 
             VariantDBIterator iterator = storageManager.iterator(query, queryOptions, getToken());
+            watch.stop();
+            prepIteratorTime += watch.getTime();
+            watch.reset();
 
             // Read mutation context from reference genome (.gz, .gz.fai and .gz.gzi files)
+            watch.start();
             String base = getRefGenomePath().toAbsolutePath().toString();
             BlockCompressedIndexedFastaSequenceFile indexed = new BlockCompressedIndexedFastaSequenceFile(getRefGenomePath(),
                     new FastaSequenceIndex(new File(base + ".fai")), GZIIndex.loadIndex(Paths.get(base + ".gzi")));
+            watch.stop();
+            prepFaiTime += watch.getTime();
+            watch.reset();
 
+            long count = 0;
+            long ko = 0;
+            long contextCount = 0;
+            loopWatch.start();
             while (iterator.hasNext()) {
+                count++;
+                watch.start();
                 Variant variant = iterator.next();
+                watch.stop();
+                iteratorTime += watch.getTime();
+                watch.reset();
                 String key = variant.getReference() + ">" + variant.getAlternate();
                 if (countMap.containsKey(key)) {
+                    watch.start();
+                    contextCount++;
                     try {
                         ReferenceSequence refSeq = indexed.getSubsequenceAt(variant.getChromosome(), variant.getStart() - 1,
                                 variant.getEnd() + 1);
@@ -85,14 +116,24 @@ public class MutationalSignatureLocalAnalysisExecutor extends MutationalSignatur
                         }
                     } catch (Exception e) {
                         System.out.println("Error getting context sequence for variant " + variant.toStringSimple() + ": " + e.getMessage());
+                        ko++;
                     }
+                    watch.stop();
+                    faiTime += watch.getTime();
+                    watch.reset();
                 }
             }
+            loopWatch.stop();
 
             // Write context
+            watch.start();
             writeCountMap(countMap, getOutDir().resolve(CONTEXT_FILENAME).toFile());
+            watch.stop();
+            writeCountTime += watch.getTime();
+            watch.reset();
 
             // Execute R script in docker
+            watch.start();
             String rScriptPath = getExecutorParams().getString("opencgaHome") + "/analysis/R/" + getToolId();
             List<AbstractMap.SimpleEntry<String, String>> inputBindings = new ArrayList<>();
             inputBindings.add(new AbstractMap.SimpleEntry<>(rScriptPath, "/data/input"));
@@ -101,7 +142,27 @@ public class MutationalSignatureLocalAnalysisExecutor extends MutationalSignatur
             String scriptParams = "R CMD Rscript --vanilla /data/input/mutational-signature.r /data/output/" + CONTEXT_FILENAME + " "
                     + "/data/output/" + MutationalSignatureAnalysis.SIGNATURES_FILENAME + " /data/output";
             String cmdline = DockerUtils.run(R_DOCKER_IMAGE, inputBindings, outputBinding, scriptParams, null);
+            watch.stop();
+            rTime += watch.getTime();
+            watch.reset();
+
+            totalWatch.stop();
+
             System.out.println("Docker command line: " + cmdline);
+
+            System.out.println("number of variants = " + count);
+            System.out.println("number of errors = " + ko);
+            System.out.println("Context count = " + contextCount);
+            System.out.println("prepare iterator time = " + prepIteratorTime);
+            System.out.println("prepare FAI time = " + prepFaiTime);
+            System.out.println("iterator time = " + iteratorTime);
+            System.out.println("FAI time = " + faiTime);
+            System.out.println("loop watch (iterator time + FAI time + skip context) = " + loopWatch.getTime());
+            System.out.println("write counts time = " + writeCountTime);
+            System.out.println("R script time = " + rTime);
+            System.out.println("Sum time (prep. iterator, pre. FAI, iterator, FAI, write, R) = "
+                    + (prepIteratorTime + prepFaiTime + iteratorTime + faiTime + writeCountTime + rTime));
+            System.out.println("Total time = " + totalWatch.getTime());
         } catch (Exception e) {
             throw new ToolExecutorException(e);
         }
