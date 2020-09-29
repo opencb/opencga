fa9aac37ce8915c410568da4e46f512877f57457
pfurio <pedrofurio@gmail.com>
2020-09-29 15:02:46 +0200

    client: update Java clients

--- opencga-app/app/misc/clients/rest_client_generator.py
+++ opencga-app/app/misc/clients/rest_client_generator.py
@@ -24,7 +24,8 @@ class RestClientGenerator(ABC):
 
         self.endpoints = {
             'users/{user}/filters/{filterId}/update': {'method_name': 'update_filter'},
-            'ga4gh/reads/{study}/{file}': {'method_name': 'fetch_reads'}
+            'ga4gh/reads/{study}/{file}': {'method_name': 'fetch_reads'},
+            'analysis/clinical/{clinicalAnalysis}/interpretation/{interpretationId}/merge': {'method_name': 'merge_interpretation'}
         }
         self.categories = {
             'Users': 'User',
