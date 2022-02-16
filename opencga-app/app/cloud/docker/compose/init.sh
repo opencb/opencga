752049cdf8ebd1f05cb6007f07216bc06915c93c
imedina <igmecas@gmail.com>
2022-02-16 04:45:05 +0000

    app: docker compose work

--- opencga-app/app/cloud/docker/compose/init.sh
+++ opencga-app/app/cloud/docker/compose/init.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -x
 
+# Mongo client is mounted from local system for now.
+/usr/bin/mongo /opt/opencga/conf/mongodb-replica-set-init.js
+
 # Deploy new catalog
 sleep 10
 echo "Installing OpenCGA"
