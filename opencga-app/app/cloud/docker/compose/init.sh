3a814f54e2d7d2a92f205c8cbf41c0de474efacd
juanfeSanahuja <juanfe.sanahuja@zettagenomics.com>
2022-02-17 16:16:43 +0100

    app: Volume for persitence mongo and solr

--- opencga-app/app/cloud/docker/compose/init.sh
+++ opencga-app/app/cloud/docker/compose/init.sh
@@ -1,10 +1,15 @@
 #!/bin/sh -x
 
 # Mongo client is mounted from local system for now.
-mongo /opt/opencga/conf/mongodb-replica-set-init.js
+sleep 3
+echo "================================================================================================="
+mongo mongodb://mongodb:27017 /opt/mongodb/mongodb-replica-set-init.js
+
+echo "================================================================================================="
 
 # Deploy new catalog
-sleep 10
+sleep 5
 echo "Installing OpenCGA"
-echo "adminOpencga2021." | ./opencga-admin.sh catalog install --force
+echo "adminOpencga2021." | ./opencga-admin.sh catalog install
 
+echo "===================================== OK ====================================================="
