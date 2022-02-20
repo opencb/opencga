f1d0aa5164db1a5130971828dc414cf53ebcba27
juanfeSanahuja <juanfe.sanahuja@zettagenomics.com>
2022-02-20 20:36:52 +0100

    app: persions for solr directory

--- opencga-app/app/cloud/docker/compose/start.sh
+++ opencga-app/app/cloud/docker/compose/start.sh
@@ -2,9 +2,13 @@
 
 mkdir ~/opencga-docker-data ~/opencga-docker-data/mongodb ~/opencga-docker-data/solr
 
-export SOLR_USER="$(id -u -n)"
-export SOLR_UID="$(id -u)"
+chown -R 1001:1001 ~/opencga-docker-data/solr
+#chown solr:solr ~/opencga-docker-data/solr
+#chmod 777 ~/opencga-docker-data/solr
+#export SOLR_USER="$(id -u -n)"
+#export SOLR_UID="$(id -u)"
 
-echo $SOLR_USER
-echo $SOLR_UID
+#echo $SOLR_USER
+#echo $SOLR_UID
+#docker-compose config
 docker-compose up
