e4d67dde35f784e7646c1431b7e45520d3e41b81
Jacobo Coll Moragón <jacobo167@gmail.com>
2022-03-15 17:52:30 +0000

    storage: Ensure sorted output at BreakendVariantQueryExecutor

--- opencga-storage/opencga-storage-core/src/main/java/org/opencb/opencga/storage/core/variant/query/executors/BreakendVariantQueryExecutor.java
+++ opencga-storage/opencga-storage-core/src/main/java/org/opencb/opencga/storage/core/variant/query/executors/BreakendVariantQueryExecutor.java
@@ -182,19 +182,30 @@ public class BreakendVariantQueryExecutor extends VariantQueryExecutor {
                 throw new VariantQueryException("Unable to find mate of variant " + variant + " with MATEID=" + mateid);
             }
 
-            // Check for duplicated pairs
-            if (validPair(filter, variant, mateVariant)) {
-                variantPairs.add(variant);
-                variantPairs.add(mateVariant);
-            }
+            addPair(filter, variantPairs, variant, mateVariant);
         }
         return variantPairs;
     }
 
+    private void addPair(Predicate<Variant> filter, List<Variant> variantPairs, Variant variant, Variant mateVariant) {
+        // Check for duplicated pairs
+        if (VariantDBIterator.VARIANT_COMPARATOR.compare(variant, mateVariant) > 0) {
+            // The mate variant is "before" the main variant
+            // This pair might be discarded if the mate matches the given query
+            if (!filter.test(mateVariant)) {
+                // Otherwise, both variants are added to the list of variant pairs.
+                // But first the "mate" to respect order
+                variantPairs.add(mateVariant);
+                variantPairs.add(variant);
+            }
+        } else {
+            variantPairs.add(variant);
+            variantPairs.add(mateVariant);
+        }
+    }
+
     private boolean validPair(Predicate<Variant> filter, Variant variant, Variant mateVariant) {
         if (VariantDBIterator.VARIANT_COMPARATOR.compare(variant, mateVariant) > 0) {
-            // If the mate variant is "before" the main variant
-            // This pair might be discarded if the mate matches the given query
             return !filter.test(mateVariant);
         }
         return true;
