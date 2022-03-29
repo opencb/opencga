cc6501233f52e26b9b0e3c204e90ff217bf777f9
pfurio <pedrofurio@gmail.com>
2022-03-29 16:01:03 +0200

    app: manually delete unused annotationsets, #TASK-537

--- opencga-app/src/main/java/org/opencb/opencga/app/migrations/v2_2_0/catalog/DeleteUnusedVariableSets.java
+++ opencga-app/src/main/java/org/opencb/opencga/app/migrations/v2_2_0/catalog/DeleteUnusedVariableSets.java
@@ -1,16 +1,20 @@
 package org.opencb.opencga.app.migrations.v2_2_0.catalog;
 
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.model.Projections;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.opencb.opencga.catalog.db.mongodb.MongoDBAdaptorFactory;
 import org.opencb.opencga.catalog.db.mongodb.converters.StudyConverter;
 import org.opencb.opencga.catalog.exceptions.CatalogException;
 import org.opencb.opencga.catalog.migration.Migration;
 import org.opencb.opencga.catalog.migration.MigrationTool;
 import org.opencb.opencga.core.models.study.Study;
+import org.opencb.opencga.core.models.study.VariableSet;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Migration(id = "delete_unused_variablesets",
@@ -37,16 +41,50 @@ public class DeleteUnusedVariableSets extends MigrationTool {
 
         for (Study study : studyList) {
             logger.info("Deleting VariableSets from study '{}'", study.getFqn());
-            delete(study.getFqn(), "opencga_alignment_samtools_flagstat");
-            delete(study.getFqn(), "opencga_alignment_stats");
-            delete(study.getFqn(), "opencga_sample_qc");
-            delete(study.getFqn(), "opencga_file_variant_stats");
+            delete(study, "opencga_alignment_samtools_flagstat");
+            delete(study, "opencga_alignment_stats");
+            delete(study, "opencga_sample_qc");
+            delete(study, "opencga_file_variant_stats");
         }
     }
 
-    private void delete(String studyFqn, String variableSetId) {
+    private void deleteAnnotationSets(VariableSet variableSet, String collection) {
+        MongoCollection<Document> mongoCollection = getMongoCollection(collection);
+        Bson query = new Document()
+                .append("$or", Arrays.asList(
+                    new Document("customAnnotationSets.vs", variableSet.getUid()),
+                    new Document("customInternalAnnotationSets.vs", variableSet.getUid())
+                ));
+        Bson update = new Document()
+                .append("$pull", new Document()
+                        .append("customAnnotationSets", new Document("vs", variableSet.getUid())))
+                        .append("customInternalAnnotationSets", new Document("vs", variableSet.getUid()))
+                .append("$unset", new Document()
+                        .append("_vsMap." + variableSet.getUid(), "")
+                        .append("_ivsMap." + variableSet.getUid(), "")
+                );
+        mongoCollection.updateMany(query, update);
+    }
+
+    private void delete(Study study, String variableSetId) {
+        // Find variable set
+        VariableSet variableSet = null;
+        for (VariableSet tmpVariableSet : study.getVariableSets()) {
+            if (tmpVariableSet.getId().equals(variableSetId)) {
+                variableSet = tmpVariableSet;
+            }
+        }
+        if (variableSet == null) {
+            return;
+        }
+        deleteAnnotationSets(variableSet, MongoDBAdaptorFactory.FILE_COLLECTION);
+        deleteAnnotationSets(variableSet, MongoDBAdaptorFactory.SAMPLE_COLLECTION);
+        deleteAnnotationSets(variableSet, MongoDBAdaptorFactory.INDIVIDUAL_COLLECTION);
+        deleteAnnotationSets(variableSet, MongoDBAdaptorFactory.FAMILY_COLLECTION);
+        deleteAnnotationSets(variableSet, MongoDBAdaptorFactory.COHORT_COLLECTION);
+
         try {
-            catalogManager.getStudyManager().deleteVariableSet(studyFqn, variableSetId, true, token);
+            catalogManager.getStudyManager().deleteVariableSet(study.getFqn(), variableSetId, true, token);
         } catch (CatalogException e) {
             logger.warn(e.getMessage());
         }
