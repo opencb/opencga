30fc6d7214e2e228f3264065c9791af30fd8cc09
JuanfeSanahuja <juanfe.sanahuja@zettagenomics.com>
2022-06-02 17:45:21 +0200

    server: add boolean param for unformatted json #TASK-949

--- opencga-server/src/main/java/org/opencb/opencga/server/rest/MetaWSServer.java
+++ opencga-server/src/main/java/org/opencb/opencga/server/rest/MetaWSServer.java
@@ -105,7 +105,7 @@ public class MetaWSServer extends OpenCGAWSServer {
     public Response model(@QueryParam("model") String modelStr) {
         OpenCGAResult<String> queryResult = null;
         try {
-            queryResult = new OpenCGAResult<>(0, Collections.emptyList(), 1, Collections.singletonList(DataModelsUtils.dataModelToJsonString(Class.forName(modelStr))), 1);
+            queryResult = new OpenCGAResult<>(0, Collections.emptyList(), 1, Collections.singletonList(DataModelsUtils.dataModelToJsonString(Class.forName(modelStr), false)), 1);
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
