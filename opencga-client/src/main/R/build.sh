83f53cbc9417741969290f98c8a1799ffee51918
imedina <igmecas@gmail.com>
2022-06-09 01:30:59 +0100

    cicd: cfreate new workflow to create r-builder docker image

--- opencga-client/src/main/R/build.sh
+++ opencga-client/src/main/R/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --mount type=bind,source=$1,target=/opt/opencga/R --mount type=bind,source=/tmp,target=/opt/opencga opencb/opencga-ext-tools:$2 R CMD build /opt/opencga/R
+docker run --mount type=bind,source=$1,target=/opt/opencga/R --mount type=bind,source=/tmp,target=/opt/opencga opencb/opencga-r-builder:1.0.0 R CMD build /opt/opencga/R
