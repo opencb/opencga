23b1b56c770d6ab69d1033ac952e2038398eb1fa
pfurio <pedrofurio@gmail.com>
2022-06-28 12:43:58 +0200

    app: replace job-depends-on for execution-depends-on

--- opencga-app/app/cloud/docker/opencga-demo/load-demo.sh
+++ opencga-app/app/cloud/docker/opencga-demo/load-demo.sh
@@ -21,13 +21,13 @@ echo "Creating demo@family:corpasome ...."
 ./opencga.sh files fetch --study 'demo@family:corpasome' --path 'data' --url 'http://resources.opencb.org/datasets/corpasome/data/quartet.variants.annotated.vcf.gz' \
     --job-id 'download_quartet.variants.annotated.vcf.gz'
 ./opencga.sh operations variant-index --file 'quartet.variants.annotated.vcf.gz' --family true \
-     --job-id 'variant_index' --job-depends-on 'download_quartet.variants.annotated.vcf.gz'
+     --job-id 'variant_index' --execution-depends-on 'download_quartet.variants.annotated.vcf.gz'
 ./opencga.sh operations variant-stats-index --study 'demo@family:corpasome' --cohort 'ALL' \
-     --job-id 'variant_stats' --job-depends-on 'variant_index'
+     --job-id 'variant_stats' --execution-depends-on 'variant_index'
 ./opencga.sh operations variant-annotation-index --project 'demo@family' \
-     --job-id 'variant_annotation' --job-depends-on 'variant_index'
+     --job-id 'variant_annotation' --execution-depends-on 'variant_index'
 ./opencga.sh operations variant-secondary-index --project 'demo@family' \
-     --job-id 'variant_secondary_index' --job-depends-on 'variant_stats,variant_annotation'
+     --job-id 'variant_secondary_index' --execution-depends-on 'variant_stats,variant_annotation'
 
 TEMPLATE=$(./opencga.sh studies templates-upload -i /opt/opencga/misc/demo/corpasome/ --study 'demo@family:corpasome')
 ./opencga.sh studies templates-run --id "$TEMPLATE" --study 'demo@family:corpasome' --overwrite
