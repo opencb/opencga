f65611b021934e8462c4939540bcab84f55bf5d6
Joaquín Tárraga Giménez <joaquintarraga@gmail.com>
2022-08-25 08:57:49 +0200

    analysis: use HTTP method POST insead of GET when annotating with CellBase, #TASK-1789

--- opencga-analysis/src/main/java/org/opencb/opencga/analysis/clinical/exomiser/ExomiserInterpretationAnalysis.java
+++ opencga-analysis/src/main/java/org/opencb/opencga/analysis/clinical/exomiser/ExomiserInterpretationAnalysis.java
@@ -218,7 +218,7 @@ public class ExomiserInterpretationAnalysis extends InterpretationAnalysis {
                     // Annotate variants
                     List<Variant> annotatedVariants = new ArrayList<>();
                     CellBaseDataResponse<Variant> cellBaseResponse = variantClient.annotate(variants, new QueryOptions("exclude",
-                            "expression"));
+                            "expression"), true);
                     if (cellBaseResponse != null && CollectionUtils.isNotEmpty(cellBaseResponse.getResponses())) {
                         for (CellBaseDataResult<Variant> response : cellBaseResponse.getResponses()) {
                             annotatedVariants.addAll(response.getResults());
