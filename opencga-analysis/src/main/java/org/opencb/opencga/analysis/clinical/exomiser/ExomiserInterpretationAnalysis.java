1a242700f3fdf382e8c48b8c266ad04449c271ef
Joaquín Tárraga Giménez <joaquintarraga@gmail.com>
2022-08-31 11:13:09 +0200

    analysis: save exomiser interpretation as secondary, #TASK-1789

--- opencga-analysis/src/main/java/org/opencb/opencga/analysis/clinical/exomiser/ExomiserInterpretationAnalysis.java
+++ opencga-analysis/src/main/java/org/opencb/opencga/analysis/clinical/exomiser/ExomiserInterpretationAnalysis.java
@@ -175,7 +175,7 @@ public class ExomiserInterpretationAnalysis extends InterpretationAnalysis {
         // Store interpretation analysis in DB
         try {
             catalogManager.getInterpretationManager().create(studyId, clinicalAnalysis.getId(), new Interpretation(interpretation),
-                    ParamUtils.SaveInterpretationAs.PRIMARY, QueryOptions.empty(), token);
+                    ParamUtils.SaveInterpretationAs.SECONDARY, QueryOptions.empty(), token);
         } catch (CatalogException e) {
             throw new ToolException("Error saving interpretation into database", e);
         }
