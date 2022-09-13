2f6b9049dd8976a1265ee0cbb057aa5ceb6995d6
JuanfeSanahuja <juanfe.sanahuja@zettagenomics.com>
2022-09-13 16:15:46 +0200

    core: AclEntryList does not extend ArrayList #TASK-1289

--- opencga-core/src/main/java/org/opencb/opencga/core/models/AclEntryList.java
+++ opencga-core/src/main/java/org/opencb/opencga/core/models/AclEntryList.java
@@ -1,6 +1,37 @@
 package org.opencb.opencga.core.models;
 
-import java.util.ArrayList;
 
-public class AclEntryList<E extends Enum<E>> extends ArrayList<AclEntry<E>> {
+import java.util.List;
+
+public class AclEntryList<E extends Enum<E>> {
+
+    protected String id;
+    private List<AclEntry<E>> acl;
+
+    public String getId() {
+        return id;
+    }
+
+    public AclEntryList setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("SampleAclEntryList{");
+        sb.append("id='").append(id).append('\'');
+        sb.append(", acl=").append(acl);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    public List<AclEntry<E>> getAcl() {
+        return acl;
+    }
+
+    public AclEntryList setAcl(List<AclEntry<E>> acl) {
+        this.acl = acl;
+        return this;
+    }
 }
