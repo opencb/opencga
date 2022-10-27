6485b13127094fbf603bc912734f485dc8d2153e
pfurio <pedrofurio@gmail.com>
2022-10-27 16:30:04 +0200

    app: add migration script, #TASK-2237

--- opencga-app/src/main/java/org/opencb/opencga/app/migrations/v2_4_10/catalog/AddMissingEndDateOnFinishedJobs.java
+++ opencga-app/src/main/java/org/opencb/opencga/app/migrations/v2_4_10/catalog/AddMissingEndDateOnFinishedJobs.java
@@ -1,9 +1,21 @@
 package org.opencb.opencga.app.migrations.v2_4_10.catalog;
 
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Projections;
+import com.mongodb.client.model.UpdateOneModel;
+import org.bson.Document;
+import org.opencb.opencga.catalog.db.mongodb.MongoDBAdaptorFactory;
 import org.opencb.opencga.catalog.migration.Migration;
 import org.opencb.opencga.catalog.migration.MigrationTool;
+import org.opencb.opencga.core.common.TimeUtils;
 
-@Migration(id = "AddMissingEndDateOnFinishedJobs" ,
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.mongodb.client.model.Filters.eq;
+
+@Migration(id = "add_missing_endDate_on_finished_jobs" ,
         description = "Add missing end date on finished jobs",
         version = "2.4.10",
         domain = Migration.MigrationDomain.CATALOG,
@@ -11,8 +23,47 @@ import org.opencb.opencga.catalog.migration.MigrationTool;
         date = 20221026
 )
 public class AddMissingEndDateOnFinishedJobs extends MigrationTool {
+
     @Override
     protected void run() throws Exception {
-        throw new IllegalStateException("PENDING");
+        migrateCollection(Arrays.asList(MongoDBAdaptorFactory.JOB_COLLECTION, MongoDBAdaptorFactory.DELETED_JOB_COLLECTION),
+                Filters.and(Arrays.asList(
+                        Filters.in("internal.status.id", Arrays.asList("DONE", "ERROR", "ABORTED", "UNKNOWN", "READY")),
+                        Filters.or(Arrays.asList(
+                                Filters.exists("execution.end", false),
+                                Filters.eq("execution.end", null))
+                        )
+                )),
+                Projections.include(Arrays.asList("execution", "_modificationDate")),
+                (document, bulk) -> {
+                    Document execution = document.get("execution", Document.class);
+                    if (execution != null) {
+                        // Add end date
+                        Object modificationDate = document.get("_modificationDate");
+                        if (modificationDate == null) {
+                            modificationDate = TimeUtils.getDate();
+                        }
+                        execution.put("end", modificationDate);
+
+                        // Add event
+                        List<Document> events = execution.getList("events", Document.class);
+                        if (events == null) {
+                            events = new ArrayList<>();
+                        } else {
+                            events = new ArrayList<>(events);
+                        }
+                        events.add(new Document()
+                                .append("type", "WARNING")
+                                .append("id", "missing-execution-end-date")
+                                .append("message", "Missing execution field 'end'. End date set as part of migration fix."));
+                        execution.put("events", events);
+
+                        bulk.add(new UpdateOneModel<>(
+                                eq("_id", document.get("_id")),
+                                new Document("$set", new Document("execution", execution)))
+                        );
+                    }
+                });
     }
+
 }
