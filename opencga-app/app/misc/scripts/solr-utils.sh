b5579be9c33c8ab4901cff69d5ca06f5f1b07cf4
Jacobo Coll Moragón <jacobo167@gmail.com>
2023-02-07 16:11:22 +0000

    app: Extend solr-utils.sh script. #TASK-2491

--- opencga-app/app/misc/scripts/solr-utils.sh
+++ opencga-app/app/misc/scripts/solr-utils.sh
@@ -15,18 +15,33 @@ Commands
   list - Fetch the names of the collections in the cluster.
              --filter              STRING     Regex to filter collections
 
+  delete - Delete a collection
+    * -c     --collection          STRING     The name of the collection to be deleted.
+
   status - Request the status and response of an already submitted Asynchronous Collection API call.
       --id   --request-id          STRING     Request id
+
+  del-status - Delete the status of an already submitted Asynchronous Collection API call.
+      --id   --request-id          STRING     Request id
+
+  wait   - Waits for an asynchronous job to finish
+      --id   --request-id          STRING     Request id
+             --fail-on-missing     FLAG       Fail if the request is not found
+             --sleep               NUMBER     Seconds to sleep between request status check
 
   backup - Backs up Solr collections and associated configurations to a shared filesystem - for example a Network File System.
     * -c     --collection          STRING     The name of the collection to be backed up.
       -n     --backup-name         STRING     Backup folder name at location. Default: {date}_{collection}
       -l     --location            DIRECTORY  Server side backup location. Default: /var/solr/data/backup/
+             --request-id          STRING     User defined request id to identify this asynchronous request.
+                                                By default, {backup-name}
 
   restore - Restores Solr indexes and associated configurations.
     * -c     --collection          STRING     The collection where the indexes will be restored into.
     * -n     --backup-name         STRING     The name of the existing backup that you want to restore.
       -l     --location            DIRECTORY  Server side backup location. Default: /var/solr/data/backup/
+             --request-id          STRING     User defined request id to identify this asynchronous request.
+                                                By default, {backup-name}_restore
 EOF
 }
 
@@ -63,6 +78,7 @@ function requiredDirectory() {
 
 function solr-admin() {
   RESOURCE=${1}
+  ARGS=""
   shift
   while [[ $# -gt 0 ]]; do
     key="$1"
@@ -134,25 +150,36 @@ function main() {
     SOLR_HOST="${SOLR_HOST}:8983"
   fi
 
-  ## Check solr is available
-  LIVE_NODES=$(solr-admin-collections action CLUSTERSTATUS | jq '.cluster.live_nodes | length')
-  if [ -z "$LIVE_NODES" ] || [ "$LIVE_NODES" -eq 0 ]; then
-    echo "Solr host '$SOLR_HOST' not available"
-    exit 1
-  fi
+  is_alive
 
   case "${COMMAND}" in
+    is-alive | isalive)
+      # Alive status already checked
+      exit 0
+      ;;
     list)
       list_collections ${args[@]}
+      ;;
+    delete)
+      delete_collection ${args[@]}
       ;;
     backup)
       backup_collection ${args[@]}
       ;;
+    wait)
+      wait_async_job ${args[@]}
+      ;;
+#    del-backup)
+#      del_backup_collection ${args[@]}
+#      ;;
     restore)
       restore_collection ${args[@]}
       ;;
     status)
       request_status ${args[@]}
+      ;;
+    del-status)
+      del_status ${args[@]}
       ;;
     *) # unknown command
       echo "Unknown command $COMMAND"
@@ -162,7 +189,21 @@ function main() {
   esac
 }
 
+function is_alive() {
+  SOLR_HOSTNAME=$(sed -E 's/(.*:\/\/)?([^:/?]*).*/\2/g' <<< "$SOLR_HOST")
 
+  if getent hosts "$SOLR_HOSTNAME" >/dev/null ; then
+      ## Check solr is available
+      LIVE_NODES=$(solr-admin-collections action CLUSTERSTATUS | jq '.cluster.live_nodes | length')
+      if [ -z "$LIVE_NODES" ] || [ "$LIVE_NODES" -eq 0 ]; then
+        echo "Solr host '$SOLR_HOST' not available"
+        exit 1
+      fi
+  else
+    echo "Solr host $SOLR_HOSTNAME not found"
+    exit 1;
+  fi
+}
 
 function list_collections() {
   COLL_FILTER=
@@ -185,6 +226,29 @@ function list_collections() {
   done
 
   solr-admin-collections action LIST | jq -r .collections[] | grep "${COLL_FILTER}"
+}
+
+function delete_collection() {
+  COLLECTION=
+
+  while [[ $# -gt 0 ]]; do
+    key="$1"
+    value="$2"
+    case $key in
+    --collection)
+      COLLECTION="$value"
+      shift # past argument
+      shift # past value
+      ;;
+    *) # unknown option
+      echo "Unknown option $key"
+      printUsage
+      exit 1
+      ;;
+    esac
+  done
+
+  solr-admin-collections action DELETE name "${COLLECTION:?}" | jq .
 }
 
 function request_status() {
@@ -212,11 +276,96 @@ function request_status() {
   solr-admin-collections action REQUESTSTATUS requestid "$REQUEST" | jq .
 }
 
+function wait_async_job() {
+  REQUEST=
+  FAIL_ON_MISSING="false"
+  SLEEP=60
+
+  while [[ $# -gt 0 ]]; do
+    key="$1"
+    value="$2"
+    case $key in
+    --id | --request-id)
+      REQUEST="$value"
+      shift # past argument
+      shift # past value
+      ;;
+    --fail-on-missing)
+      FAIL_ON_MISSING="true"
+      shift # past argument
+      ;;
+    --sleep)
+      SLEEP="$value"
+      shift # past argument
+      shift # past value
+      ;;
+    *) # unknown option
+      echo "Unknown option $key"
+      printUsage
+      exit 1
+      ;;
+    esac
+  done
+
+  while true; do
+    STATE=$(request_status --request-id "$REQUEST" | jq -r .status.state)
+    case "$STATE" in
+    completed)
+        echo "Solr job completed: $REQUEST"
+        return 0;
+      ;;
+    running|submitted)
+        echo "Wait for '$REQUEST' ..."
+        sleep "$SLEEP";
+      ;;
+    failed)
+        echo "Error executing: $REQUEST"
+        return 1;
+      ;;
+    notfound)
+        if [[ "$FAIL_ON_MISSING" == "true" ]]; then
+          echo "Error executing: $REQUEST"
+          return 1;
+        else
+          echo "Ignoring request not found: $REQUEST"
+          return 0;
+        fi
+      ;;
+    esac
+  done
+}
+
+function del_status() {
+  REQUEST=
+
+  while [[ $# -gt 0 ]]; do
+    key="$1"
+    value="$2"
+    case $key in
+    --id | --request-id)
+      REQUEST="$value"
+      shift # past argument
+      shift # past value
+      ;;
+    *) # unknown option
+      echo "Unknown option $key"
+      printUsage
+      exit 1
+      ;;
+    esac
+  done
+
+  requiredParam "--request-id" "$REQUEST"
+
+  solr-admin-collections action DELETESTATUS requestid "$REQUEST" | jq .
+}
+
 
 function backup_collection() {
   COLLECTION=
   LOCATION=
   BACKUP_NAME=
+  REQUEST_ID="${BACKUP_NAME}"
 
   while [[ $# -gt 0 ]]; do
     key="$1"
@@ -237,6 +386,11 @@ function backup_collection() {
       shift
       shift
       ;;
+    --request-id)
+      REQUEST_ID=$value
+      shift
+      shift
+      ;;
     *) # unknown option
       echo "Unknown option $key"
       printUsage
@@ -252,7 +406,7 @@ function backup_collection() {
       action BACKUP \
       name "${BACKUP_NAME}" \
       collection "${COLLECTION}" \
-      async "${BACKUP_NAME}" \
+      async "${REQUEST_ID}" \
       location "${LOCATION}" | jq .
 }
 
@@ -260,6 +414,7 @@ function restore_collection() {
   COLLECTION=
   LOCATION=
   BACKUP_NAME=
+  REQUEST_ID="${BACKUP_NAME}_restore"
 
   while [[ $# -gt 0 ]]; do
     key="$1"
@@ -280,6 +435,11 @@ function restore_collection() {
       shift
       shift
       ;;
+    --request-id)
+      REQUEST_ID=$value
+      shift
+      shift
+      ;;
     *) # unknown option
       echo "Unknown option $key"
       printUsage
@@ -295,7 +455,7 @@ function restore_collection() {
       action RESTORE \
       name "${BACKUP_NAME}" \
       collection "${COLLECTION}" \
-      async "${BACKUP_NAME}_restore" \
+      async "${REQUEST_ID}" \
       location "${LOCATION}" | jq .
 }
 
