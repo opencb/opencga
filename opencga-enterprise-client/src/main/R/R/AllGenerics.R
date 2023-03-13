e7708c6aba9c72f6652f9e669558ad30b0fa329f
imedina <igmecas@gmail.com>
2023-03-13 11:43:39 +0000

    client: first client implemented

---
+++ opencga-enterprise-client/src/main/R/R/AllGenerics.R
@@ -0,0 +1,85 @@
+# ##############################################################################
+## UserClient
+setGeneric("userClient", function(OpencgaR, users, filterId, user, endpointName, params=NULL, ...)
+    standardGeneric("userClient"))
+
+# ##############################################################################
+## ProjectClient
+setGeneric("projectClient", function(OpencgaR, project, projects, endpointName, params=NULL, ...)
+    standardGeneric("projectClient"))
+
+# ##############################################################################
+## StudyClient
+setGeneric("studyClient", function(OpencgaR, variableSet, studies, study, templateId, group, members, endpointName, params=NULL, ...)
+    standardGeneric("studyClient"))
+
+# ##############################################################################
+## FileClient
+setGeneric("fileClient", function(OpencgaR, files, annotationSet, file, members, folder, endpointName, params=NULL, ...)
+    standardGeneric("fileClient"))
+
+# ##############################################################################
+## JobClient
+setGeneric("jobClient", function(OpencgaR, jobs, job, members, endpointName, params=NULL, ...)
+    standardGeneric("jobClient"))
+
+# ##############################################################################
+## SampleClient
+setGeneric("sampleClient", function(OpencgaR, annotationSet, samples, sample, members, endpointName, params=NULL, ...)
+    standardGeneric("sampleClient"))
+
+# ##############################################################################
+## IndividualClient
+setGeneric("individualClient", function(OpencgaR, annotationSet, individual, individuals, members, endpointName, params=NULL, ...)
+    standardGeneric("individualClient"))
+
+# ##############################################################################
+## FamilyClient
+setGeneric("familyClient", function(OpencgaR, annotationSet, family, families, members, endpointName, params=NULL, ...)
+    standardGeneric("familyClient"))
+
+# ##############################################################################
+## CohortClient
+setGeneric("cohortClient", function(OpencgaR, annotationSet, cohort, cohorts, members, endpointName, params=NULL, ...)
+    standardGeneric("cohortClient"))
+
+# ##############################################################################
+## PanelClient
+setGeneric("panelClient", function(OpencgaR, panels, members, endpointName, params=NULL, ...)
+    standardGeneric("panelClient"))
+
+# ##############################################################################
+## AlignmentClient
+setGeneric("alignmentClient", function(OpencgaR, endpointName, params=NULL, ...)
+    standardGeneric("alignmentClient"))
+
+# ##############################################################################
+## VariantClient
+setGeneric("variantClient", function(OpencgaR, endpointName, params=NULL, ...)
+    standardGeneric("variantClient"))
+
+# ##############################################################################
+## ClinicalClient
+setGeneric("clinicalClient", function(OpencgaR, interpretation, clinicalAnalyses, clinicalAnalysis, interpretations, members, endpointName, params=NULL, ...)
+    standardGeneric("clinicalClient"))
+
+# ##############################################################################
+## OperationClient
+setGeneric("operationClient", function(OpencgaR, endpointName, params=NULL, ...)
+    standardGeneric("operationClient"))
+
+# ##############################################################################
+## MetaClient
+setGeneric("metaClient", function(OpencgaR, endpointName, params=NULL, ...)
+    standardGeneric("metaClient"))
+
+# ##############################################################################
+## CvaClient
+setGeneric("cvaClient", function(OpencgaR, caseId, endpointName, params=NULL, ...)
+    standardGeneric("cvaClient"))
+
+# ##############################################################################
+## AdminClient
+setGeneric("adminClient", function(OpencgaR, endpointName, params=NULL, ...)
+    standardGeneric("adminClient"))
+
