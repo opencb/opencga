dacd657ec9ebabf83ec691eadd177f92c058bd1c
JuanfeSanahuja <juanfe.sanahuja@zettagenomics.com>
2023-03-30 12:01:57 +0200

    cicd: create script to calculate the branch name #TASK-1974

--- .github/workflows/scripts/opencga_branch.sh
+++ .github/workflows/scripts/opencga_branch.sh
@@ -1,4 +1,32 @@
 #!/bin/bash
+#Navigate to the root folder where the pom.xml is
+cd ../../../
 
-echo "develop"
+#Read the opencga version from the pom.xml
+BUILD_VERSION=$(mvn help:evaluate -Dexpression=opencga.version -q -DforceStdout)
+
+#We remove the -SNAPSHOT if it exists
+CLEAN_BUILD_VERSION=$(echo "$BUILD_VERSION" | cut -d "-" -f 1)
+#Count the number of points to know if it is a portpatch
+COUNT=$(echo "$CLEAN_BUILD_VERSION" | grep -o '\.' | wc -l )
+
+#Read the numbers separately to compose the name of the branch
+MAJOR=$(echo "$CLEAN_BUILD_VERSION" | cut -d "." -f 1)
+MINOR=$(echo "$CLEAN_BUILD_VERSION" | cut -d "." -f 2)
+PATCH=$(echo "$CLEAN_BUILD_VERSION" | cut -d "." -f 3)
+
+#Is Portpatch
+if [ "$COUNT" -gt 2 ]; then
+  echo "release-$MAJOR.$MINOR.$PATCH.x"
+  exit 0
+fi
+
+#Is develop branch
+if [[ "$PATCH" ==  "0" ]]; then
+    echo "develop"
+    exit 0
+else #Is release branch
+  echo "release-$MAJOR.$MINOR.x"
+  exit 0
+fi
 
