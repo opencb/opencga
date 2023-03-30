3dcd7f7e09e3cdeb90ecf06d456e34d14ee0ebbe
imedina <igmecas@gmail.com>
2023-03-30 14:48:30 +0100

    cicd: minor clean=ups

--- .github/workflows/scripts/opencga_branch.sh
+++ .github/workflows/scripts/opencga_branch.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
-#Navigate to the root folder where the pom.xml is
+
+## Navigate to the root folder where the pom.xml is
 cd /home/runner/work/opencga-enterprise/opencga-enterprise || exit 2
 
-#Read the opencga version from the pom.xml
+## Read the opencga version from the pom.xml
 BUILD_VERSION=$(mvn help:evaluate -Dexpression=opencga.version -q -DforceStdout)
 
-#We remove the -SNAPSHOT if it exists
+## We remove the -SNAPSHOT if it exists
 CLEAN_BUILD_VERSION=$(echo "$BUILD_VERSION" | cut -d "-" -f 1)
-#Count the number of points to know if it is a portpatch
-COUNT=$(echo "$CLEAN_BUILD_VERSION" | grep -o '\.' | wc -l )
 
-#Read the numbers separately to compose the name of the branch
+## Read the numbers separately to compose the name of the branch
 MAJOR=$(echo "$CLEAN_BUILD_VERSION" | cut -d "." -f 1)
 MINOR=$(echo "$CLEAN_BUILD_VERSION" | cut -d "." -f 2)
 PATCH=$(echo "$CLEAN_BUILD_VERSION" | cut -d "." -f 3)
 
-#Is Portpatch
+## it's a HOTFIX. Count the number of points to know if it is a hotfix
+COUNT=$(echo "$CLEAN_BUILD_VERSION" | grep -o '\.' | wc -l )
 if [ "$COUNT" -gt 2 ]; then
   echo "release-$MAJOR.$MINOR.$PATCH.x"
   exit 0
 fi
 
-#Is develop branch
+## It's develop branch
 if [[ "$PATCH" ==  "0" ]]; then
     echo "develop"
     exit 0
