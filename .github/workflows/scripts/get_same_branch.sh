46b5e9118018d6212331775d874799f501a37f30
JuanfeSanahuja <juanfe.sanahuja@zettagenomics.com>
2023-04-11 14:56:21 +0200

    cicd: git branch -r --list  #TASK-4163

--- .github/workflows/scripts/get_same_branch.sh
+++ .github/workflows/scripts/get_same_branch.sh
@@ -19,7 +19,8 @@ function install(){
   git clone https://github.com/opencb/"$REPO".git
   cd "$REPO" || exit 2
   git checkout -b "$BRANCH_NAME"
-  local BRANCHES=$(git branch --list $BRANCH_NAME)
+  local BRANCHES=""
+  BRANCHES=$(git branch -r --list $BRANCH_NAME)
   if [[ -n $BRANCHES  ]]; then
     echo "Branch name $BRANCH_NAME already exists."
     mvn clean install -DskipTests
