455c876ca2305ef9890c19828a871d1e07268593
JuanfeSanahuja <juanfe.sanahuja@zettagenomics.com>
2023-04-12 18:26:21 +0200

    cicd: clean script and workflows #TASK-4163

--- .github/workflows/scripts/get_same_branch.sh
+++ .github/workflows/scripts/get_same_branch.sh
@@ -15,40 +15,11 @@ fi
 function install(){
   local REPO=$1
   cd /home/runner/work/ || exit 2
-  #git clone git@github.com:opencb/"$REPO".git
-  git clone https://github.com/opencb/"$REPO".git
-  cd "$REPO" || exit 2
-  git checkout "$BRANCH_NAME"
-  CURRENT=$(git branch --show-current)
-  echo "Current branch name $CURRENT"
-  if [[ "$CURRENT" == "$BRANCH_NAME"  ]]; then
-    echo "Branch name $BRANCH_NAME already exists."
-    mvn clean install -DskipTests
-  else
-    echo "$CURRENT Branch is NOT EQUALS $BRANCH_NAME "
-  fi
+  git clone https://github.com/opencb/"$REPO".git -b "$BRANCH_NAME"
+  echo "Branch name $BRANCH_NAME already exists."
+  mvn clean install -DskipTests
 }
 
-PWD=$(pwd)
-echo "The absolute exec path is $PWD"
-
-if [[ "$PWD" != *"java-common-libs"* ]]; then
-  ## biodata
-  if [[ "$PWD" == *"biodata"* ]]; then
-    echo "It's biodata."
-    install "java-common-libs"
-  fi
-  ## cellbase
-  if [[ "$PWD" == *"cellbase"* ]]; then
-    echo "It's cellbase."
-    install "java-common-libs"
-    install "biodata"
-  fi
-  ## opencga
-  if [[ "$PWD" == *"opencga"* ]]; then
-    echo "It's opencga."
-    install "java-common-libs"
-    install "biodata"
-    install "cellbase"
-  fi
-fi
+install "java-common-libs"
+install "biodata"
+install "cellbase"
