f881aef3df1ba1f58b27ae856c2cca7a72a702bc
Jacobo Coll Moragón <jacobo167@gmail.com>
2024-02-05 14:56:38 +0000

    pom: Upgrade to use new thirdparty hadoop libs. #TASK-4436

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+set -e
+set -o pipefail
+set -o nounset
+
 function printUsage() {
   echo ""
   echo "Build opencga-enterprise."
@@ -8,17 +12,38 @@ function printUsage() {
   echo ""
   echo "Options:"
   echo "     -o     --opencga-home        STRING         Opencga project repo directory. By default, ./opencga-home"
-  echo "     -H     --storage-hadoop      STRING         Hadoop flavour. hdp3.1, emr6.1, ..."
+  echo "     -H     --storage-hadoop      STRING         Hadoop flavour. hdp3.1, hdi5.1, emr6.1, emr6.13 ..."
+  echo "            --skip-opencga-build  FLAG           Assume opencga is already build in the correct version. Use with caution"
   echo "            --verbose             FLAG           Print verbose logs"
   echo "            --help                FLAG           Print this help and exit"
 }
 
+function log() {
+    if [[ "$#" -gt 0 ]]; then
+      echo "$@" | log
+      return
+    fi
+    if [[ -n "$LOG_FILE" ]]; then
+      tee -a $LOG_FILE
+    else
+      cat >&2
+    fi
+}
+
+function error() {
+  log "=========================="
+  log "[ERROR] - " "$@"
+  log "=========================="
+}
+
 OPENCGA_HOME_DIR="$PWD/opencga-home/"
 STORAGE_HADOOP_DEPS="hdp3.1"
+SKIP_OPENCGA_BUILD=false
+LOG_FILE=""
 
 while [[ $# -gt 0 ]]; do
   key="$1"
-  value="$2"
+  value="${2:-}"
   case $key in
   -h | --help)
     printUsage
@@ -38,6 +63,10 @@ while [[ $# -gt 0 ]]; do
     shift # past argument
     shift # past value
     ;;
+  --skip-opencga-build)
+    SKIP_OPENCGA_BUILD=true
+    shift # past argument
+    ;;
   *) # unknown option
     echo "Unknown option $key"
     printUsage
@@ -56,23 +85,20 @@ if [ -d "$OPENCGA_HOME_DIR" ]; then
   cd "$OPENCGA_HOME_DIR" || exit 2
   OPENCGA_CURRENT_VERSION="$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)"
 
-  echo "OPENCGA_DEPENDENCY_VERSION= $OPENCGA_DEPENDENCY_VERSION"
-  echo "OPENCGA_CURRENT_VERSION= $OPENCGA_CURRENT_VERSION"
+  log "OPENCGA_DEPENDENCY_VERSION= $OPENCGA_DEPENDENCY_VERSION"
+  log "OPENCGA_CURRENT_VERSION= $OPENCGA_CURRENT_VERSION"
 
   if [ "$OPENCGA_DEPENDENCY_VERSION" == "$OPENCGA_CURRENT_VERSION" ]; then
-    echo "Compiling opencga..."
+    log "Compiling opencga..."
     if ! (mvn enforcer:enforce -Denforcer.rules=requireProfileIdsExist -P"$STORAGE_HADOOP_DEPS" -pl :opencga) ; then
-      echo OpenCGA storage hadoop "$STORAGE_HADOOP_DEPS" not found!
+      log OpenCGA storage hadoop "$STORAGE_HADOOP_DEPS" not found!
       exit 1
     fi
 
-    mvn clean install -DskipTests -Pstorage-hadoop -P"$STORAGE_HADOOP_DEPS" -T 2
-    # shellcheck disable=SC2181
-    if [ $? -eq 0 ]; then
-      echo "Opencga compilation success!"
+    if [ "$SKIP_OPENCGA_BUILD" = true ] ; then
+      log "Skipping opencga build"
     else
-      echo "Opencga compilation ERROR"
-      exit 1
+      mvn clean install -DskipTests -P"$STORAGE_HADOOP_DEPS" -T 2 || (error "Opencga compilation ERROR" && exit 1)
     fi
   else
     cd "$OPENCGA_ENTERPRISE_HOME_DIR" || exit 2
@@ -88,19 +114,22 @@ if [ -d "$OPENCGA_HOME_DIR" ]; then
       REF_TYPE="branch"
       REF="$OPENCGA_EXPECTED_BRANCH"
     fi
-    echo "Opencga version no match! You must checkout $REF_TYPE \"$REF\" to build from version \"$OPENCGA_DEPENDENCY_VERSION\" of opencga"
-    echo "Please, execute bellow command and retry:"
-    echo "  git -C \"$OPENCGA_HOME_DIR\" checkout $REF"
+    log "Opencga version no match! You must checkout $REF_TYPE \"$REF\" to build from version \"$OPENCGA_DEPENDENCY_VERSION\" of opencga"
+    log "Please, execute bellow command and retry:"
+    log "  git -C \"$OPENCGA_HOME_DIR\" checkout $REF"
     exit 1
   fi
 else
-  echo "ERROR OPENCGA HOME NOT FOUND!!!"
-  echo "You must create in the current directory a symbolic link to the directory where you have downloaded opencga and call it opencga-home"
-  echo "         ln -s /path/to/opencga $OPENCGA_HOME_DIR    "
+  log "ERROR OPENCGA HOME NOT FOUND!!!"
+  log "You must create in the current directory a symbolic link to the directory where you have downloaded opencga and call it opencga-home"
+  log "         ln -s /path/to/opencga $OPENCGA_HOME_DIR    "
   printUsage
   exit 1
 fi
 
 cd "$OPENCGA_ENTERPRISE_HOME_DIR" || exit 2
 
-mvn clean install -DskipTests -T 2 -Dopencga.build.dir="${OPENCGA_HOME_DIR}/build/" -Dopencga-storage-hadoop-deps.id="$STORAGE_HADOOP_DEPS" -Dopencga.war.name=opencga
+mvn clean install -DskipTests -T 2 \
+    -Dopencga.build.dir="${OPENCGA_HOME_DIR}/build/" \
+    -Dopencga-hadoop-shaded.id="$STORAGE_HADOOP_DEPS" \
+    -Dopencga.war.name=opencga || (error "Opencga enterprise compilation ERROR" && exit 1)
