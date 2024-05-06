68a9561438d584dab17f220348fc44ab73f13245
JuanfeSanahuja <juanfe.sanahuja@zettagenomics.com>
2024-05-06 12:41:16 +0200

    MailUtils:FIX: Code scanning/SonarCloud - Server hostnames should be verified during SSL/TLS connections #TASK-464

--- opencga-core/src/main/java/org/opencb/opencga/core/common/MailUtils.java
+++ opencga-core/src/main/java/org/opencb/opencga/core/common/MailUtils.java
@@ -45,7 +45,9 @@ public class MailUtils {
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.starttls.required", "true");
         props.put("mail.smtp.ssl.protocols", "TLSv1.2");
+        props.put("mail.smtp.ssl.checkserveridentity", "true");
         props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+
         Session session = Session.getInstance(props,
                 new javax.mail.Authenticator() {
                     protected PasswordAuthentication getPasswordAuthentication() {
