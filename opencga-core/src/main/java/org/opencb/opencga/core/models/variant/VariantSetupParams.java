f5ea5a475fa773e5106814473267e10a87d11ac5
Jacobo Coll Moragón <jacobo167@gmail.com>
2024-05-23 10:38:25 +0100

    core: Add data field descriptions to VariantSetupPatams. #TASK-5861, #TASK-5448

--- opencga-core/src/main/java/org/opencb/opencga/core/models/variant/VariantSetupParams.java
+++ opencga-core/src/main/java/org/opencb/opencga/core/models/variant/VariantSetupParams.java
@@ -1,21 +1,41 @@
 package org.opencb.opencga.core.models.variant;
 
+import org.opencb.commons.annotations.DataField;
 import org.opencb.opencga.core.tools.ToolParams;
 
 public class VariantSetupParams extends ToolParams {
 
+    @DataField(description = "Expected number of samples that will be loaded. Used to infer some parameters. "
+            + "This number is only used as a hint. "
+            + "If the real number of samples is different, if it grows beyond expectation, or if , the loader should be able to handle it.",
+            required = true)
     private Integer expectedSamples;
 
+    @DataField(description = "Expected number of files that will be loaded. Used to infer some parameters. "
+            + "This number is only used as a hint. "
+            + "If the real number of files is different, the loader should be able to handle it.", required = true)
     private Integer expectedFiles;
 
+    @DataField(description = "Main type of the files that will be loaded. If the dataset contains multiple types of files,"
+            + " provide the one that matches most of the files.")
     private FileType fileType;
 
+    @DataField(description = "Average size of the files that will be loaded. This number is only used as a hint. "
+            + "If the real size of the files is different, the loader should be able to handle it. Accepts units. e.g. 435MB, 2GB, 86KB. "
+            + "If not provided, the value will be inferred from the file type.")
     private String averageFileSize;
 
+    @DataField(description = "Number of variants per sample. This number is only used as a hint. "
+            + "If the real number of variants per sample is different, the loader should be able to handle it. "
+            + "If not provided, the value will be inferred from the file type.")
     private Integer variantsPerSample;
 
+    @DataField(description = "Average number of samples per file. This number is only used as a hint. "
+            + "If the real number of samples per file is different, the loader should be able to handle it. "
+            + "If not provided, the value will be inferred from the expectedSamples and expectedFiles and dataDistribution.")
     private Float averageSamplesPerFile;
 
+    @DataField(description = "Data distribution of the files. This parameter is used to infer the number of samples per file.")
     private DataDistribution dataDistribution;
 
     public VariantSetupParams(VariantSetupParams params) {
@@ -37,27 +57,39 @@ public class VariantSetupParams extends ToolParams {
         //   - Platinum gVCF
         //   - Cancer germline
         //   - RD germline without family calling
+        @DataField(description = "Single sample VCF files. One file per sample. e.g. Platinum gVCF, Cancer germline, RD germline without family calling")
         SINGLE_SAMPLE_FILES,
+
         // Multi samples VCF files. One file with multiple samples.
         // e.g.
         //   - Corpasome
         //   - RD germline with family calling
+        @DataField(description = "Multi samples VCF files. One file with multiple samples. e.g. Corpasome, RD germline with family calling")
         MULTI_SAMPLE_FILES,
+
         // Multiple files per sample. Each file might have multiple samples.
         // e.g.
         //   - Somatic study with multiple callers
+        @DataField(description = "Multiple files per sample. Each file might have multiple samples. e.g. Somatic study with multiple callers")
         MULTIPLE_FILE_PER_SAMPLE,
+
         // Large aggregated/joined/merged files. Each file has all samples. Each file contains a specific set of chromosomes.
         // e.g.
         //   - 1000 genomes
+        @DataField(description = "Large aggregated/joined/merged files. Each file has all samples. Each file contains a specific set of chromosomes. e.g. 1000 genomes")
         MULTI_SAMPLE_FILES_SPLIT_BY_CHROMOSOME,
-        // Large aggregated/joined/merged files. Each file has all samples. Each file contains a specific region.
+
+        // Large aggregated/joined/merged files. Each file has all samples. Each file contains an arbitrary region.
+        @DataField(description = "Large aggregated/joined/merged files. Each file has all samples. Each file contains an arbitrary region.")
         MULTI_SAMPLE_FILES_SPLIT_BY_REGION,
     }
 
     public enum FileType {
+        @DataField(description = "Whole genome VCF file.")
         GENOME_VCF,
+        @DataField(description = "Whole genome gVCF file.")
         GENOME_gVCF,
+        @DataField(description = "Exome VCF file.")
         EXOME
     }
 
