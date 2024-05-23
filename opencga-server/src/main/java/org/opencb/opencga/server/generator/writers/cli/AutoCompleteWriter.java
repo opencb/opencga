56a781994456f9c3ede9db0f5d61765c9312ef18
Jacobo Coll Moragón <jacobo167@gmail.com>
2024-05-23 17:06:33 +0100

    client: Remove dates and random changes from client auto-generation process. #TASK-6302

--- opencga-server/src/main/java/org/opencb/opencga/server/generator/writers/cli/AutoCompleteWriter.java
+++ opencga-server/src/main/java/org/opencb/opencga/server/generator/writers/cli/AutoCompleteWriter.java
@@ -25,8 +25,6 @@ import org.opencb.opencga.server.generator.writers.ParentClientRestApiWriter;
 
 import java.io.File;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class AutoCompleteWriter extends ParentClientRestApiWriter {
 
@@ -37,23 +35,6 @@ public class AutoCompleteWriter extends ParentClientRestApiWriter {
     @Override
     protected String getClassImports(String key) {
         StringBuilder sb = new StringBuilder();
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        sb.append("/*\n");
-        sb.append("* Copyright 2015-").append(sdf.format(new Date())).append(" OpenCB\n");
-        sb.append("*\n");
-        sb.append("* Licensed under the Apache License, Version 2.0 (the \"License\");\n");
-        sb.append("* you may not use this file except in compliance with the License.\n");
-        sb.append("* You may obtain a copy of the License at\n");
-        sb.append("*\n");
-        sb.append("*     http://www.apache.org/licenses/LICENSE-2.0\n");
-        sb.append("*\n");
-        sb.append("* Unless required by applicable law or agreed to in writing, software\n");
-        sb.append("* distributed under the License is distributed on an \"AS IS\" BASIS,\n");
-        sb.append("* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n");
-        sb.append("* See the License for the specific language governing permissions and\n");
-        sb.append("* limitations under the License.\n");
-        sb.append("*/\n");
-        sb.append("\n");
         sb.append("package ").append(config.getOptions().getParserPackage()).append(";\n");
         sb.append("\n");
 
