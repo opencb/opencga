5346eec3014e2315838c58062a9ee8822fdcabba
JuanfeSanahuja <juanfe.sanahuja@zettagenomics.com>
2024-08-13 22:16:18 +0200

    build: fix report path  #TASK-6399

--- build.sh
+++ build.sh
@@ -333,7 +333,7 @@ function publish_reports() {
     echo "Preparing destination path"
     local VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate -Dexpression=project.version -q -DforceStdout)
     echo "Xetabase tested is $VERSION"
-    FILE_TO_SEND="$OPENCGA_ENTERPRISE_HOME_DIR/reports/test/*"
+    FILE_TO_SEND="$OPENCGA_ENTERPRISE_HOME_DIR/reports/test"
     echo "The reports are in $FILE_TO_SEND"
     DESTINATION_PATH="/var/www/html/reports/xetabase"
     if [[ $TASK_REFERENCE == TASK* ]]; then
