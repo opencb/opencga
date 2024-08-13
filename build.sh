aecda9be58757dfffd0e7bd6dae2a584c60816e0
JuanfeSanahuja <juanfe.sanahuja@zettagenomics.com>
2024-08-13 22:00:55 +0200

    build: fix report path  #TASK-6399

--- build.sh
+++ build.sh
@@ -333,7 +333,7 @@ function publish_reports() {
     echo "Preparing destination path"
     local VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate -Dexpression=project.version -q -DforceStdout)
     echo "Xetabase tested is $VERSION"
-    FILE_TO_SEND="$OPENCGA_ENTERPRISE_HOME_DIR/reports/test"
+    FILE_TO_SEND="$OPENCGA_ENTERPRISE_HOME_DIR/reports/test/*"
     echo "The reports are in $FILE_TO_SEND"
     DESTINATION_PATH="/var/www/html/reports/xetabase"
     if [[ $TASK_REFERENCE == TASK* ]]; then
@@ -344,15 +344,14 @@ function publish_reports() {
     echo "Destination path: $DESTINATION_PATH"
     sshpass -p "$SSH_PASS" ssh -p "$SSH_PORT" "$SSH_USER@$SSH_HOST" "mkdir -p $DESTINATION_PATH"
     echo "Created remote path: $DESTINATION_PATH"
-    sshpass -p "$SSH_PASS" scp -P "$SSH_PORT" "$FILE_TO_SEND" "$SSH_USER@$SSH_HOST:$DESTINATION_PATH"
+    sshpass -p "$SSH_PASS" scp -r -P "$SSH_PORT" "$FILE_TO_SEND" "$SSH_USER@$SSH_HOST:$DESTINATION_PATH"
     if [ $? -eq 0 ]; then
       echo "Uploaded test report to $DESTINATION_PATH"
     else
-      echo "Error transfering file to $SSH_HOST"
+      echo "Error transferring file to $SSH_HOST"
       exit 1
     fi
   fi
-
 }
 
 # Function to upload the docker of Oopencga-enterprise to https://hub.docker.com/repositories/zettagenomics
