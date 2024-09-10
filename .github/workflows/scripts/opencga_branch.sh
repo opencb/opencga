bcb23046060736f7205bd52ff0ff97822710060a
JuanfeSanahuja <juanfe.sanahuja@zettagenomics.com>
2024-09-10 18:28:56 +0200

    opencga_branch.sh: Fix  == true #TASK-6865

--- .github/workflows/scripts/opencga_branch.sh
+++ .github/workflows/scripts/opencga_branch.sh
@@ -7,9 +7,8 @@ cd "$(dirname "$0")"/../../../ || exit 2
 
 ## Read the opencga version from the pom.xml
 BUILD_VERSION=$(mvn help:evaluate -Dexpression=opencga.version -q -DforceStdout)
-ls -lrth
 
-if [ "$IS_RELEASE" == "true" ]; then
+if [[ "$IS_RELEASE" == "true" ]]; then
   echo "v$BUILD_VERSION"
   exit 0
 fi
