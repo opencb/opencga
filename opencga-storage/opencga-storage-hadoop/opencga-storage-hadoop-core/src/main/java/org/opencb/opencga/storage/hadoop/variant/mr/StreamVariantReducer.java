154befa4baec3f7bba3773eb7ec7b6c1b5f7f438
Jacobo Coll Moragón <jacobo167@gmail.com>
2024-10-25 10:21:57 +0100

    storage: Do not write multiple headers. #TASK-6722

--- opencga-storage/opencga-storage-hadoop/opencga-storage-hadoop-core/src/main/java/org/opencb/opencga/storage/hadoop/variant/mr/StreamVariantReducer.java
+++ opencga-storage/opencga-storage-hadoop/opencga-storage-hadoop-core/src/main/java/org/opencb/opencga/storage/hadoop/variant/mr/StreamVariantReducer.java
@@ -18,20 +18,37 @@ public class StreamVariantReducer extends Reducer<ImmutableBytesWritable, Text, 
     public static final byte[] STDERR_KEY_BYTES = Bytes.toBytes(STDERR_KEY);
 
     private static final Log LOG = LogFactory.getLog(StreamVariantReducer.class);
+    public static final byte[] HEADER_PREFIX_BYTES = Bytes.toBytes("#");
     private MultipleOutputs<ImmutableBytesWritable, Text> mos;
+    private boolean headerWritten = false;
 
     @Override
-    protected void setup(Reducer<ImmutableBytesWritable, Text, ImmutableBytesWritable, Text>.Context context) throws IOException, InterruptedException {
+    protected void setup(Reducer<ImmutableBytesWritable, Text, ImmutableBytesWritable, Text>.Context context)
+            throws IOException, InterruptedException {
         super.setup(context);
         mos = new MultipleOutputs<>(context);
     }
 
     @Override
-    protected void reduce(ImmutableBytesWritable key, Iterable<Text> values, Reducer<ImmutableBytesWritable, Text, ImmutableBytesWritable, Text>.Context context)
+    protected void reduce(ImmutableBytesWritable key, Iterable<Text> values,
+                          Reducer<ImmutableBytesWritable, Text, ImmutableBytesWritable, Text>.Context context)
             throws IOException, InterruptedException {
         for (Text value : values) {
-            if (Bytes.equals(key.get(), key.getOffset(), STDOUT_KEY_BYTES.length, STDOUT_KEY_BYTES, 0, STDOUT_KEY_BYTES.length)) {
-                mos.write("stdout", key, value);
+            if (hasPrefix(key, STDOUT_KEY_BYTES)) {
+                if (hasPrefix(value, HEADER_PREFIX_BYTES)) {
+                    if (headerWritten) {
+                        // skip header
+                        context.getCounter(VariantsTableMapReduceHelper.COUNTER_GROUP_NAME, "header_records_skip").increment(1);
+                    } else {
+                        mos.write("stdout", key, value);
+                        context.getCounter(VariantsTableMapReduceHelper.COUNTER_GROUP_NAME, "header_records").increment(1);
+                    }
+                } else {
+                    // No more header, assume all header is written
+                    headerWritten = true;
+                    mos.write("stdout", key, value);
+                    context.getCounter(VariantsTableMapReduceHelper.COUNTER_GROUP_NAME, "body_records").increment(1);
+                }
                 context.getCounter(VariantsTableMapReduceHelper.COUNTER_GROUP_NAME, "stdout_records").increment(1);
             } else {
                 mos.write("stderr", key, value);
@@ -41,8 +58,24 @@ public class StreamVariantReducer extends Reducer<ImmutableBytesWritable, Text, 
         }
     }
 
+    private static boolean hasPrefix(ImmutableBytesWritable key, byte[] prefix) {
+        return hasPrefix(key.get(), key.getOffset(), key.getLength(), prefix);
+    }
+
+    private static boolean hasPrefix(Text text, byte[] prefix) {
+        return hasPrefix(text.getBytes(), 0, text.getLength(), prefix);
+    }
+
+    private static boolean hasPrefix(byte[] key, int offset, int length, byte[] prefix) {
+        if (length < prefix.length) {
+            return false;
+        }
+        return Bytes.equals(key, offset, prefix.length, prefix, 0, prefix.length);
+    }
+
     @Override
-    protected void cleanup(Reducer<ImmutableBytesWritable, Text, ImmutableBytesWritable, Text>.Context context) throws IOException, InterruptedException {
+    protected void cleanup(Reducer<ImmutableBytesWritable, Text, ImmutableBytesWritable, Text>.Context context)
+            throws IOException, InterruptedException {
         super.cleanup(context);
         mos.close();
     }
