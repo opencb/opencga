09566088453665dd083445a681169b58cca92531
Joaquín Tárraga Giménez <joaquintarraga@gmail.com>
2024-12-12 12:27:57 +0100

    catalog: update JUnit tests for resource manager, #TASK-7047, #TASK-6442

--- opencga-catalog/src/test/java/org/opencb/opencga/catalog/managers/ResourceManagerTest.java
+++ opencga-catalog/src/test/java/org/opencb/opencga/catalog/managers/ResourceManagerTest.java
@@ -4,13 +4,17 @@ import org.junit.Before;
 import org.junit.Test;
 import org.opencb.opencga.catalog.exceptions.ResourceException;
 import org.opencb.opencga.catalog.utils.ResourceManager;
+import org.opencb.opencga.core.common.JacksonUtils;
 import org.opencb.opencga.core.common.TimeUtils;
+import org.opencb.opencga.core.config.Configuration;
 import org.opencb.opencga.core.exceptions.ToolException;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collections;
 
 import static org.opencb.opencga.catalog.utils.ResourceManager.ANALYSIS_DIRNAME;
 import static org.opencb.opencga.catalog.utils.ResourceManager.RESOURCES_DIRNAME;
@@ -33,6 +37,9 @@ public class ResourceManagerTest extends AbstractManagerTest {
         analysisPath = openCgaHome.resolve(ANALYSIS_DIRNAME);
         analysisResourcePath = analysisPath.resolve(RESOURCES_DIRNAME);
 
+        Configuration configuration = Configuration.load(Files.newInputStream(openCgaHome.resolve("conf").resolve("configuration.yml").toFile().toPath()));
+        configuration.getAnalysis().getResource().setBasePath(openCgaHome.resolve("analysis/resources").toAbsolutePath());
+        configuration.serialize(Files.newOutputStream(openCgaHome.resolve("conf").resolve("configuration.yml").toFile().toPath()));
         resourceManager = new ResourceManager(openCgaHome);
     }
 
@@ -41,7 +48,7 @@ public class ResourceManagerTest extends AbstractManagerTest {
         resourceManager.checkResourcePaths("relatedness");
     }
 
-//    @Test
+    //    @Test
 //    public void testFetchAllResources() throws IOException, ResourceException {
 //        System.out.println("analysisResourcePath = " + analysisResourcePath.toAbsolutePath());
 //        Path outDir = createDir("jobdir").resolve(RESOURCES_DIRNAME);
@@ -57,60 +64,36 @@ public class ResourceManagerTest extends AbstractManagerTest {
 //        }
 //    }
 //
-//    @Test(expected = ResourceException.class)
-//    public void testFetchAllResourcesNoAdmin() throws ResourceException {
-//        resourceManager.fetchAllResources(version, null, catalogManagerResource.getCatalogManager(), normalToken1);
-//    }
-//
-//    @Test
-//    public void testFetchResourcesForAGivenAnalysis() throws IOException, ResourceException {
-//        System.out.println("analysisResourcePath = " + analysisResourcePath.toAbsolutePath());
-//        Path outDir = createDir("jobdir").resolve(RESOURCES_DIRNAME);
-//        Files.createDirectories(outDir);
-//        System.out.println("outDir = " + outDir.toAbsolutePath());
-//        resourceManager.fetchAllResources(version, outDir, catalogManagerResource.getCatalogManager(), catalogManagerResource.getAdminToken());
-//        resourceMetadata = JacksonUtils.getDefaultObjectMapper().readerFor(ResourceMetadata.class).readValue(openCgaHome.resolve(ANALYSIS_DIRNAME).resolve(RESOURCES_DIRNAME).resolve(resourceManager.getResourceMetaFilename(version)).toFile());
-//
-//        String analysisId = "qc";
-//        AnalysisResourceList analysisResourceList = null;
-//        for (AnalysisResourceList list : resourceMetadata.getAnalysisResourceLists()) {
-//            if (analysisId.equals(list.getAnalysisId())) {
-//                analysisResourceList = list;
-//                break;
-//            }
-//        }
-//
-//        List<File> resourceFiles = resourceManager.getResourceFiles(analysisId, version);
-//        Assert.assertEquals(analysisResourceList.getResources().size(), resourceFiles.size());
-//        for (File resourceFile : resourceFiles) {
-//            Assert.assertTrue(Files.exists(resourceFile.toPath()));
-//            Assert.assertTrue(analysisResourceList.getResources().stream().map(r -> Paths.get(r.getLocalRelativePath()).getFileName().toString()).collect(Collectors.toList()).contains(resourceFile.getName()));
-//        }
-//    }
-//
-//    @Test
-//    public void testFetchAGivenResource() throws IOException, ResourceException {
-//        System.out.println("analysisResourcePath = " + analysisResourcePath.toAbsolutePath());
-//        Path outDir = createDir("jobdir").resolve(RESOURCES_DIRNAME);
-//        Files.createDirectories(outDir);
-//        System.out.println("outDir = " + outDir.toAbsolutePath());
-//        resourceManager.fetchAllResources(version, outDir, catalogManagerResource.getCatalogManager(), catalogManagerResource.getAdminToken());
-//        resourceMetadata = JacksonUtils.getDefaultObjectMapper().readerFor(ResourceMetadata.class).readValue(openCgaHome.resolve(ANALYSIS_DIRNAME).resolve(RESOURCES_DIRNAME).resolve(resourceManager.getResourceMetaFilename(version)).toFile());
-//
-//        String analysisId = "liftover";
-//        String resourceName = "chain.frq";
-//        AnalysisResourceList analysisResourceList = null;
-//        for (AnalysisResourceList list : resourceMetadata.getAnalysisResourceLists()) {
-//            if (analysisId.equals(list.getAnalysisId())) {
-//                analysisResourceList = list;
-//                break;
-//            }
-//        }
-//
-//        File resourceFile = resourceManager.getResourceFile(analysisId, resourceName, version);
-//        Assert.assertTrue(Files.exists(resourceFile.toPath()));
-//        Assert.assertTrue(analysisResourceList.getResources().stream().map(r -> Paths.get(r.getLocalRelativePath()).getFileName().toString()).collect(Collectors.toList()).contains(resourceFile.getName()));
-//    }
+    @Test(expected = ResourceException.class)
+    public void testFetchAllResourcesNoAdmin() throws ResourceException, IOException {
+        resourceManager.fetchAllResources(null, catalogManagerResource.getCatalogManager(), normalToken1);
+    }
+
+    @Test
+    public void testFetchResources() throws IOException, ResourceException, ToolException {
+        System.out.println("analysisResourcePath = " + analysisResourcePath.toAbsolutePath());
+        Path outDir = createDir("jobdir").resolve(RESOURCES_DIRNAME);
+        Files.createDirectories(outDir);
+        System.out.println("outDir = " + outDir.toAbsolutePath());
+
+        resourceManager.fetchResources(Arrays.asList("RELATEDNESS_VARIANTS_PRUNE_IN", "RELATEDNESS_VARIANTS_FRQ"), outDir,
+                catalogManagerResource.getCatalogManager(), catalogManagerResource.getAdminToken());
+
+        resourceManager.checkResourcePaths("relatedness");
+    }
+
+    @Test
+    public void testFetchAGivenResource() throws IOException, ResourceException {
+        System.out.println("analysisResourcePath = " + analysisResourcePath.toAbsolutePath());
+        Path outDir = createDir("jobdir").resolve(RESOURCES_DIRNAME);
+        Files.createDirectories(outDir);
+        System.out.println("outDir = " + outDir.toAbsolutePath());
+
+        resourceManager.fetchResources(Arrays.asList("RELATEDNESS_VARIANTS_PRUNE_IN"), outDir,
+                catalogManagerResource.getCatalogManager(), catalogManagerResource.getAdminToken());
+
+        resourceManager.checkResourcePath("RELATEDNESS_VARIANTS_PRUNE_IN");
+    }
 
     //-------------------------------------------------------------------------
     // P R I V A T E
