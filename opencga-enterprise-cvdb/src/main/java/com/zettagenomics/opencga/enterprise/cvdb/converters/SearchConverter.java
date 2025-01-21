0ce681f9f6f0925de7ff6061dfe7955bbaa26cda
Joaquín Tárraga Giménez <joaquintarraga@gmail.com>
2025-01-21 14:27:14 +0100

    cvdb: remove unused constants, #TASK-5610

--- opencga-enterprise-cvdb/src/main/java/com/zettagenomics/opencga/enterprise/cvdb/converters/SearchConverter.java
+++ opencga-enterprise-cvdb/src/main/java/com/zettagenomics/opencga/enterprise/cvdb/converters/SearchConverter.java
@@ -18,13 +18,13 @@ import java.util.stream.Collectors;
 
 public class SearchConverter<M, N> {
 
-    public static final String CVDB_INTERNALS_KEY = "OPENCGA_CVDB_INTERNALS";
-    public static final String CVDB_VIEWERS_KEY = "OPENCGA_VIEWERS";
-    public static final String CVDB_STUDY_ID_KEY = "OPENCGA_STUDY_ID";
-
-    protected static final String UP_FIELD_SEPARATOR = "===";
-    protected static final String FIELD_SEPARATOR = "---";
-    protected static final String EMPTY_VALUE = "**";
+//    public static final String CVDB_INTERNALS_KEY = "OPENCGA_CVDB_INTERNALS";
+//    public static final String CVDB_VIEWERS_KEY = "OPENCGA_VIEWERS";
+//    public static final String CVDB_STUDY_ID_KEY = "OPENCGA_STUDY_ID";
+//
+//    protected static final String UP_FIELD_SEPARATOR = "===";
+//    protected static final String FIELD_SEPARATOR = "---";
+//    protected static final String EMPTY_VALUE = "**";
 
     protected ObjectMapper mapper;
     protected ObjectReader mapReader;
