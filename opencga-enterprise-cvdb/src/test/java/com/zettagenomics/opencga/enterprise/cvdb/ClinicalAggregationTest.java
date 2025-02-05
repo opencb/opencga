a67a02d7a0431594c0894fb77d8cb3c8f9dd8e68
Joaquín Tárraga Giménez <joaquintarraga@gmail.com>
2025-02-05 09:39:27 +0100

    test: fix JUnit test, #TASK-7134

--- opencga-enterprise-cvdb/src/test/java/com/zettagenomics/opencga/enterprise/cvdb/ClinicalAggregationTest.java
+++ opencga-enterprise-cvdb/src/test/java/com/zettagenomics/opencga/enterprise/cvdb/ClinicalAggregationTest.java
@@ -131,7 +131,7 @@ public class ClinicalAggregationTest {
         DataResult<FacetField> facetResult = cvdbEngine.facetClinicalAnalyses(query, queryOptions, null);
         assertEquals(1, facetResult.getNumResults());
         assertTrue(facetResult.first().getCount() != null);
-        assertEquals(2, facetResult.first().getCount().longValue());
+        assertEquals(2L, facetResult.first().getCount().longValue());
         assertEquals(CA_DISORDER_ID_NAME, facetResult.first().getName());
         for (FacetField result : facetResult.getResults()) {
             System.out.println(result);
