a62e2b74b722ea1b91ade975b782d0a092946600
Joaquín Tárraga Giménez <joaquintarraga@gmail.com>
2025-02-21 10:31:43 +0100

    core: update COSMIC variant annotator extension to v101, #TASK-7431, #TASK-7367

--- opencga-storage/opencga-storage-core/src/main/java/org/opencb/opencga/storage/core/variant/annotation/annotators/extensions/cosmic/CosmicVariantAnnotatorExtensionTask.java
+++ opencga-storage/opencga-storage-core/src/main/java/org/opencb/opencga/storage/core/variant/annotation/annotators/extensions/cosmic/CosmicVariantAnnotatorExtensionTask.java
@@ -2,8 +2,10 @@ package org.opencb.opencga.storage.core.variant.annotation.annotators.extensions
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectReader;
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
 import org.apache.commons.lang3.StringUtils;
-import org.opencb.biodata.formats.variant.cosmic.CosmicParser;
+import org.opencb.biodata.formats.variant.cosmic.CosmicParser101;
 import org.opencb.biodata.models.variant.Variant;
 import org.opencb.biodata.models.variant.avro.EvidenceEntry;
 import org.opencb.biodata.models.variant.avro.VariantAnnotation;
@@ -19,6 +21,9 @@ import org.rocksdb.RocksDBException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URI;
 import java.nio.file.Files;
@@ -26,6 +31,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.List;
+import java.util.zip.GZIPInputStream;
 
 public class CosmicVariantAnnotatorExtensionTask implements VariantAnnotatorExtensionTask {
 
@@ -56,6 +62,28 @@ public class CosmicVariantAnnotatorExtensionTask implements VariantAnnotatorExte
         // Sanity check
         Path cosmicFile = Paths.get(options.getString(VariantStorageOptions.ANNOTATOR_EXTENSION_COSMIC_FILE.key()));
         FileUtils.checkFile(cosmicFile);
+
+        // Check and decompress tarball, checking the COSMIC files: GenomeScreensMutant and Classification
+        Path genomeScreensMutantFile = null;
+        Path classificationFile = null;
+        if (!cosmicFile.getFileName().toString().endsWith(".tar.gz")) {
+            throw new ToolException("Invalid COSMIC file format '" + cosmicFile.getFileName() + "': it must be a .tar.gz file");
+        }
+        decompressTarBall(cosmicFile, Paths.get(output.getPath()).toAbsolutePath());
+        for (File file : Paths.get(output.getPath()).toAbsolutePath().toFile().listFiles()) {
+            if (file.getName().contains("Classification")) {
+                classificationFile = file.toPath();
+            } else if (file.getName().contains("GenomeScreensMutant")) {
+                genomeScreensMutantFile = file.toPath();
+            }
+        }
+        if (genomeScreensMutantFile == null) {
+            throw new ToolException("Missing the GenomeScreensMutant file in the COSMIC tarball '" + cosmicFile.getFileName() + "'");
+        }
+        if (classificationFile == null) {
+            throw new ToolException("Missing the Classification file in the COSMIC tarball '" + cosmicFile.getFileName() + "'");
+        }
+
         cosmicVersion = (String) options.getOrDefault(VariantStorageOptions.ANNOTATOR_EXTENSION_COSMIC_VERSION.key(), null);
         if (StringUtils.isEmpty(cosmicVersion)) {
             throw new IllegalArgumentException("Missing COSMIC version");
@@ -80,7 +108,7 @@ public class CosmicVariantAnnotatorExtensionTask implements VariantAnnotatorExte
             // Call COSMIC parser
             try {
                 CosmicExtensionTaskCallback callback = new CosmicExtensionTaskCallback(rdb);
-                CosmicParser.parse(cosmicFile, cosmicVersion, ID, assembly, callback);
+                CosmicParser101.parse(genomeScreensMutantFile, classificationFile, cosmicVersion, ID, assembly, callback);
             } catch (IOException e) {
                 throw new ToolException(e);
             }
@@ -159,6 +187,34 @@ public class CosmicVariantAnnotatorExtensionTask implements VariantAnnotatorExte
             }
         } catch (RocksDBException e) {
             throw new ToolException("Error initializing RocksDB", e);
+        }
+    }
+
+    protected void decompressTarBall(Path file, Path outputPath) throws IOException {
+        byte[] buffer = new byte[1024];
+
+        if (!Files.exists(outputPath)) {
+            Files.createDirectories(outputPath);
+        }
+
+        try (FileInputStream fis = new FileInputStream(file.toFile());
+             GZIPInputStream gis = new GZIPInputStream(fis);
+             TarArchiveInputStream tarInput = new TarArchiveInputStream(gis)) {
+
+            TarArchiveEntry entry;
+            while ((entry = tarInput.getNextTarEntry()) != null) {
+                if (entry.isDirectory()) {
+                    Files.createDirectories(outputPath.resolve(entry.getName()));
+                } else {
+                    Path entryOutputPath = outputPath.resolve(entry.getName());
+                    try (FileOutputStream fos = new FileOutputStream(entryOutputPath.toFile())) {
+                        int len;
+                        while ((len = tarInput.read(buffer)) > 0) {
+                            fos.write(buffer, 0, len);
+                        }
+                    }
+                }
+            }
         }
     }
 }
