b818b5dd0ff8a29340de11e100193cb4f6b392af
Joaquín Tárraga Giménez <joaquintarraga@gmail.com>
2025-04-08 10:31:48 +0200

    analysis: remove files added by mistake, #TASK-7037

--- opencga-storage/opencga-storage-hadoop/regenie_walker/step1/src/main/java/org/opencb/opencga/storage/hadoop/variant/index/annotation/AnnotationSummaryIndexSchema.java
+++
@@ -1,9 +0,0 @@
-package org.opencb.opencga.storage.hadoop.variant.index.annotation;
-
-import org.opencb.opencga.storage.hadoop.variant.index.core.FixedSizeIndexSchema;
-
-public class AnnotationSummaryIndexSchema extends FixedSizeIndexSchema {
-
-
-
-}
