9f2c58a0bb56ae3dfd1ca79c7886207f0d115898
Joaquín Tárraga Giménez <joaquintarraga@gmail.com>
2025-05-07 15:58:04 +0200

    cvdb: update the script CVDB install.sh, #TASK-7640

--- opencga-enterprise-app/app/cvdb/install.sh
+++ opencga-enterprise-app/app/cvdb/install.sh
@@ -8,4 +8,5 @@ ${SOLR_HOME}/bin/solr zk upconfig -n opencga-ca-configset-${ENTERPRISE_VERSION} 
 ${SOLR_HOME}/bin/solr zk upconfig -n opencga-ci-configset-${ENTERPRISE_VERSION} -d ./opencga-ci-configset-${ENTERPRISE_VERSION} -z ${ZK_HOST}
 ${SOLR_HOME}/bin/solr zk upconfig -n opencga-cv-configset-${ENTERPRISE_VERSION} -d ./opencga-cv-configset-${ENTERPRISE_VERSION} -z ${ZK_HOST}
 ${SOLR_HOME}/bin/solr zk upconfig -n opencga-cve-configset-${ENTERPRISE_VERSION} -d ./opencga-cve-configset-${ENTERPRISE_VERSION} -z ${ZK_HOST}
+${SOLR_HOME}/bin/solr zk upconfig -n opencga-viewers-configset-${ENTERPRISE_VERSION} -d ./opencga-viewers-configset-${ENTERPRISE_VERSION} -z ${ZK_HOST}
 
