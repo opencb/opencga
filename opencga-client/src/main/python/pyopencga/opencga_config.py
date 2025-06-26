f203c62bd097d0221869a6b215be597b5f7220b7
dapregi <dperezgil89@gmail.com>
2025-06-26 14:17:14 +0100

    client: fixed config parameter to skip ssl certification verification #TASK-7612

--- opencga-client/src/main/python/pyopencga/opencga_config.py
+++ opencga-client/src/main/python/pyopencga/opencga_config.py
@@ -59,7 +59,7 @@ class ClientConfiguration(object):
 
     def _validate_host(self):
         try:
-            r = requests.head(self.host, timeout=2, verify=self.tlsAllowInvalidCertificates)
+            r = requests.head(self.host, timeout=2, verify=not self.tlsAllowInvalidCertificates)
             if r.status_code == 302:
                 return
         except requests.exceptions.SSLError:
@@ -82,10 +82,11 @@ class ClientConfiguration(object):
 
     @property
     def tlsAllowInvalidCertificates(self):
-        if 'tlsAllowInvalidCertificates' in self._config['rest']:
+        if ('tlsAllowInvalidCertificates' in self._config['rest'] and
+                self._config['rest']['tlsAllowInvalidCertificates'] is not None):
             return self._config['rest']['tlsAllowInvalidCertificates']
         else:
-            return None
+            return False
 
     @tlsAllowInvalidCertificates.setter
     def tlsAllowInvalidCertificates(self, tlsAllowInvalidCertificates):
