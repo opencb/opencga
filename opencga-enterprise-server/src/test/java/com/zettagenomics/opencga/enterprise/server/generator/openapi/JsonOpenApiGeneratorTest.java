5ddbfd53f4d21c68ac559cc10fd642fad760600d
JuanfeSanahuja <juanfe.sanahuja@zettagenomics.com>
2025-07-14 12:11:12 +0200

    openapi: Added number of endpoints in category #TASK-7763

--- opencga-enterprise-server/src/test/java/com/zettagenomics/opencga/enterprise/server/generator/openapi/JsonOpenApiGeneratorTest.java
+++ opencga-enterprise-server/src/test/java/com/zettagenomics/opencga/enterprise/server/generator/openapi/JsonOpenApiGeneratorTest.java
@@ -39,7 +39,7 @@ public class JsonOpenApiGeneratorTest {
         ApiCommons apiCommons = new EnterpriseApiCommonsImpl();
         swagger = generator.generateJsonOpenApi(
                 apiCommons,
-                "SHA256SOMEVALIDTKEN",
+                "ANYVALIDTOKEN",
                 "test.app.zettagenomics.com/task-swagger/opencga",
                 "v2",
                 "test@germline:platinum"
@@ -167,6 +167,6 @@ public class JsonOpenApiGeneratorTest {
         assertNotNull("GET /v2/files/{file}/download should have security defined", security);
         assertEquals("GET /v2/files/{file}/download should have one security definition", 1, security.size());
         assertNotNull("GET /v2/files/{file}/download should have BearerAuth defined",  security.get(0).get("BearerAuth"));
-        assertEquals("GET /v2/files/{file}/download should have one security definition", security.get(0).get("BearerAuth").get(0), "Bearer SHA256SOMEVALIDTKEN");
+        assertEquals("GET /v2/files/{file}/download should have one security definition", security.get(0).get("BearerAuth").get(0), "Bearer ANYVALIDTOKEN");
     }
 }
