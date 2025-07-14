515ee57532a5442a14398d08c7c86e17ff74d857
JuanfeSanahuja <juanfe.sanahuja@zettagenomics.com>
2025-07-14 14:55:07 +0200

    openapi: Added correct opencga o opencga-enterprise version in JSON #TASK-7763

--- opencga-enterprise-server/src/main/java/com/zettagenomics/opencga/enterprise/server/generator/EnterpriseApiCommonsImpl.java
+++ opencga-enterprise-server/src/main/java/com/zettagenomics/opencga/enterprise/server/generator/EnterpriseApiCommonsImpl.java
@@ -1,5 +1,6 @@
 package com.zettagenomics.opencga.enterprise.server.generator;
 
+import com.zettagenomics.opencga.enterprise.core.GitUtils;
 import com.zettagenomics.opencga.enterprise.server.rest.*;
 import org.opencb.opencga.core.common.GitRepositoryState;
 import org.opencb.opencga.server.generator.commons.ApiCommons;
@@ -79,6 +80,6 @@ public class EnterpriseApiCommonsImpl implements ApiCommons {
 
     @Override
     public String getVersion() {
-        return "OpenCGA-Enterprise-"+GitRepositoryState.getInstance().getBuildVersion();
+        return "OpenCGA-Enterprise-"+ GitUtils.getEnterprise().getBuildVersion();
     }
 }
