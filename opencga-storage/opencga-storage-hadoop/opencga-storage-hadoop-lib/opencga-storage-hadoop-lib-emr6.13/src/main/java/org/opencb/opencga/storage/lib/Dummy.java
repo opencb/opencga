523c663dc1e2fe2d7fb4a52d6703abd74c0cc717
JuanfeSanahuja <juanfe.sanahuja@zettagenomics.com>
2025-08-07 13:13:37 +0200

    cicd: Fix Javadocs must be provided but not found in entries #TASK-7783

--- opencga-storage/opencga-storage-hadoop/opencga-storage-hadoop-lib/opencga-storage-hadoop-lib-emr6.13/src/main/java/org/opencb/opencga/storage/lib/Dummy.java
+++ opencga-storage/opencga-storage-hadoop/opencga-storage-hadoop-lib/opencga-storage-hadoop-lib-emr6.13/src/main/java/org/opencb/opencga/storage/lib/Dummy.java
@@ -1,5 +1,5 @@
 package org.opencb.opencga.storage.lib;
 /**
- * Classe to avoid the error "Javadocs must be provided but not found in entries" when deploy to maven repo.
+ * Class to avoid the error "Javadocs must be provided but not found in entries" when deploy to maven repo.
  */
 public class Dummy {}
