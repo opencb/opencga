c6a155c66756ce28990d3a8241dfffc0c958503f
Joaquín Tárraga Giménez <joaquintarraga@gmail.com>
2025-08-11 13:09:08 +0200

    analyis: implement the HISAT2 wrapper, #TASK-7877, #TASK-7838

---
+++ opencga-analysis/src/main/java/org/opencb/opencga/analysis/wrappers/hisat2/Hisat2WrapperAnalysis.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright 2015-2020 OpenCB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opencb.opencga.analysis.wrappers.hisat2;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.opencb.opencga.analysis.tools.OpenCgaToolScopeStudy;
+import org.opencb.opencga.analysis.wrappers.multiqc.MultiQcWrapperAnalysisExecutor;
+import org.opencb.opencga.catalog.managers.CatalogManager;
+import org.opencb.opencga.core.exceptions.ToolException;
+import org.opencb.opencga.core.models.common.Enums;
+import org.opencb.opencga.core.models.wrapper.Hisat2WrapperParams;
+import org.opencb.opencga.core.models.wrapper.MultiQcWrapperParams;
+import org.opencb.opencga.core.tools.annotations.Tool;
+import org.opencb.opencga.core.tools.annotations.ToolParams;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.opencb.opencga.analysis.wrappers.WrapperUtils.*;
+import static org.opencb.opencga.analysis.wrappers.hisat2.Hisat2WrapperAnalysisExecutor.*;
+import static org.opencb.opencga.analysis.wrappers.multiqc.MultiQcWrapperAnalysisExecutor.OUTDIR_PARAM;
+
+@Tool(id = Hisat2WrapperAnalysis.ID, resource = Enums.Resource.ALIGNMENT, description = Hisat2WrapperAnalysis.DESCRIPTION)
+public class Hisat2WrapperAnalysis extends OpenCgaToolScopeStudy {
+
+    public final static String ID = "hisat2";
+    public final static String DESCRIPTION = "HISAT2 is a fast and sensitive alignment program for mapping next-generation sequencing"
+            + " reads (whole-genome, transcriptome, and exome sequencing data) against the general human population.";
+
+    private final static String SAM_FILENAME_DEFAULT = "output.sam";
+
+    @ToolParams
+    protected final Hisat2WrapperParams analysisParams = new Hisat2WrapperParams();
+
+    private Hisat2WrapperParams updatedParams;
+
+    protected void check() throws Exception {
+        // IMPORTANT: the first thing to do since it initializes "study" from params.get(STUDY_PARAM)
+        super.check();
+
+        setUpStorageEngineExecutor(study);
+
+        updatedParams = new Hisat2WrapperParams();
+
+        // Check parameters, and get physical paths from OpenCGA catalog files before passing them to the executor
+
+        // Check command
+        if (StringUtils.isEmpty(analysisParams.getHisat2Params().getCommand())) {
+            throw new ToolException("Missing command. Please, specify the HISAT2 command to run (e.g., 'hisat2', 'hisat2-build',...).");
+        }
+        String command = analysisParams.getHisat2Params().getCommand().toLowerCase();
+        updatedParams.getHisat2Params().setCommand(command);
+
+        // Check input
+        if (CollectionUtils.isNotEmpty(analysisParams.getHisat2Params().getInput())) {
+            if (analysisParams.getHisat2Params().getInput().size() == 2
+                    && (command.equals(HISAT2_BUILD_TOOL) || command.equals(HISAT2_BUILD_L_TOOL) || command.equals(HISAT2_BUILD_S_TOOL))) {
+                List<String> input = checkPaths((List<String>) analysisParams.getHisat2Params().getInput().get(0), study, catalogManager,
+                        token);
+                updatedParams.getHisat2Params().getInput().add(input);
+                // Set output directory to the JOB directory
+                String basename = Paths.get(analysisParams.getHisat2Params().getInput().get(1).toString()).getFileName().toString();
+                logger.warn("The parameter positional '<ht2_index_base>' will be overwritten by the JOB directory instead.");
+                updatedParams.getHisat2Params().getInput().add(OUTPUT_FILE_PREFIX + getOutDir().toAbsolutePath() + "/" + basename);
+            } else {
+                throw new ToolException("Not yet implemented: HISAT2 command '" + command + "' with input size "
+                        + analysisParams.getHisat2Params().getInput().size() + ". Expected 2 for 'hisat2-build' commands.");
+            }
+        }
+
+        // Check HISAT2 parameters
+        if (MapUtils.isNotEmpty(analysisParams.getHisat2Params().getParams())) {
+            String samFilename = analysisParams.getHisat2Params().getParams().getString(S_PARAM);
+            if (StringUtils.isEmpty(samFilename)) {
+                samFilename = SAM_FILENAME_DEFAULT;
+            } else {
+                if (samFilename.startsWith(FILE_PREFIX)) {
+                    samFilename = Paths.get(samFilename.substring(FILE_PREFIX.length())).getFileName().toString();
+                } else {
+                    samFilename = Paths.get(samFilename).getFileName().toString();
+                }
+            }
+            if (command.equals(HISAT2_TOOL)) {
+                analysisParams.getHisat2Params().getParams().remove(S_PARAM);
+            }
+
+            updatedParams.getHisat2Params().setParams(checkParams(analysisParams.getHisat2Params().getParams(), study, catalogManager,
+                    token));
+
+            if (command.equals(HISAT2_TOOL)) {
+                // Set output directory to the JOB directory
+                logger.warn("The parameter '{}' will be overwritten by the JOB directory instead (filename = {}).", S_PARAM, samFilename);
+                updatedParams.getHisat2Params().getParams().put(S_PARAM, OUTPUT_FILE_PREFIX + getOutDir().toAbsolutePath() + "/"
+                        + samFilename);
+            }
+        }
+    }
+
+    @Override
+    protected List<String> getSteps() {
+        return Arrays.asList(ID);
+    }
+
+    protected void run() throws ToolException, IOException {
+        // Run HISAT2
+        step(ID, this::runHisat2);
+    }
+
+    protected void runHisat2() throws ToolException {
+        // Get executor
+        Hisat2WrapperAnalysisExecutor executor = getToolExecutor(Hisat2WrapperAnalysisExecutor.class);
+
+        // Set parameters and execute
+        executor.setStudy(study)
+                .setHisat2WrapperParams(updatedParams)
+                .execute();
+    }
+}
---
+++ opencga-analysis/src/main/java/org/opencb/opencga/analysis/wrappers/hisat2/Hisat2WrapperAnalysisExecutor.java
@@ -0,0 +1,154 @@
+package org.opencb.opencga.analysis.wrappers.hisat2;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.opencb.commons.datastore.core.Event;
+import org.opencb.commons.datastore.core.ObjectMap;
+import org.opencb.commons.utils.FileUtils;
+import org.opencb.opencga.analysis.wrappers.executors.DockerWrapperAnalysisExecutor;
+import org.opencb.opencga.analysis.wrappers.multiqc.MultiQcWrapperAnalysis;
+import org.opencb.opencga.core.exceptions.ToolExecutorException;
+import org.opencb.opencga.core.models.wrapper.Hisat2WrapperParams;
+import org.opencb.opencga.core.models.wrapper.MultiQcWrapperParams;
+import org.opencb.opencga.core.tools.annotations.ToolExecutor;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+import static org.opencb.opencga.analysis.wrappers.WrapperUtils.*;
+import static org.opencb.opencga.analysis.wrappers.multiqc.MultiQcWrapperAnalysis.ID;
+
+@ToolExecutor(id = Hisat2WrapperAnalysisExecutor.ID,
+        tool = ID,
+        source = ToolExecutor.Source.FILE,
+        framework = ToolExecutor.Framework.DOCKER)
+public class Hisat2WrapperAnalysisExecutor extends DockerWrapperAnalysisExecutor {
+
+    public static final String ID = MultiQcWrapperAnalysis.ID + "-docker";
+
+    public static final String X_PARAM = "-x";
+    public static final String S_PARAM = "-S";
+
+    public static final String HISAT2_TOOL = "hisat2";
+    public static final String HISAT2_BUILD_TOOL = "hisat2-build";
+    public static final String HISAT2_BUILD_L_TOOL = "hisat2-build-l";
+    public static final String HISAT2_BUILD_S_TOOL = "hisat2-build-s";
+
+    private static final String WRAPPER_SCRIPT = "multiqc_wrapper.py";
+
+    private String study;
+    private Hisat2WrapperParams hisat2WrapperParams;
+
+    @Override
+    protected void run() throws Exception {
+        // Sanity check, outdir must match the output directory
+        String outDir;
+        if (hisat2WrapperParams.getHisat2Params().getParams().containsKey(S_PARAM)) {
+            outDir = hisat2WrapperParams.getHisat2Params().getParams().getString(S_PARAM).substring(OUTPUT_FILE_PREFIX.length());
+        } else {
+            outDir = ((String) hisat2WrapperParams.getHisat2Params().getInput().get(1)).substring(OUTPUT_FILE_PREFIX.length());
+        }
+//        if (!getOutDir().toAbsolutePath().toString().startsWith(outDir)) {
+//            throw new ToolExecutorException("Output directory '" + outDir + "' does not match the expected output directory '"
+//                    + getOutDir() + "'");
+//        }
+
+        // HISAT2 parameters to be executed in the docker container, it must contain virtual paths
+        ObjectMap updatedParams = new ObjectMap();
+
+        // Input and output bindings
+        List<AbstractMap.SimpleEntry<String, String>> bindings = new ArrayList<>();
+        Set<String> readOnlyBindings = new HashSet<>();
+
+        // Script path
+        String virtualAnalysisPath = buildVirtualAnalysisPath(getExecutorParams().getString("opencgaHome"), bindings, readOnlyBindings);
+
+        // HISAT2 input
+        String indexBasename ;
+        if (CollectionUtils.isNotEmpty(hisat2WrapperParams.getHisat2Params().getInput())) {
+            List<String> input = new ArrayList<>();
+            List<String> input0 = buildVirtualPaths((List<String>) hisat2WrapperParams.getHisat2Params().getInput().get(0), "input",
+                    bindings, readOnlyBindings);
+            input.add(StringUtils.join(input0, ","));
+
+            // If the command is hisat2-build, we need to get index basename
+            Path path = Paths.get(((String) hisat2WrapperParams.getHisat2Params().getInput().get(1))
+                    .substring(OUTPUT_FILE_PREFIX.length()));
+            indexBasename = path.getFileName().toString();
+            String input1 = buildVirtualPath(OUTPUT_FILE_PREFIX + path.getParent().toAbsolutePath(), "output", bindings, readOnlyBindings);
+            input.add(input1 + indexBasename);
+
+            updatedParams.put(INPUT, input);
+        }
+
+        // HISAT params
+        // Save SAM file path if it exists, so we can update it later
+        String samFile = hisat2WrapperParams.getHisat2Params().getParams().getString(S_PARAM);
+        if (StringUtils.isNotEmpty(samFile)) {
+            hisat2WrapperParams.getHisat2Params().getParams().remove(S_PARAM);
+        }
+        // Save index file path if it exists, so we can update it later
+        String indexFile = hisat2WrapperParams.getHisat2Params().getParams().getString(X_PARAM);
+        if (StringUtils.isNotEmpty(indexFile)) {
+            hisat2WrapperParams.getHisat2Params().getParams().remove(X_PARAM);
+        }
+        updatedParams.put(PARAMS, updateParams(hisat2WrapperParams.getHisat2Params().getParams(), "data", bindings, readOnlyBindings));
+        // Restore SAM file path if it exists
+        if (StringUtils.isNotEmpty(samFile)) {
+            String output = buildVirtualPath(OUTPUT_FILE_PREFIX + Paths.get(samFile.substring(OUTPUT_FILE_PREFIX.length())).getParent()
+                            .toAbsolutePath(), "output", bindings, readOnlyBindings);
+            String samFilename = Paths.get(samFile.substring(OUTPUT_FILE_PREFIX.length())).getFileName().toString();
+            updatedParams.getMap(PARAMS).put(S_PARAM, output + samFilename);
+        }
+        // Restore index file path if it exists
+        if (StringUtils.isNotEmpty(indexFile)) {
+            String index = buildVirtualPath(INPUT_FILE_PREFIX + Paths.get(indexFile.substring(INPUT_FILE_PREFIX.length())).getParent()
+                    .toAbsolutePath(), "index", bindings, readOnlyBindings);
+            String indexFilename = Paths.get(indexFile.substring(INPUT_FILE_PREFIX.length())).getFileName().toString();
+            String basename = indexFilename.substring(0, indexFilename.indexOf('.'));
+            updatedParams.getMap(PARAMS).put(X_PARAM, index + basename);
+        }
+
+        // Params file path
+        String virtualParamsPath = processParamsFile(updatedParams, getOutDir(), bindings, readOnlyBindings);
+
+        // Build Python command line with params file and execute it in docker
+//        String wrapperCli = buildWrapperCli("python3", virtualAnalysisPath, StarWrapperAnalysis.ID, WRAPPER_SCRIPT, virtualParamsPath);
+        String wrapperCli = buildWrapperCli("python3", virtualAnalysisPath, hisat2WrapperParams.getHisat2Params().getCommand(),
+                virtualParamsPath);
+        String dockerImage = "opencb/opencga-transcriptomics:" + getDockerImageVersion();
+
+        // User: array of two strings, the first string, the user; the second, the group
+        String[] user = FileUtils.getUserAndGroup(getOutDir(), true);
+        Map<String, String> dockerParams = new HashMap<>();
+        dockerParams.put("user", user[0] + ":" + user[1]);
+
+        String dockerCli = buildCommandLine(dockerImage, bindings, readOnlyBindings, wrapperCli, dockerParams);
+        addEvent(Event.Type.INFO, "Docker command line: " + dockerCli);
+        logger.info("Docker command line: {}", dockerCli);
+        int exitValue = runCommandLine(dockerCli);
+
+        if (exitValue != 0) {
+            throw new ToolExecutorException("Error executing HISAT2: exit value " + exitValue + ". Check the logs for more details.");
+        }
+    }
+
+    public String getStudy() {
+        return study;
+    }
+
+    public Hisat2WrapperAnalysisExecutor setStudy(String study) {
+        this.study = study;
+        return this;
+    }
+
+    public Hisat2WrapperParams getHisat2WrapperParams() {
+        return hisat2WrapperParams;
+    }
+
+    public Hisat2WrapperAnalysisExecutor setHisat2WrapperParams(Hisat2WrapperParams hisat2WrapperParams) {
+        this.hisat2WrapperParams = hisat2WrapperParams;
+        return this;
+    }
+}
