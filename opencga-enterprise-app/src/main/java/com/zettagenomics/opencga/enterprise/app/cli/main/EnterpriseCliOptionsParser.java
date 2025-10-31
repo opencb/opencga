d24ee44d78cfdccf5292d1096765cfee57fd72da
imedina <igmecas@gmail.com>
2025-10-31 14:46:03 +0000

    clients: affy update

--- opencga-enterprise-app/src/main/java/com/zettagenomics/opencga/enterprise/app/cli/main/EnterpriseCliOptionsParser.java
+++ opencga-enterprise-app/src/main/java/com/zettagenomics/opencga/enterprise/app/cli/main/EnterpriseCliOptionsParser.java
@@ -134,6 +134,7 @@ public class EnterpriseCliOptionsParser extends CustomCliOptionsParser {
         analysisClinicalSubCommands.addCommand("interpreter-zetta-run", analysisClinicalCommandOptions.runInterpreterZettaCommandOptions);
         analysisClinicalSubCommands.addCommand("load", analysisClinicalCommandOptions.loadCommandOptions);
         analysisClinicalSubCommands.addCommand("ngs-pipeline-run", analysisClinicalCommandOptions.runNgsPipelineCommandOptions);
+        analysisClinicalSubCommands.addCommand("pipeline-affy-run", analysisClinicalCommandOptions.runPipelineAffyCommandOptions);
         analysisClinicalSubCommands.addCommand("pipeline-genomics-run", analysisClinicalCommandOptions.runPipelineGenomicsCommandOptions);
         analysisClinicalSubCommands.addCommand("pipeline-prepare-run", analysisClinicalCommandOptions.runPipelinePrepareCommandOptions);
         analysisClinicalSubCommands.addCommand("rga-aggregation-stats", analysisClinicalCommandOptions.aggregationStatsRgaCommandOptions);
