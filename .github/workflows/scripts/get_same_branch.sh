fdaa58dd2a5da27d8ef11a8ddbf6d5d573e0e6ce
Jacobo Coll Moragón <jacobo167@gmail.com>
2025-11-04 15:56:30 +0000

    cicd: Add log groups to get_same_branch.sh #TASK-7809

--- .github/workflows/scripts/get_same_branch.sh
+++ .github/workflows/scripts/get_same_branch.sh
@@ -10,15 +10,17 @@ fi
 
 function install(){
   local REPO=$1
+  echo "::group::Installing '$REPO' project from branch $BRANCH_NAME"
   cd /home/runner/work/ || exit 2
   git clone https://github.com/opencb/"$REPO".git -b "$BRANCH_NAME"
   if [ -d "./$REPO" ]; then
     cd "$REPO" || exit 2
     echo "Branch name $BRANCH_NAME already exists."
-      mvn clean install -DskipTests --no-transfer-progress
+    mvn clean install -DskipTests --no-transfer-progress
   else
-    echo "$CURRENT Branch is NOT EQUALS $BRANCH_NAME "
+    echo "Branch name $BRANCH_NAME does not exist in $REPO repository. Skipping installation."
   fi
+  echo "::endgroup::"
 }
 
 install "java-common-libs"
