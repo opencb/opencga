65a4b3cd59323a6362061d9f67127182dce39a55
Joaquín Tárraga Giménez <joaquintarraga@gmail.com>
2026-02-24 15:13:32 +0100

    server: prioritize the OpenCGAObjectMapperProvider over the default one, #TASK-8139

--- opencga-server/src/main/java/org/opencb/opencga/server/rest/OpenCGAObjectMapperProvider.java
+++ opencga-server/src/main/java/org/opencb/opencga/server/rest/OpenCGAObjectMapperProvider.java
@@ -1,8 +1,13 @@
 package org.opencb.opencga.server.rest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.opencb.biodata.models.variant.avro.GwasAssociationStudyTraitScores;
 import org.opencb.opencga.core.common.JacksonUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.annotation.Priority;
+import javax.ws.rs.Priorities;
 import javax.ws.rs.ext.ContextResolver;
 import javax.ws.rs.ext.Provider;
 
@@ -20,18 +25,27 @@ import javax.ws.rs.ext.Provider;
  * @see org.opencb.opencga.core.models.common.mixins.GwasAssociationStudyTraitScoresMixin
  */
 @Provider
+@Priority(Priorities.USER)
 public class OpenCGAObjectMapperProvider implements ContextResolver<ObjectMapper> {
 
+    private static final Logger logger = LoggerFactory.getLogger(OpenCGAObjectMapperProvider.class);
     private final ObjectMapper mapper;
 
     public OpenCGAObjectMapperProvider() {
         // Use the default non-null mapper which includes all variant mixins
         // (GwasAssociationStudyTraitScores, VariantAnnotation, ConsequenceType, etc.)
         mapper = JacksonUtils.getDefaultNonNullObjectMapper();
+
+        // Log that the provider was initialized with the mixin
+        Class<?> gwasRawMixin = mapper.findMixInClassFor(GwasAssociationStudyTraitScores.class);
+        logger.info("OpenCGAObjectMapperProvider initialized");
+        logger.info("GwasAssociationStudyTraitScores mixin: {}",
+                gwasRawMixin != null ? gwasRawMixin.getSimpleName() : "NOT FOUND");
     }
 
     @Override
     public ObjectMapper getContext(Class<?> type) {
+        logger.debug("OpenCGAObjectMapperProvider.getContext called for type: {}", type.getSimpleName());
         return mapper;
     }
 }
