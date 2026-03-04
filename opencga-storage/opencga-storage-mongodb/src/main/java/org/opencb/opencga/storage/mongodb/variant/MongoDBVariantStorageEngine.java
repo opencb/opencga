57db603277223118e8c9e8c59b50134592038cd8
Jacobo Coll Moragón <jacobo167@gmail.com>
2026-03-04 15:11:28 +0000

    storage: Remove dead-code guard for mendelian/de-novo query filters in MongoDB. #TASK-8038

--- opencga-storage/opencga-storage-mongodb/src/main/java/org/opencb/opencga/storage/mongodb/variant/MongoDBVariantStorageEngine.java
+++ opencga-storage/opencga-storage-mongodb/src/main/java/org/opencb/opencga/storage/mongodb/variant/MongoDBVariantStorageEngine.java
@@ -37,7 +37,6 @@ import org.opencb.opencga.storage.core.metadata.models.TaskMetadata;
 import org.opencb.opencga.storage.core.metadata.models.project.SearchIndexMetadata;
 import org.opencb.opencga.storage.core.variant.VariantStorageEngine;
 import org.opencb.opencga.storage.core.variant.VariantStorageOptions;
-import org.opencb.opencga.storage.core.variant.adaptors.VariantQueryException;
 import org.opencb.opencga.storage.core.variant.adaptors.VariantQueryParam;
 import org.opencb.opencga.storage.core.variant.adaptors.iterators.VariantDBIterator;
 import org.opencb.opencga.storage.core.variant.annotation.VariantAnnotationManager;
@@ -608,16 +607,6 @@ public class MongoDBVariantStorageEngine extends VariantStorageEngine {
 
     @Override
     public Query preProcessQuery(Query originalQuery, QueryOptions options) {
-        if (isValidParam(originalQuery, SAMPLE_MENDELIAN_ERROR)) {
-            throw VariantQueryException.unsupportedVariantQueryFilter(SAMPLE_MENDELIAN_ERROR, getStorageEngineId());
-        }
-        if (isValidParam(originalQuery, SAMPLE_DE_NOVO)) {
-            throw VariantQueryException.unsupportedVariantQueryFilter(SAMPLE_DE_NOVO, getStorageEngineId());
-        }
-        if (isValidParam(originalQuery, SAMPLE_DE_NOVO_STRICT)) {
-            throw VariantQueryException.unsupportedVariantQueryFilter(SAMPLE_DE_NOVO_STRICT, getStorageEngineId());
-        }
-
         Query query = super.preProcessQuery(originalQuery, options);
         List<String> studyNames = metadataManager.getStudyNames();
 
